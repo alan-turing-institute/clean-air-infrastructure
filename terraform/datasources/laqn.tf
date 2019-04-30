4b39dcf8eac67a7badd174d2d32eb3a5ac179d7a
James Robinson <james.em.robinson@gmail.com>
2019-04-30 11:10:43 +0100

    Attempt to use Terraform file provisioning

--- terraform/datasources/laqn.tf
+++ terraform/datasources/laqn.tf
@@ -1,17 +1,18 @@
-# Create a random admin password each time the network card is created
+# Generate a random string that persists for the lifetime of the resource group
 # NB. we cannot tie this to the creation of the VM, since this creates a dependency cycle
-resource "random_string" "password" {
+resource "random_string" "alphanumeric_with_special_laqn" {
   keepers = {
-    network_card = "${azurerm_network_interface.laqn_nic.name}"
+      resource_group = "${azurerm_resource_group.rg_cleanair_datasources.name}"
   }
   length = 16
   special = true
 }
 
+
 # Store the admin password in the keyvault
 resource "azurerm_key_vault_secret" "laqn_vm_admin_password" {
   name         = "laqn-vm-admin-password"
-  value        = "${random_string.password.result}"
+  value        = "${random_string.alphanumeric_with_special_laqn.result}"
   key_vault_id = "${var.keyvault_id}"
 }
 
@@ -47,11 +48,12 @@ resource "azurerm_network_interface" "laqn_nic" {
 
 # Create the LAQN VM
 resource "azurerm_virtual_machine" "laqn_vm" {
-    name                  = "LAQN-VM"
-    location              = "${var.location}"
-    resource_group_name   = "${azurerm_resource_group.rg_cleanair_datasources.name}"
-    network_interface_ids = ["${azurerm_network_interface.laqn_nic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    name                          = "LAQN-VM"
+    delete_os_disk_on_termination = true
+    location                      = "${var.location}"
+    resource_group_name           = "${azurerm_resource_group.rg_cleanair_datasources.name}"
+    network_interface_ids         = ["${azurerm_network_interface.laqn_nic.id}"]
+    vm_size                       = "Standard_DS1_v2"
 
     storage_os_disk {
         name              = "LAQN-OSDISK"
@@ -71,6 +73,7 @@ resource "azurerm_virtual_machine" "laqn_vm" {
         computer_name  = "LAQN-VM"
         admin_username = "atiadmin"
         admin_password = "${azurerm_key_vault_secret.laqn_vm_admin_password.value}"
+        custom_data    = "${file("${path.module}/cloudinit/laqn.yaml")}"
     }
 
     os_profile_linux_config {
@@ -82,9 +85,23 @@ resource "azurerm_virtual_machine" "laqn_vm" {
         storage_uri = "${var.boot_diagnostics_uri}"
     }
 
+    # Copies the laqn script folder to /home/laqndaemon/ on the remote VM
+    provisioner "file" {
+        source      = "${path.module}/../../scripts/datasources/laqn/laqn_test.py"
+        destination = "/laqn_test.py"
+
+        connection {
+            type     = "ssh"
+            user     = "atiadmin"
+            password = "${azurerm_key_vault_secret.laqn_vm_admin_password.value}"
+        }
+    }
+
     tags {
         environment = "Terraform Clean Air"
     }
 }
 
-
+output "path" {
+    value = "${path.module}/../../scripts/datasources/laqn/"
+}
