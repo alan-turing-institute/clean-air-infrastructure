43b6fcde4bc0ec4716bcac2579e4b8a7aafe4702
James Robinson <james.em.robinson@gmail.com>
2019-05-02 16:17:54 +0100

    Consolidate datasource setup

--- terraform/datasources/datasources.tf
+++ terraform/datasources/datasources.tf
@@ -1,4 +1,60 @@
-module "datasource" {
+# Ensure the datasources resource group exists
+resource "azurerm_resource_group" "rg_cleanair_datasources" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
+  tags {
+      environment = "Terraform Clean Air"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet_cleanair_datasources" {
+    name                = "VNET_CLEANAIR_DATASOURCES"
+    address_space       = ["10.0.0.0/16"]
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg_cleanair_datasources.name}"
+    tags {
+        environment = "Terraform Clean Air"
+    }
+}
+
+resource "azurerm_subnet" "subnet_cleanair_datasources" {
+    name                 = "SUBNET_CLEANAIR_DATASOURCES"
+    resource_group_name  = "${azurerm_resource_group.rg_cleanair_datasources.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet_cleanair_datasources.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_security_group" "nsg_cleanair_datasources" {
+    name                = "NSG_CLEANAIR_DATASOURCES"
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg_cleanair_datasources.name}"
+    security_rule {
+        name                       = "GithubWebhook"
+        priority                   = 1000
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "10.0.2.0/24"
+    }
+    security_rule {
+        name                       = "SSH"
+        priority                   = 2000
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefixes    = ["193.60.220.240", "193.60.220.253"]
+        destination_address_prefix = "*"
+    }
+    tags {
+        environment = "Terraform Clean Air"
+    }
+}
+module "laqn" {
   source               = "./datasource"
   datasource           = "laqn"
   boot_diagnostics_uri = "${var.boot_diagnostics_uri}"
@@ -8,6 +64,21 @@ module "datasource" {
   nsg_id               = "${azurerm_network_security_group.nsg_cleanair_datasources.id}"
   subnet_id            = "${azurerm_subnet.subnet_cleanair_datasources.id}"
 }
+
+module "aqn" {
+  source               = "./datasource"
+  datasource           = "aqn"
+  boot_diagnostics_uri = "${var.boot_diagnostics_uri}"
+  keyvault_id          = "${var.keyvault_id}"
+  location             = "${var.location}"
+  resource_group       = "${azurerm_resource_group.rg_cleanair_datasources.name}"
+  nsg_id               = "${azurerm_network_security_group.nsg_cleanair_datasources.id}"
+  subnet_id            = "${azurerm_subnet.subnet_cleanair_datasources.id}"
+}
+
+
+
+
 
 # # Variables used by the VM
 # variable "username" {
