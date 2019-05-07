8cfcc3b6cdd20ab5190bccc5d9be4c4177c22b95
Oscar Giles <ogiles@turing.ac.uk>
2019-05-07 11:34:12 +0100

    test

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$ACR_PASSWORD" | docker login -u "$ACR_USERNAME" --password-stdin cleanairtestcontainerregistry.azurecr.io
-
+docker build -t cleanairtestcontainerregistry.azurecr.io/laqn:latest scripts/datasources/laqn/.
 docker push cleanairtestcontainerregistry.azurecr.io/laqn:latest
