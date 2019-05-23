d4d870c97b1e4a0cec83dbf6014d436968337e0e
Oscar Giles <ogiles@turing.ac.uk>
2019-05-23 17:22:56 +0100

    Change to pull the commit sha tagged image

--- terraform/datasources/datasource/github_webhook/provisioning/run_application.sh
+++ terraform/datasources/datasource/github_webhook/provisioning/run_application.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo ${password} | docker login -u ${username} --password-stdin  ${host}
-docker run -v /.secrets:/.secrets/ ${host}/${datasource}:latest
+docker run -v /.secrets:/.secrets/ ${host}/${datasource}:$LATEST_COMMIT_HASH
