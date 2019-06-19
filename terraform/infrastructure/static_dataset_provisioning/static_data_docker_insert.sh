6ce01480d73a757cbbdb7558d715940ba6c33a12
Oscar Giles <ogiles@turing.ac.uk>
2019-06-19 14:02:22 +0100

    Working templated bash script to insert static datasets into postgreSQL database

--- terraform/infrastructure/static_dataset_provisioning/static_data_docker_insert.sh
+++ terraform/infrastructure/static_dataset_provisioning/static_data_docker_insert.sh
@@ -2,6 +2,12 @@
 
 # This file was generated automatically by terraform. It will insert GDB files to a postgreSQL database 
 
+realpath() {
+    local path=`eval echo "$1"`
+    local folder=$(dirname "$path")
+    echo $(cd "$folder"; pwd)/$(basename "$path"); 
+}
+
 # Login to ACR. Will prompt for details
 docker login ${acr_login_server}
 
@@ -9,5 +15,14 @@ docker login ${acr_login_server}
 docker pull ${acr_login_server}/insert_static_datasource:latest
 
 # Run docker image
+full_data_path=$(realpath static_data_tmp/UKMap.gdb)
+full_secret_path=$(realpath terraform/.secrets/.ukmap_secret.json)
 
-# docker run -v 
+echo $full_data_path
+echo $full_secret_path
+
+docker run -it \
+  -v $full_secret_path:/.secrets/.secret.json \
+  -v $full_data_path:/data/static_data.gdb  \
+  ${acr_login_server}/insert_static_datasource:latest
+
