b6a7bc0e3cc8956c06dd6bf16462a321654b598b
Oscar Giles <ogiles@turing.ac.uk>
2019-07-17 09:46:59 +0100

    test setting EPSG

--- scripts/datasources/static/insert_gdb_file_to_postGIS.sh
+++ scripts/datasources/static/insert_gdb_file_to_postGIS.sh
@@ -11,6 +11,6 @@ python3 app/prep_database.py
 PG="host=$HOST port=$PORT dbname=$DB_NAME user=$USERNAME password=$PASSWORD sslmode=$SSL_MODE"
 echo "Inserting .gdb file into $HOST"
 
-ogr2ogr -f PostgreSQL "PG:$PG" /data/static_data.gdb -overwrite -progress --config PG_USE_COPY YES -overwrite -progress --config PG_USE_COPY YES
+ogr2ogr -f PostgreSQL "PG:$PG" /data/static_data.gdb -overwrite -progress --config PG_USE_COPY YES -t_srs EPSG:4326
 
-python3 app/configure_database.py
+# python3 app/configure_database.py
