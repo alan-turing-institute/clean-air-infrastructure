f6549ab81a7cc40b9d4f7ed1e22038d540a3c8a1
Oscar Giles <ogiles@turing.ac.uk>
2019-07-18 09:30:27 +0100

    Working configure static database

--- scripts/datasources/static/insert_gdb_file_to_postGIS.sh
+++ scripts/datasources/static/insert_gdb_file_to_postGIS.sh
@@ -11,6 +11,7 @@ python3 app/prep_database.py
 PG="host=$HOST port=$PORT dbname=$DB_NAME user=$USERNAME password=$PASSWORD sslmode=$SSL_MODE"
 echo "Inserting .gdb file into $HOST"
 
-ogr2ogr -f PostgreSQL "PG:$PG" /data/static_data.gdb -overwrite -progress --config PG_USE_COPY YES -t_srs EPSG:4326
+# Insert data into database and reproject to EPSG:4326
+ogr2ogr -f PostgreSQL "PG:$PG" /data/static_data.gdb -overwrite -progress --config PG_USE_COPY YES -t_srs EPSG:4326 
 
-# python3 app/configure_database.py
+python3 app/configure_database.py
