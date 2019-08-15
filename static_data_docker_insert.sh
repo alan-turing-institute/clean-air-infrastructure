36ca56846a3e4a3c09842f50e0281b7a2eb4a1e0
Oscar Giles <ogiles@turing.ac.uk>
2019-08-15 12:11:55 +0100

    add all uploads to static_data_docker_insert.sh

--- static_data_docker_insert.sh
+++ static_data_docker_insert.sh
@@ -20,11 +20,11 @@ docker build -t cleanair_upload_static_dataset:latest \
 db_secret_path=$(realpath terraform/.secrets/)
 
 # # Insert street canyons
-# echo "Now working on Street Canyons data..."
-# docker run -it \
-#     -v $db_secret_path:/secrets/ \
-#     -v $(realpath static_data_local/CanyonsLondon_Erase):/data/Canyons \
-#     cleanair_upload_static_dataset:latest
+echo "Now working on Street Canyons data..."
+docker run -it \
+    -v $db_secret_path:/secrets/ \
+    -v $(realpath static_data_local/CanyonsLondon_Erase):/data/Canyons \
+    cleanair_upload_static_dataset:latest
 
 # Insert road link data and then configure the database
 echo "Now working on RoadLink data..."
@@ -33,24 +33,25 @@ docker run -it \
     -v $(realpath static_data_local/RoadLink):/data/RoadLink \
     cleanair_upload_static_dataset:latest
 
-# # Insert Hex Grid data and then configure the database
-# echo "Now working on Hexgrid data..."
-# docker run -it \
-#     -v $db_secret_path:/secrets/ \
-#     -v $(realpath static_data_local/Hex350_grid_GLA):/data/HexGrid \
-#     cleanair_upload_static_dataset:latest
+# Insert Hex Grid data and then configure the database
+echo "Now working on Hexgrid data..."
+docker run -it \
+    -v $db_secret_path:/secrets/ \
+    -v $(realpath static_data_local/Hex350_grid_GLA):/data/HexGrid \
+    cleanair_upload_static_dataset:latest
 
 
-# # Insert London Bounday data and then configure the database
-# echo "Now working on London boundart data..."
-# docker run -it \
-#     -v $db_secret_path:/secrets/ \
-#     -v $(realpath static_data_local/ESRI):/data/LondonBoundary \
-#     cleanair_upload_static_dataset:latest
+# Insert London Bounday data and then configure the database
+echo "Now working on London boundart data..."
+docker run -it \
+    -v $db_secret_path:/secrets/ \
+    -v $(realpath static_data_local/ESRI):/data/LondonBoundary \
+    cleanair_upload_static_dataset:latest
 
-# # Insert UKMap data
-# echo "Now working on UKMap data..."
-# docker run -it \
-#     -v $db_secret_path:/secrets/ \
-#     -v $(realpath static_data_local/UKMap.gdb):/data/UKMap.gdb \
-#     cleanair_upload_static_dataset:latest
+
+# Insert UKMap data
+echo "Now working on UKMap data..."
+docker run -it \
+    -v $db_secret_path:/secrets/ \
+    -v $(realpath static_data_local/UKMap.gdb):/data/UKMap.gdb \
+    cleanair_upload_static_dataset:latest
