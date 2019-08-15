2fdb61650e2103566e86088a2192b269675e3c9e
Oscar Giles <ogiles@turing.ac.uk>
2019-08-15 16:43:19 +0100

    Potential class for working with reflected tables (tables added to database outside of sqlalchemy

--- docker/datasources/databases/__init__.py
+++ docker/datasources/databases/__init__.py
@@ -1,8 +1,10 @@
 """Module for interacting with the Azure Postgres database"""
 from .connector import Connector
 from .updater import Updater
+from .static_tables import StaticTableConnector
 
 __all__ = [
     "Connector",
-    "Updater"
+    "Updater",
+    "StaticTableConnector"
 ]
