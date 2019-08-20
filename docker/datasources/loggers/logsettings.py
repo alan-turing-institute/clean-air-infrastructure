62b1c4106555ffcca7de94b4dde1606f422f6830
James Robinson <james.em.robinson@gmail.com>
2019-08-20 10:39:18 +0100

    Also disable log colours in get_logger

--- docker/datasources/loggers/logsettings.py
+++ docker/datasources/loggers/logsettings.py
@@ -1,5 +1,6 @@
-import logging
 import colorlog
+import logging
+import os
 
 
 logging.basicConfig(level=logging.INFO)
@@ -10,13 +11,22 @@ def get_logger(name, verbosity=0):
     # Default level is INFO [20], but add one level for each -v given at the command line
     logger = logging.getLogger(name)
     logger.setLevel(max(logging.getLogger().getEffectiveLevel() - 10 * verbosity, 0))
-    # Enable coloured log levels (INFO is not coloured)
+    # See whether we are using coloured logs or not
+    try:
+        disable_colours = bool(os.environ["NO_TEXT_COLOUR"])
+    except KeyError:
+        disable_colours = False
+    # Enable either coloured or normal log levels (INFO is not coloured)
     stream = logging.root.handlers[0]
-    stream.setFormatter(
-        colorlog.ColoredFormatter(r"%(asctime)s %(log_color)s%(levelname)8s%(reset)s: %(message)s",
-                                  datefmt=r"%Y-%m-%d %H:%M:%S",
-                                  log_colors={"DEBUG": "cyan",
-                                              "WARNING": "yellow",
-                                              "ERROR": "red",
-                                              "CRITICAL": "bold_red"}))
+    if disable_colours:
+        stream.setFormatter(
+            logging.Formatter(r"%(asctime)s %(levelname)8s: %(message)s", datefmt=r"%Y-%m-%d %H:%M:%S")
+    else:
+        stream.setFormatter(
+            colorlog.ColoredFormatter(r"%(asctime)s %(log_color)s%(levelname)8s%(reset)s: %(message)s",
+                                    datefmt=r"%Y-%m-%d %H:%M:%S",
+                                    log_colors={"DEBUG": "cyan",
+                                                "WARNING": "yellow",
+                                                "ERROR": "red",
+                                                "CRITICAL": "bold_red"}))
     return logger
