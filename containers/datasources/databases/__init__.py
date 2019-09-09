271cf5aa52a8bdbe6534018a5ba0498865640bcf
James Robinson <james.em.robinson@gmail.com>
2019-09-09 11:43:32 +0100

    Alphabetised

--- containers/datasources/databases/__init__.py
+++ containers/datasources/databases/__init__.py
@@ -1,10 +1,10 @@
 """Module for interacting with the Azure Postgres database"""
 from .connector import Connector
-from .updater import Updater
 from .static_tables import StaticTableConnector
+from .updater import Updater
 
 __all__ = [
     "Connector",
+    "StaticTableConnector",
     "Updater",
-    "StaticTableConnector"
 ]
