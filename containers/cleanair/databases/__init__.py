d03d5818ffb62c0e3aefe8f9633aeccb94f8bc17
James Robinson <james.em.robinson@gmail.com>
2019-09-30 16:57:19 +0100

    Export DBInteractor

--- containers/cleanair/databases/__init__.py
+++ containers/cleanair/databases/__init__.py
@@ -1,6 +1,7 @@
 """Module for interacting with the Azure Postgres database"""
 from .base import Base
 from .connector import Connector
+from .db_interactor import DBInteractor
 from .db_reader import DBReader
 from .db_writer import DBWriter
 from .interest_point_table import InterestPoint
@@ -10,6 +11,7 @@ from .static_tables import StaticTableConnector
 __all__ = [
     "Base",
     "Connector",
+    "DBInteractor",
     "DBReader",
     "DBWriter",
     "InterestPoint",
