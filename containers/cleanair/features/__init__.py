f51c3f524faf044c7ebf27aaa00da98b2f2e4b0a
James Robinson <james.em.robinson@gmail.com>
2019-10-04 14:59:38 +0100

    Removed old classes

--- containers/cleanair/features/__init__.py
+++ containers/cleanair/features/__init__.py
@@ -2,15 +2,7 @@
 Module for feature extraction
 """
 from .feature_extractor import FeatureExtractor
-# from .hexgrid_reader import HexGridReader
-# from .interestpoint_reader import InterestPointReader
-# from .londonboundary_reader import LondonBoundaryReader
-# from .ukmap_reader import UKMapReader
 
 __all__ = [
     "FeatureExtractor",
-    # "HexGridReader",
-    # "InterestPointReader",
-    # "LondonBoundaryReader",
-    # "UKMapReader",
 ]
