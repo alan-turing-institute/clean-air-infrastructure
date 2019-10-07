754eafc5769ee607bfdf94691af018ea39627d98
James Robinson <james.em.robinson@gmail.com>
2019-10-07 17:14:39 +0100

    Switched to insert/from_select to reduce memory requirements

--- containers/cleanair/features/static_features.py
+++ containers/cleanair/features/static_features.py
@@ -2,7 +2,8 @@
 UKMap Feature extraction
 """
 import time
-from sqlalchemy import func, or_, between, cast, Integer
+from sqlalchemy import func, or_, between, cast, Integer, String, literal, select
+from sqlalchemy.dialects.postgresql import insert
 from ..databases import DBWriter
 from ..databases.tables import InterestPoint, LondonBoundary, UKMap, UKMapIntersectionGeoms, UKMapIntersectionValues
 from ..loggers import duration, green
@@ -46,10 +47,10 @@ class StaticFeatures(DBWriter):
                 _query = _query.filter(InterestPoint.source.in_(include_sources))
         return _query
 
-    def query_ukmap_features(self, feature_name, feature_dict):
+    def query_ukmap_features(self, feature_type, feature_dict):
         """Query UKMap, selecting all features matching the requirements in feature_dict"""
         with self.dbcnxn.open_session() as session:
-            if feature_name == "building_height":
+            if feature_type == "building_height":
                 q_ukmap = session.query(UKMap.geom, UKMap.calculated_height_of_building, UKMap.feature_type)
             else:
                 q_ukmap = session.query(UKMap.geom, UKMap.landuse, UKMap.feature_type)
@@ -57,7 +58,7 @@ class StaticFeatures(DBWriter):
                 q_ukmap = q_ukmap.filter(or_(*[getattr(UKMap, column) == value for value in values]))
         return q_ukmap
 
-    def query_feature_geoms(self, q_interest_points, q_ukmap):
+    def query_feature_geoms(self, feature_type, q_interest_points, q_ukmap):
         """Construct one record for each interest point containing the point ID and one geometry column per buffer"""
         with self.dbcnxn.open_session() as session:
             # Outer join of queries: [Npoints * Ngeometries records]
@@ -85,18 +86,20 @@ class StaticFeatures(DBWriter):
 
             # Calculate each of the buffers: [Npoints records]
             q_intersections = session.query(sq_largest_buffer.c.point_id,
+                                            literal(feature_type).label("feature_type"),
                                             *[func.ST_ForceCollection(func.ST_Intersection(
                                                 func.Geometry(func.ST_Buffer(
                                                     func.Geography(sq_largest_buffer.c.location),
-                                                    distance
+                                                    radius
                                                 )),
                                                 sq_largest_buffer.c.largest_buffer
-                                                )) for distance in self.buffer_radii_metres])
+                                                )).label("geom_{}".format(radius))
+                                                for radius in self.buffer_radii_metres])
 
         # Return the overall query
         return q_intersections
 
-    def query_feature_values(self, q_interest_points, q_ukmap):
+    def query_feature_values(self, feature_type, q_interest_points, q_ukmap):
         """Construct one record for each interest point containing the point ID and one value column per buffer"""
         with self.dbcnxn.open_session() as session:
             # Outer join of queries: [Npoints * Ngeometries records]
@@ -126,12 +129,13 @@ class StaticFeatures(DBWriter):
                                        ).subquery()
 
             # Group these by interest point: [Npoints records]
-            q_grouped = session.query(sq_buffers.c.point_id,
-                                      *[func.max(getattr(sq_buffers.c, str(radius)))
-                                        for radius in self.buffer_radii_metres]).group_by(sq_buffers.c.point_id)
+            q_intersections = session.query(sq_buffers.c.point_id,
+                                            literal(feature_type).label("feature_type"),
+                                            *[func.max(getattr(sq_buffers.c, str(radius))).label("value_{}".format(radius))
+                                              for radius in self.buffer_radii_metres]).group_by(sq_buffers.c.point_id)
 
         # Return the overall query
-        return q_grouped
+        return q_intersections
 
     def calculate_ukmap_intersections(self):
         """
@@ -142,30 +146,48 @@ class StaticFeatures(DBWriter):
         q_sensors = self.query_sensor_locations(include_sources=self.sources)
 
         # Iterate over each of the UK map features and calculate the overlap with the sensors
-        for feature_name, feature_dict in self.ukmap_features.items():
+        for feature_type, feature_dict in self.ukmap_features.items():
             start = time.time()
-            self.logger.info("Now working on the %s feature", green(feature_name))
+            self.logger.info("Now working on the %s feature", green(feature_type))
 
             # Get UKMap geometries for this feature
-            q_ukmap = self.query_ukmap_features(feature_name, feature_dict)
+            q_ukmap = self.query_ukmap_features(feature_type, feature_dict)
 
             # Construct one tuple for each sensor, consisting of the point_id and a geometry collection for each radius
-            if feature_name == "building_height":
-                results = self.query_feature_values(q_sensors, q_ukmap).all()
-                site_records = [UKMapIntersectionValues.build_entry(feature_name, result) for result in results]
+            if feature_type == "building_height":
+                # results = self.query_feature_values(q_sensors, q_ukmap).all()
+                # site_records = [UKMapIntersectionValues.build_entry(feature_type, result) for result in results]
+                select_stmt = self.query_feature_values(feature_type, q_sensors, q_ukmap).subquery().select()
+                columns = [c.key for c in UKMapIntersectionValues.__table__.columns]
+                insert_stmt = insert(UKMapIntersectionValues).from_select(columns, select_stmt)
+                indexes = [UKMapIntersectionValues.point_id, UKMapIntersectionValues.feature_type]
+                merge_stmt = insert_stmt.on_conflict_do_nothing(index_elements=indexes)
             else:
-                results = self.query_feature_geoms(q_sensors, q_ukmap).all()
-                site_records = [UKMapIntersectionGeoms.build_entry(feature_name, result) for result in results]
-            self.logger.info("Constructed %s records in %s", green(len(results)), green(duration(start, time.time())))
+                # results = self.query_feature_geoms(q_sensors, q_ukmap).all()
+                # site_records = [UKMapIntersectionGeoms.build_entry(feature_type, result) for result in results]
+                select_stmt = self.query_feature_geoms(feature_type, q_sensors, q_ukmap).subquery().select()
+                columns = [c.key for c in UKMapIntersectionGeoms.__table__.columns]
+                insert_stmt = insert(UKMapIntersectionGeoms).from_select(columns, select_stmt)
+                indexes = [UKMapIntersectionGeoms.point_id, UKMapIntersectionGeoms.feature_type]
+                merge_stmt = insert_stmt.on_conflict_do_nothing(index_elements=indexes)
 
-            # Convert the query output into database records and merge these into the existing table
-            with self.dbcnxn.open_session() as session:
-                if site_records:
-                    self.add_records(session, site_records)
-                    self.logger.info("Committing %s records to database table %s",
-                                     green(len(site_records)),
-                                     green(site_records[0].__tablename__))
-                    session.commit()
+            # self.logger.info("Constructed %s records in %s", green(len(results)), green(duration(start, time.time())))
+
+            # Apply the Convert the query output into database records and merge these into the existing table
+            with self.dbcnxn.engine.connect() as cnxn:
+                self.logger.info("Merging features into database table...")
+                cnxn.execute(merge_stmt)
+                self.logger.info("Merging finished")
+
+            # # Convert the query output into database records and merge these into the existing table
+            # with self.dbcnxn.open_session() as session:
+            #     if site_records:
+            #         self.add_records(session, site_records)
+            #         self.logger.info("Committing %s records to database table %s",
+            #                          green(len(site_records)),
+            #                          green(site_records[0].__tablename__))
+            #         session.commit()
+            self.logger.info("Finished adding records in %s", green(duration(start, time.time())))
 
     def update_remote_tables(self):
         """Update all remote tables"""
