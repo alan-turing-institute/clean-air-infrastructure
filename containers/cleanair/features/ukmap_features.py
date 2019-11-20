0c237b3c6614b8aa9720bb4d999f39ed3ae73866
Oscar Giles <ogiles@turing.ac.uk>
2019-11-20 10:57:28 +0000

    Processing rect grid

--- containers/cleanair/features/ukmap_features.py
+++ containers/cleanair/features/ukmap_features.py
@@ -15,7 +15,6 @@ class UKMapFeatures(StaticFeatures):
 
         # List of features to extract
         self.features = {
-            "building_height": {"type": "value", "feature_dict": {"feature_type": ["Building"]}},
             "flat": {"type": "geom", "feature_dict": {"feature_type": ["Vegetated", "Water"]}, 'aggfunc': sum_area},
             "grass": {"type": "geom", "feature_dict": {"feature_type": ["Vegetated"]}, 'aggfunc': sum_area},
             "hospitals": {"type": "geom", "feature_dict": {"landuse": ["Hospitals"]}, 'aggfunc': sum_area},
@@ -23,6 +22,7 @@ class UKMapFeatures(StaticFeatures):
             "park": {"type": "geom", "feature_dict": {"feature_type": ["Vegetated"],
                                                       "landuse": ["Recreational open space"]}, 'aggfunc': sum_area},
             "water": {"type": "geom", "feature_dict": {"feature_type": ["Water"]}, 'aggfunc': sum_area},
+            "building_height": {"type": "value", "feature_dict": {"feature_type": ["Building"]}},
         }
 
     def query_features(self, feature_name):
