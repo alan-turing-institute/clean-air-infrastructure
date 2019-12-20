e75feea1249e431a24689105db3f10a7cb763676
Oscar Giles <ogiles@turing.ac.uk>
2019-12-20 11:24:03 +0000

    testing traffic model

--- containers/cleanair/models/traffic_model.py
+++ containers/cleanair/models/traffic_model.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from scipy.cluster.vq import kmeans2
 import numpy as np
 from dateutil.parser import isoparse
-from cleanair.databases.tables import (ScootRoadReading)
+from cleanair.databases.tables import (ScootRoadReading, ScootDetector, ScootReading)
 from cleanair.databases import DBReader
 from cleanair.loggers import get_logger
 import logging
-import pandas as pd 
+import pandas as pd
 from fbprophet import Prophet
 
 logging.basicConfig()
@@ -24,35 +24,60 @@ class TrafficModelData(DBReader):
         if not hasattr(self, "logger"):
             self.logger = get_logger(__name__)
 
-    def get_road_profiles(self, start_date, end_date):
+    def list_scoot_detectors(self):
+        """List all availabel scoot detectors"""
 
+        with self.dbcnxn.open_session() as session:
+            scoot_detector_q = session.query(ScootDetector.detector_n,
+                                             ScootDetector.toid,
+                                             ScootDetector.date_installed)
+
+        return pd.read_sql(scoot_detector_q.statement, scoot_detector_q.session.bind)
+
+
+    def get_training_data(self, start_date, end_date, scoot_ids=NotImplemented):
+        """Get scoot readings between start_date and end_date
+
+        args:
+            start_date: Date to get data from (inclusive)
+            end_date: Date to get data to (exclusive)
+            scoot_ids: List of ids to get data for
+
+        """
         start_date = isoparse(start_date)
         end_date = isoparse(end_date)
 
         with self.dbcnxn.open_session() as session:
 
-            scoot_road_q = session.query(ScootRoadReading).filter(ScootRoadReading.measurement_start_utc >= start_date,
-                                                   ScootRoadReading.measurement_start_utc < end_date).order_by(ScootRoadReading.road_toid, ScootRoadReading.measurement_start_utc)
+            scoot_road_q = session.query(ScootReading).filter(ScootReading.measurement_start_utc >= start_date,
+                                                              ScootReading.measurement_start_utc < end_date,
+                                                              )
 
+            if scoot_ids:
+                scoot_road_q = scoot_road_q.filter(ScootReading.detector_id.in_(scoot_ids))
 
-            return pd.read_sql(scoot_road_q.limit(1000).statement, scoot_road_q.session.bind)
+            scoot_road_q = scoot_road_q.order_by(ScootReading.detector_id, ScootReading.measurement_start_utc)
+
+        return pd.read_sql(scoot_road_q.statement, scoot_road_q.session.bind)
 
 
 
 traffic = TrafficModelData(secretfile='/Users/ogiles/Documents/project_repos/clean-air-infrastructure/terraform/.secrets/db_secrets.json')
 
-traffic_data = traffic.get_road_profiles('2019-11-10', '2019-12-11')
+scoot_detectors = traffic.list_scoot_detectors()
 
-sub_dat = traffic_data[traffic_data['road_toid'] == 'osgb4000000027865921']
+sub_data = traffic.get_training_data('2019-11-01', '2019-11-10', [scoot_detectors['detector_n'].values[0]])
+
+
 
 y_name = ['measurement_start_utc', 'occupancy_percentage']
 
-fit_data = sub_dat[y_name].copy()
+fit_data = sub_data[y_name].copy()
 fit_data = fit_data.rename(columns={'measurement_start_utc': 'ds', y_name[1]: 'y'})
 
 m = Prophet(changepoint_prior_scale=0.01).fit(fit_data)
 
-predict_df = m.make_future_dataframe(0, freq='H', include_history=True)
+predict_df = m.make_future_dataframe(100, freq='H', include_history=True)
 forecast = m.predict(predict_df)
 
 
