aa29742b436487a0e09cc5a877e6465a56e7d427
Oscar Giles <ogiles@turing.ac.uk>
2019-12-20 14:39:44 +0000

    Add satellite tables

--- containers/cleanair/databases/tables/satellite_tables.py
+++ containers/cleanair/databases/tables/satellite_tables.py
@@ -2,69 +2,30 @@
 Tables for Satellite data
 """
 from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, TIMESTAMP, UUID
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, TIMESTAMP, UUID, INTEGER
 from sqlalchemy.orm import relationship
 from ..base import Base
 
 
-class SatelliteArchive(Base):
-    """Table of Satellite readings"""
-    __tablename__ = "satellite_archieve"
-    __table_args__ = {"schema": "dynamic_data"}
+class SatelliteSite(Base):
+    """Locations of the descritised satellite locations"""
+    __tablename__ = "satellite_site"
+    __table_args__ = {"schema": "interest_points"}
 
-    site_code = Column(String(4), primary_key=True, nullable=False)
-    point_id = Column(UUID, ForeignKey("interest_points.meta_point.id"), nullable=False)
-    site_type = Column(String(20), nullable=False)
-    date_opened = Column(TIMESTAMP, nullable=False)
-    date_closed = Column(TIMESTAMP)
+    point_id = Column(UUID, ForeignKey("interest_points.meta_point.id"), primary_key=True, nullable=False)
+    box_id = Column(INTEGER, nullable=False)
 
-    # Create LAQNSite.readings and LAQNReading.site
-    readings = relationship("LAQNReading", backref="site")
-
-    # Create LAQNSite.point with no reverse relationship
+    # Create SatelliteSite.point with no reverse relationship
     point = relationship("MetaPoint")
 
+class SatelliteForecastReading(Base):
+    """Table of Satellite readings"""
+    __tablename__ = "satellite_forecast"
+    __table_args__ = {"schema": "dynamic_data"}
 
-# class SatelliteForecast(Base):
-#     """Table of LAQN readings"""
-#     __tablename__ = "laqn_reading"
-#     __table_args__ = {"schema": "dynamic_data"}
+    measurement_start_utc = Column(TIMESTAMP, primary_key=True, nullable=False)
+    measurement_end_utc = Column(TIMESTAMP, primary_key=True, nullable=False)
+    box_id = Column(INTEGER, ForeignKey("interest_points.satelite_site.box_id"), primary_key=True, nullable=False)
+    species_code = Column(String(4), primary_key=True, nullable=False)
+    value = Column(DOUBLE_PRECISION, nullable=True)
 
-#     site_code = Column(String(4), ForeignKey("interest_points.laqn_site.site_code"), primary_key=True, nullable=False)
-#     species_code = Column(String(4), primary_key=True, nullable=False)
-#     measurement_start_utc = Column(TIMESTAMP, primary_key=True, nullable=False)
-#     measurement_end_utc = Column(TIMESTAMP, primary_key=True, nullable=False)
-#     value = Column(DOUBLE_PRECISION, nullable=True)
-
-#     def __repr__(self):
-#         return "<LAQNReading(" + ", ".join([
-#             "site_code='{}'".format(self.site_code),
-#             "species_code='{}'".format(self.species_code),
-#             "measurement_start_utc='{}'".format(self.measurement_start_utc),
-#             "measurement_end_utc='{}'".format(self.measurement_end_utc),
-#             "value='{}'".format(self.value),
-#             ])
-
-#     @staticmethod
-#     def build_entry(reading_dict, return_dict=False):
-#         """
-#         Create an LAQNReading entry, replacing empty strings with None
-#         If return_dict then return a dictionary rather than and entry, to allow inserting via sqlalchemy core
-#         """
-
-#         # Replace empty strings
-#         reading_dict = {k: (v if v else None) for k, v in reading_dict.items()}
-
-#         if return_dict:
-#             new_key = ['site_code', 'species_code', 'measurement_start_utc', 'measurement_end_utc', 'value']
-#             old_key = ['SiteCode', '@SpeciesCode', 'MeasurementStartUTC', 'MeasurementEndUTC', '@Value']
-
-#             for i, key in enumerate(old_key):
-#                 reading_dict[new_key[i]] = reading_dict.pop(key)
-#             return reading_dict
-#         # Construct the record and return it
-#         return LAQNReading(site_code=reading_dict["SiteCode"],
-#                            species_code=reading_dict["@SpeciesCode"],
-#                            measurement_start_utc=reading_dict["MeasurementStartUTC"],
-#                            measurement_end_utc=reading_dict["MeasurementEndUTC"],
-#                            value=reading_dict["@Value"])
