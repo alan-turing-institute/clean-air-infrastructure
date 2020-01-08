606c013b97d834bb3a1f45c692d1f2aa0111f260
Oliver Hamelijnck <ohamelijnck@gmail.com>
2020-01-08 12:05:12 +0000

    move run_model outside of containers

---
+++ run_model/run_svgp.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('../containers')
+import cleanair
+from cleanair.models import SVGP_TF1
+
+import numpy as np
+
+X = np.load('data/processed_x_train.npy', allow_pickle=True)
+Y = np.load('data/processed_y_train.npy', allow_pickle=True)
+
+m = SVGP_TF1()
+
+model_parameters = {
+    'n_inducing_points': 200
+}
+
+m.fit(X, Y, max_iter=11, model_params=model_parameters, refresh=10)
+ys, ys_var = m.predict(X[1][:, 0, :])
+print(ys, ys_var)
