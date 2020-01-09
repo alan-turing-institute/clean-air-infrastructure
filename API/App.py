72abd393f11a6d7738dff2c2cbb26fc1fe1d7103
Oscar Giles <ogiles@turing.ac.uk>
2020-01-09 17:08:50 +0000

    datetime must be string

--- API/App.py
+++ API/App.py
@@ -7,20 +7,24 @@ app = Flask(__name__)
 api = Api(app)
 
 
-PRED_DATA = [{'lat': np.random.rand() , 'lon': np.random.rand(), 'datetime': datetime.datetime.now(), 'value': np.random.rand(), 'var': np.random.rand(1)} for i in range(272451)]
+PRED_DATA = [{'lat': np.random.rand(), 'lon': np.random.rand(), 'datetime': datetime.datetime.now().strftime(
+    "%Y-%M-%dT%H:%M:%S"), 'value': np.random.rand(), 'var': np.random.rand(1)} for i in range(272451)]
+
 
 @api.resource('/')
 class Welcome(Resource):
     def get(self):
-        
+
         return 'Welcome to the CleanAir API'
+
 
 @api.resource('/forecast')
 class Forecast(Resource):
     def get(self):
-        
+
         return PRED_DATA
 # api.add_resource(HelloWorld, '/')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
