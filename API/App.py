74892a316379363d7c49558fecb4a0e6ef26ebcc
Oscar Giles <ogiles@turing.ac.uk>
2020-01-09 15:26:55 +0000

    Send fake data

--- API/App.py
+++ API/App.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import numpy as np
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
+
+
+PRED_DATA = [{'lat': np.random.rand(1) , 'lon': np.random.rand(1), 'datetime': datetime.datetime.now(), 'value': np.random.rand(1), 'var': np.random.rand(1)} for i in range(272451)]
 
 @api.resource('/')
 class Welcome(Resource):
@@ -14,7 +19,7 @@ class Welcome(Resource):
 class Forecast(Resource):
     def get(self):
         
-        return [{'val1': 19}, {'val2': 25}]
+        return PRED_DATA
 # api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
