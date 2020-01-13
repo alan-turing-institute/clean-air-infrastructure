29bce094510dc794fb652bc512628b3eb1f4b6d9
Oliver Hamelijnck <ohamelijnck@gmail.com>
2020-01-13 15:19:46 +0000

    logging work on cluster

--- containers/cleanair/loggers/logcolours.py
+++ containers/cleanair/loggers/logcolours.py
@@ -2,14 +2,27 @@
 log colours
 """
 import os
-import termcolor
+try:
+    import termcolor
+    USE_TERMCOLOR_FLAG=True
+except:
+    USE_TERMCOLOR_FLAG=False
 
 try:
     DISABLE_COLOURS = bool(os.environ["NO_TEXT_COLOUR"])
 except KeyError:
     DISABLE_COLOURS = False
 
-
+def term_color_check(formatter):
+    def wrapper(*args, **kw):
+        if USE_TERMCOLOR_FLAG:
+            formatter(*args)
+        else:
+            #return text argument not list
+            return args[0] 
+    return wrapper
+    
+@term_color_check
 def bold(text):
     """
     Make text bold
@@ -18,7 +31,7 @@ def bold(text):
         return text
     return termcolor.colored(text, attrs=["bold"])
 
-
+@term_color_check
 def green(text):
     """
     Make text green
@@ -27,7 +40,7 @@ def green(text):
         return text
     return termcolor.colored(text, "cyan")
 
-
+@term_color_check
 def red(text):
     """
     Make text red
