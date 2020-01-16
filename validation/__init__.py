a5abb2c8cd0ba2bd4f64e15571d443c0735c4296
PatrickOHara <pohara@turing.ac.uk>
2020-01-16 11:23:13 +0000

    Updated init for validatoin.

--- validation/__init__.py
+++ validation/__init__.py
@@ -1,11 +1,9 @@
+"""
+Validation for the London air quality project.
+"""
 from . import spatial
 from . import temporal
-from . import parameters
 from . import choose_sensors
 from . import metrics
 from . import experiment
 from . import run_validation
-
-import sys
-sys.path.append("../containers")
-import cleanair
