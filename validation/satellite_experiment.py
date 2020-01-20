3dd3c38bfc5cd0e731fc1667556c7ceca6913967
PatrickOHara <pohara@turing.ac.uk>
2020-01-20 11:12:31 +0000

    Created classes for satellite experiment.

---
+++ validation/satellite_experiment.py
@@ -0,0 +1,45 @@
+"""
+A module for satellite experiments.
+"""
+
+from abc import abstractmethod
+import pandas as pd
+import experiment
+
+class SatelliteExperiment(experiment.Experiment):
+    """
+    An experiment with Satellite data.
+    """
+
+    def __init__(self, experiment_name, models, cluster_name, **kwargs):
+        super().__init__(experiment_name, models, cluster_name, **kwargs)
+        if 'data_config' not in kwargs:
+            self.data_config = self.get_default_data_config()
+        if 'experiment_df' not in kwargs:
+            self.experiment_df = self.get_default_experiment_df()
+
+    @abstractmethod
+    def get_default_model_params(self):
+        """
+        Get model parameters for an experiment on satellite data.
+        """
+
+    def get_default_experiment_df(self):
+        return pd.DataFrame()
+
+    def get_default_data_config(self):
+        return []
+    
+
+class DeepGPExperiment(SatelliteExperiment):
+    """
+    An experiment with satellite data for the deep GP.
+    """
+
+    def __init__(self, experiment_name, models, cluster_name, **kwargs):
+        super().__init__(experiment_name, models, cluster_name, **kwargs)
+        if 'model_params' not in kwargs:
+            self.model_params = self.get_default_model_params()
+
+    def get_default_model_params(self):
+        return {}
