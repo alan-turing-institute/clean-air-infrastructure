f52bb2463113805213c150239dd08ce4ec4ef8bf
PatrickOHara <pohara@turing.ac.uk>
2020-01-21 14:07:09 +0000

    Base experiment created.

--- val/manager.py
+++ val/manager.py
@@ -8,6 +8,7 @@ class ValidationParser(argparse.ArgumentParser):
         super().__init__(description="Run validation")
         self.add_argument('-s', '--setup', action='store_true', help='setup an experiment with parameters and data')
         self.add_argument('-r', '--run', action='store_true', help='train and predict a model')
+        self.add_argument('-k', '--check', action='store_true', help='check the status of a cluster')
         self.add_argument('-n', '--name', type=str, help='name of the experiment')
         self.add_argument('-c', '--cluster', type=str, help='name of the cluster')
         self.add_argument('-d', '--home_directory', type=str, help='path to home directory')
---
+++ val/validation/experiments/base.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+import pathlib
+
+class Experiment():
+    def __init__(self, experiment_name, models, cluster_name, **kwargs):
+        """
+        An abstract experiment class.
+
+        Parameters
+        ___
+
+        experiment_name : str
+            Name that identifies the experiment.
+
+        models : list
+            List of string identifying the models to run in the experiment.
+
+        cluster_name : str 
+            String to identify the cluster the experiment was ran on.
+
+        model_params : list of dicts
+            A list of model parameter configurations.
+            Each model config is a dictionary.
+
+        data_config : list of dicts
+            A list of data configurations.
+            Each config is a dictionary.
+
+        experiment_df : DataFrame
+            A dataframe describing every run in the experiment.
+        """
+        super().__init__()
+        self.name = experiment_name
+        self.models = models
+        self.cluster = cluster_name
+        self.model_params = kwargs['model_params'] if 'model_params' in kwargs else []
+        self.data_config = kwargs['data_config'] if 'data_config' in kwargs else []
+        self.experiment_df = kwargs['experiment_df'] if 'experiment_df' in kwargs else pd.DataFrame()
+        self.model_data_list = kwargs['model_data_list'] if 'model_data_list' in kwargs else []
+
+    @abstractmethod
+    def get_default_model_params(self):
+        """
+        Get the default parameters of the model for this experiment.
+        """
+
+    @abstractmethod
+    def get_default_data_config(self):
+        """
+        Get the default data configurations for this experiment.
+        """
+
+    @abstractmethod
+    def get_default_experiment_df(self):
+        """
+        Get all the default experiment configurations.
+        """
+
+    def setup(self, experiment_data_dir='experiment_data/', secret_fp="../terraform/.secrets/db_secrets.json", force_redownload=False):
+        """
+        Given an experiment create directories, data and files.
+        """
+        # create directories if they don't exist
+        exp_dir = experiment_data_dir + self.name + '/'
+        pathlib.Path(experiment_data_dir).mkdir(exist_ok=True)
+        pathlib.Path(exp_dir).mkdir(exist_ok=True)
+        pathlib.Path(exp_dir + 'results').mkdir(exist_ok=True)
+        pathlib.Path(exp_dir + 'data').mkdir(exist_ok=True)
+        pathlib.Path(exp_dir + 'meta').mkdir(exist_ok=True)
+        pathlib.Path(exp_dir + 'models').mkdir(exist_ok=True)
+        pathlib.Path(exp_dir + 'models/restore').mkdir(exist_ok=True)
+
+        # load and write model data objects to files
+        pass
+
+    def run(self):
+        """
+        Run the experiment.
+        """
+        pass
+
+    def check_status(self):
+        """
+        Check the status of an experiment on a cluster.
+        """
+        pass
+
+    def update_model_data_list(self):
+        """
+        Update the model data list.
+        """
+        pass
