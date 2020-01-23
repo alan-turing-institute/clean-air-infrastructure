7c089b834852c5cf18fbadf80a83826ad1bb38b5
PatrickOHara <pohara@turing.ac.uk>
2020-01-23 11:16:42 +0000

    Updated parser in manager.

--- val/manager.py
+++ val/manager.py
@@ -13,11 +13,12 @@ class ValidationParser(argparse.ArgumentParser):
         self.add_argument('-s', '--setup', action='store_true', help='setup an experiment with parameters and data')
         self.add_argument('-r', '--run', action='store_true', help='train and predict a model')
         self.add_argument('-k', '--check', action='store_true', help='check the status of a cluster')
+        self.add_argument('-v', '--validate', action='store_true', help='run validation methods')
         self.add_argument('-f', '--force', action='store_true', help='force download data, even it exists')
         self.add_argument('-n', '--name', type=str, help='name of the experiment')
         self.add_argument('-c', '--cluster', type=str, help='name of the cluster')
         self.add_argument('-d', '--home_directory', type=str, help='path to home directory')
-        self.add_argument('-e', '--experiments_directory', type=str, default='experiment_data', help='path to experiments directory')
+        self.add_argument('-e', '--experiments_directory', type=str, default='experiment_data/', help='path to experiments directory')
 
 def main():
     parser = ValidationParser()
@@ -26,26 +27,25 @@ def main():
 
     models = ['svgp']
     print(vars(args))
-    exp = experiment_class(args.name, models, args.cluster, directory=args.experiments_directory, **vars(args))
 
     if args.setup:
-        models = ['svgp']
+        exp = experiment_class(args.name, models, args.cluster, directory=args.experiments_directory, **vars(args))
         exp.setup(force_redownload=args.force)
-        print(exp.name)
 
-    elif args.run:
-        models = ['svgp']
-        if False:
-            exp.update_model_data_list(update_train=False, update_test=False)
-            for model_data in exp.model_data_list:
-                print(model_data.get_training_dicts()['laqn']['Y']['NO2'])
-                print(model_data.get_testing_dicts()['laqn']['Y']['NO2'])
-                print()
-        else:
-            exp.run()
+    if args.run:
+        exp = experiment_class(args.name, models, args.cluster, directory=args.experiments_directory, **vars(args))
+        exp.run()
 
-    elif args.check:
+    if args.check:
         pass
+
+    if args.validate:
+        exp = util.load_experiment_from_directory(args.name, experiment_data=args.experiments_directory)
+        exp.update_model_data_list(update_train=False, update_test=True)
+        for model_data in exp.model_data_list:
+            print(model_data.get_training_dicts()['laqn']['Y']['NO2'])
+            print(model_data.get_testing_dicts()['laqn']['Y']['NO2'])
+            print()
 
 if __name__ == "__main__":
     main()
