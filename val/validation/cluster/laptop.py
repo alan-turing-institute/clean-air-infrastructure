794bba92dafc54b29025250af4cba9f2bcf21958
Oliver Hamelijnck <ohamelijnck@gmail.com>
2020-01-23 08:56:44 +0000

    Added laptop class

--- val/validation/cluster/laptop.py
+++ val/validation/cluster/laptop.py
@@ -4,5 +4,51 @@
 
 from . import Cluster
 
+import subprocess
+
 class Laptop(Cluster):
-    pass
+    def setup(self):
+        """
+            not needed on laptop
+        """
+        pass
+
+    @property
+    def slurm_template_fp(self):
+        """
+            N/A on laptop
+        """
+
+        return None
+
+    @property
+    def default_config(self):
+        """
+            N/A on laptop
+        """
+    
+        return None
+
+    def run(self):
+        """
+            Run all experiments serially and locally
+        """
+
+        for config in self.experiment_configs:
+            model = config['filename']
+            file_name = 'm_{model}'.format(model=model)
+            file_inputs = self.input_format_fn(config)
+            file_fp = self.experiment_fp+self.experiment_name+'/models'
+            file_name=file_name+'.py'
+
+            #construct bash script to send files
+            call_array = [
+                "cd ",  file_fp, '&&',
+                "python", file_name, file_inputs
+            ]
+            script = ' '.join(call_array)
+
+            subprocess.run(script)
+
+
+
