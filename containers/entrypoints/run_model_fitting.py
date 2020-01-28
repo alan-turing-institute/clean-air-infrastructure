5f6ec8e6a6896870084b854b10da468e04e89f03
Oscar Giles <ogiles@turing.ac.uk>
2020-01-28 15:45:48 +0000

    Try with smaller mumber of features

--- containers/entrypoints/run_model_fitting.py
+++ containers/entrypoints/run_model_fitting.py
@@ -80,7 +80,7 @@ def main():
         "train_satellite_interest_points": "all",
         "pred_interest_points": "all",
         "species": ["NO2"],
-        "features": ['value_1000_avg_min_width', 'value_1000_avg_ratio_avg', 'value_1000_building_height', 'value_1000_hospitals', 'value_1000_max_min_width', 'value_1000_max_ratio_avg', 'value_1000_min_min_width', 'value_1000_min_ratio_avg', 'value_1000_museums', 'value_1000_park', 'value_1000_total_a_road_length', 'value_1000_total_a_road_primary_length', 'value_1000_total_b_road_length', 'value_1000_total_length', 'value_1000_total_road_length', 'value_1000_water', 'value_500_avg_min_width', 'value_500_avg_ratio_avg', 'value_500_building_height', 'value_500_hospitals', 'value_500_max_min_width', 'value_500_max_ratio_avg', 'value_500_min_min_width', 'value_500_min_ratio_avg', 'value_500_museums', 'value_500_park', 'value_500_total_a_road_length', 'value_500_total_a_road_primary_length', 'value_500_total_b_road_length', 'value_500_total_length', 'value_500_total_road_length', 'value_500_water'],
+        "features": ['value_1000_total_a_road_length', 'value_500_total_a_road_length', 'value_500_total_a_road_primary_length', 'value_500_total_b_road_length'],
         "norm_by": "laqn",
         "model_type": "svgp",
         "tag": "full_test",
@@ -99,7 +99,6 @@ def main():
     # model_data = ModelData(config_dir='/secrets/test/', **kwargs)
 
     model_data.save_config_state('/run_model_test/')
-    # quit()
 
     # # training_data_dict = model_data.training_data_df
     training_data_dict = model_data.get_training_data_arrays(dropna=True)
