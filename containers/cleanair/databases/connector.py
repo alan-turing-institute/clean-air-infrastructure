518ff94b5197e6a93bded924198451d0b5f48b0b
Oscar Giles <ogiles@turing.ac.uk>
2020-01-30 14:45:59 +0000

    Refactor out connection string generator into a mixin

--- containers/cleanair/databases/connector.py
+++ containers/cleanair/databases/connector.py
@@ -13,9 +13,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.schema import CreateSchema
 from ..loggers import get_logger, green, red
 from .base import Base
+from ..mixins import DBConnectionMixin
 
 
-class Connector:
+class Connector(DBConnectionMixin):
     """
     Base class for connecting to databases with sqlalchemy
     """
@@ -24,12 +25,13 @@ class Connector:
     __sessionfactory = None
 
     def __init__(self, secretfile):
+
+        # Pass unused arguments onwards
+        super().__init__(secretfile)
+
         # Ensure logging is available
         if not hasattr(self, "logger"):
             self.logger = get_logger(__name__)
-
-        # Get database connection string
-        self.connection_info = self.load_connection_info(secretfile)
 
         # Avoid repeated internet tests
         self.last_successful_connection = None
@@ -41,75 +43,14 @@ class Connector:
         # Next create all other tables
         Base.metadata.create_all(self.engine, checkfirst=True)
 
-    def load_connection_info(self, secret_file):
-        """
-        Loads database secrets from a json file.
-        Check file system is accessable from docker and return database login info
-        """
-
-        # Attempt to open assuming secret_file is full path
-        try:
-            with open(secret_file) as f_secret:
-                data = json.load(f_secret)
-                self.logger.info(
-                    "Database connection information loaded from %s", green(f_secret)
-                )
-                return data
-
-        except FileNotFoundError:
-
-            # Construct available secrets files
-            secrets_directories = ["/secrets"]
-            secrets_files = [
-                f
-                for f in [os.path.join(s, secret_file) for s in secrets_directories]
-                if os.path.isfile(f)
-            ]
-
-            # Check that at least one can be seen
-            if not secrets_files:
-                raise FileNotFoundError(
-                    "{} could not be found. Have you mounted the directory?".format(
-                        secret_file
-                    )
-                )
-
-            # Attempt to load secrets from each available file in turn
-            for secret_fname in secrets_files:
-                try:
-                    with open(secret_fname) as f_secret:
-                        data = json.load(f_secret)
-                    self.logger.info(
-                        "Database connection information loaded from %s",
-                        green(secret_fname),
-                    )
-                    return data
-                except json.decoder.JSONDecodeError:
-                    self.logger.debug(
-                        "Database connection information could not be loaded from %s",
-                        red(secret_fname),
-                    )
-
-            raise FileNotFoundError(
-                "Database secrets could not be loaded from {}".format(secret_file)
-            )
-
     @property
     def engine(self):
         """Access the class-level sqlalchemy engine"""
         # Initialise the class-level engine if it does not already exist
         if not self.__engine:
             # See here (https://www.postgresql.org/docs/11/libpq-connect.html) for keepalive documentation
-            connection_dict = {
-                "options": "keepalives=1&keepalives_idle=10",
-                **self.connection_info,
-            }
-            self.__engine = create_engine(
-                "postgresql://{username}:{password}@{host}:{port}/{db_name}?{options}".format(
-                    **connection_dict
-                ),
-                pool_pre_ping=True,
-            )
+
+            self.__engine = create_engine(self.connection_string, pool_pre_ping=True,)
             self.__sessionfactory = sessionmaker(bind=self.__engine)
         # Return the class-level engine
         return self.__engine
