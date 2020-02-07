1e6b7abd85cdb7d298505c5e2af2c0a5c4255c2a
PatrickOHara <pohara@turing.ac.uk>
2020-02-07 17:04:55 +0000

    Need to fix bootstrap

--- containers/cleanair/dashboard/apps.py
+++ containers/cleanair/dashboard/apps.py
@@ -6,7 +6,6 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-import plotly.graph_objects as go
 import plotly.express as px
 from dash.dependencies import Input, Output
 from . import components
