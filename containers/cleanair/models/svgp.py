a0598d397c5ca6862a76be119a2d121bf776cf1c
PatrickOHara <pohara@turing.ac.uk>
2020-02-10 16:46:25 +0000

    Predicting over multiple sources.

--- containers/cleanair/models/svgp.py
+++ containers/cleanair/models/svgp.py
@@ -231,7 +231,7 @@ class SVGP_TF1(Model):
 
         return ys, ys_var
 
-    def predict(self, x_test):
+    def predict(self, x_test, species=['NO2']):
         """
         Predict using the model at the laqn sites for NO2.
 
@@ -241,10 +241,20 @@ class SVGP_TF1(Model):
         x_test : dict
             See `Model.predict` for further details.
         """
+        if species != ['NO2']:
+            raise NotImplementedError("Multiple pollutants not supported. Use only NO2.")
         self.check_test_set_is_valid(x_test)
-        x_test = x_test["laqn"]
-        y_mean, y_var = self.batch_predict(x_test)
-        return dict(laqn=dict(NO2=dict(mean=y_mean, var=y_var)))
+        y_dict = dict()
+        for src, x_src in x_test.items():
+            for pollutant in species:
+                y_mean, y_var = self.batch_predict(x_src)
+                y_dict[src] = {
+                    pollutant: dict(
+                        mean=y_mean,
+                        var=y_var
+                    )
+                }
+        return y_dict
 
     def clean_data(self, x_array, y_array):
         """Remove nans and missing data for use in GPflow
