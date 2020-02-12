0ff295d0fd39dc391cbc3431316c1c4b589ec8d3
James Robinson <james.em.robinson@gmail.com>
2020-02-12 14:22:21 +0000

    :recycle: Refactored decorators

--- containers/cleanair/decorators/api.py
+++ containers/cleanair/decorators/api.py
@@ -1,8 +1,7 @@
-"""Clean air decorators"""
+"""API decorators"""
 import functools
 import time
 import logging
-import pandas as pd
 import requests
 from ..loggers import get_logger
 
@@ -37,32 +36,3 @@ def robust_api(api_call):
                     raise requests.exceptions.HTTPError
 
     return robust_api_output
-
-
-def db_query(query_f):
-    """Wrapper for functions that return an sqlalchemy query object.
-    kwargs:
-        output_type: Either 'query', 'subquery', 'df' or 'list'. list returns the first column of the query
-    """
-
-    @functools.wraps(query_f)
-    def db_query_output(*args, output_type="query", **kwargs):
-
-        output_q = query_f(*args, **kwargs)
-
-        if output_type == "df":
-            return pd.read_sql(output_q.statement, output_q.session.bind)
-
-        if output_type == "list":
-            query_df = pd.read_sql(output_q.statement, output_q.session.bind)
-            return query_df[query_df.columns[0]].tolist()
-
-        if output_type == "subquery":
-            return output_q.subquery()
-
-        if output_type == "sql":
-            return output_q.statement.compile(compile_kwargs={"literal_binds": True})
-
-        return output_q
-
-    return db_query_output
