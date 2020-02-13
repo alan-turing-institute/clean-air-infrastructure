a73acdd1ffb777348d213aff2bb098bec183d03e
PatrickOHara <pohara@turing.ac.uk>
2020-02-13 13:12:44 +0000

    Initial commit for tests

---
+++ containers/tests/model_data_test.py
@@ -0,0 +1,9 @@
+"""
+Given a model data object, check that the data matches the config.
+"""
+
+def check_shapes(model_data):
+    """
+    Check the shape of all the numpy arrays are correct.
+    """
+    raise NotImplementedError()
---
+++ containers/tests/model_fit_test.py
@@ -0,0 +1,9 @@
+"""
+Check a model has fit the data correctly.
+"""
+
+def check_y_pred(model_data, y_pred):
+    """
+    Check that `y_pred` is in the correct shape.
+    """
+    raise NotImplementedError()
