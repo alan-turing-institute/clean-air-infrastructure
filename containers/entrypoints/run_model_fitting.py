2a67532bcd93e1189a3aaa5596b2bd973bfde478
PatrickOHara <pohara@turing.ac.uk>
2020-02-14 13:57:57 +0000

    Created parser object and run pylint

--- containers/entrypoints/run_model_fitting.py
+++ containers/entrypoints/run_model_fitting.py
@@ -8,107 +8,69 @@ from dateutil.relativedelta import relativedelta
 from cleanair.models import ModelData, SVGP
 from cleanair.loggers import get_log_level
 
+class ModelFitParser(argparse.ArgumentParser):
+    """
+    A parser for the model fitting entrypoint.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_argument(
+            "-s",
+            "--secretfile",
+            default="../../terraform/.secrets/db_secrets.json",
+            help="File with connection secrets.",
+        )
+        self.add_argument(
+            "-d",
+            "--config_dir",
+            default="./",
+            help="Filepath to directory to store model and data.",
+        )
+        self.add_argument(
+            "-w", "--write", action="store_true", help="Write model data config to file.",
+        )
+        self.add_argument(
+            "-r", "--read", action="store_true", help="Read model data from config_dir.",
+        )
+        self.add_argument(
+            "-u",
+            "--update",
+            action="store_true",
+            help="Update the database with model results.",
+        )
+        self.add_argument("-v", "--verbose", action="count", default=0)
+
+        self.add_argument(
+            "--trainend",
+            type=str,
+            default="2020-01-30T00:00:00",
+            help="The last datetime (YYYY-MM-DD HH:MM:SS) to get model data for training.",
+        )
+        self.add_argument(
+            "--trainhours",
+            type=int,
+            default=48,
+            help="The number of hours to get training data for.",
+        )
+        self.add_argument(
+            "--predstart",
+            type=str,
+            default="2020-01-30T00:00:00",
+            help="The first datetime (YYYY-MM-DD HH:MM:SS) to get model data for prediction.",
+        )
+        self.add_argument(
+            "--predhours", type=int, default=48, help="The number of hours to predict for"
+        )
 
 def strtime_offset(strtime, offset_hours):
     """Give an datetime as an iso string and an offset and return a new time"""
 
     return (isoparse(strtime) + relativedelta(hours=offset_hours)).isoformat()
 
-
-def model_data_tests(model_data, y_pred):
+def get_data_config(**kwargs):
     """
-    Check a model data fit is as expected.
+    Return a dictionary of model data configs given parser arguments.
     """
-    training_data_dict = model_data.get_training_data_arrays(dropna=False)
-    predict_data_dict = model_data.get_pred_data_arrays(dropna=False, return_y=True)
-    x_train = training_data_dict["X"]
-    y_train = training_data_dict["Y"]
-    x_test = predict_data_dict["X"]
-
-    # checks for satellite
-    assert "satellite" not in x_test
-    assert not "include_satellite" in model_data.config or "satellite" in x_train
-
-    # check the shape of x_test and y_pred
-    try:
-        num_pred_rows = y_pred["laqn"]["NO2"]["mean"].shape[0]
-        num_x_rows = x_test["laqn"].shape[0]
-        assert num_pred_rows == num_x_rows
-    except AssertionError:
-        error_message = "Rows in y_pred laqn No2 mean is {pred_rows}. ".format(
-            pred_rows=num_pred_rows
-        )
-        error_message += "Rows in x_test laqn is {x_rows}. ".format(x_rows=num_x_rows)
-        error_message += "The number of rows in both arrays should be the same."
-        raise ValueError(error_message)
-
-
-def main():
-    """
-    Run model fitting
-    """
-    # Read command line arguments
-    parser = argparse.ArgumentParser(description="Run model fitting")
-    parser.add_argument(
-        "-s",
-        "--secretfile",
-        default="../../terraform/.secrets/db_secrets.json",
-        help="File with connection secrets.",
-    )
-    parser.add_argument(
-        "-d",
-        "--config_dir",
-        default="./",
-        help="Filepath to directory to store model and data.",
-    )
-    parser.add_argument(
-        "-w", "--write", action="store_true", help="Write model data config to file.",
-    )
-    parser.add_argument(
-        "-r", "--read", action="store_true", help="Read model data from config_dir.",
-    )
-    parser.add_argument(
-        "-u",
-        "--update",
-        action="store_true",
-        help="Update the database with model results.",
-    )
-    parser.add_argument("-v", "--verbose", action="count", default=0)
-
-    parser.add_argument(
-        "--trainend",
-        type=str,
-        default="2020-01-30T00:00:00",
-        help="The last datetime (YYYY-MM-DD HH:MM:SS) to get model data for training.",
-    )
-    parser.add_argument(
-        "--trainhours",
-        type=int,
-        default=48,
-        help="The number of hours to get training data for.",
-    )
-    parser.add_argument(
-        "--predstart",
-        type=str,
-        default="2020-01-30T00:00:00",
-        help="The first datetime (YYYY-MM-DD HH:MM:SS) to get model data for prediction.",
-    )
-    parser.add_argument(
-        "--predhours", type=int, default=48, help="The number of hours to predict for"
-    )
-
-    # Parse and interpret arguments
-    args = parser.parse_args()
-    kwargs = vars(args)
-
-    # Update database/write to file
-    update = kwargs.pop("update")
-    write = kwargs.pop("write")
-    read = kwargs.pop("read")
-
-    # Set logging verbosity
-    logging.basicConfig(level=get_log_level(kwargs.pop("verbose", 0)))
-
     # Get training and pred start and end datetimes
     train_end = kwargs.pop("trainend")
     train_n_hours = kwargs.pop("trainhours")
@@ -141,6 +103,29 @@ def main():
         "model_type": "svgp",
         "tag": "tf1_test",
     }
+    return model_config
+
+def main():
+    """
+    Run model fitting
+    """
+    # Read command line arguments
+    parser = ModelFitParser(description="Run model fitting")
+
+    # Parse and interpret arguments
+    args = parser.parse_args()
+    kwargs = vars(args)
+
+    # Update database/write to file
+    update = kwargs.pop("update")
+    write = kwargs.pop("write")
+    read = kwargs.pop("read")
+
+    # Set logging verbosity
+    logging.basicConfig(level=get_log_level(kwargs.pop("verbose", 0)))
+
+    # get the model config from the parser arguments
+    model_config = get_data_config(**kwargs)
 
     if "aqe" in model_config["train_sources"] + model_config["pred_sources"]:
         raise NotImplementedError("AQE cannot currently be run. Coming soon")
@@ -180,9 +165,6 @@ def main():
 
     # Do prediction
     y_pred = model_fitter.predict(x_test)
-
-    # Check the model data fit is as expected
-    model_data_tests(model_data, y_pred)
 
     # Internally update the model results in the ModelData object
     model_data.update_test_df_with_preds(y_pred)
