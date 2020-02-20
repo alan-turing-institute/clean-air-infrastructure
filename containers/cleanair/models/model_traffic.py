aa3cffade7bc120714caa085165d6b650a3b4576
James Robinson <james.em.robinson@gmail.com>
2020-02-20 16:32:30 +0000

    :zap: Updated forecasting to use multiprocessing

--- containers/cleanair/models/model_traffic.py
+++ containers/cleanair/models/model_traffic.py
@@ -1,14 +1,14 @@
 """Traffic model fitting"""
 import datetime
-from functools import reduce
+from functools import partial, reduce
 import logging
 import time
-import warnings
+from pathos.multiprocessing import ProcessingPool as Pool
 from fbprophet import Prophet
 import pandas as pd
 from sqlalchemy.exc import IntegrityError
 from ..databases import DBWriter
-from ..databases.tables import ScootDetector, ScootReading, ScootForecast
+from ..databases.tables import ScootReading, ScootForecast
 from ..decorators import SuppressStdoutStderr
 from ..loggers import duration, get_logger, green
 from ..mixins import DateRangeMixin
@@ -20,7 +20,7 @@ logging.getLogger("fbprophet").setLevel(logging.ERROR)
 class TrafficForecast(DateRangeMixin, DBWriter):
     """Traffic forecasting using FB prophet"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, forecast_length_hrs=72, detector_ids=None, **kwargs):
         # Initialise parent classes
         super().__init__(**kwargs)
 
@@ -35,7 +35,16 @@ class TrafficForecast(DateRangeMixin, DBWriter):
         if not hasattr(self, "logger"):
             self.logger = get_logger(__name__)
 
-    def scoot_readings(self, detector_ids=None):
+        self.detector_ids = detector_ids
+        self.forecast_length_hrs = forecast_length_hrs
+
+        # The end of the forecast will happen `self.forecast_length_hrs` into the future
+        self.forecast_end_time = datetime.datetime.now().replace(
+            second=0, microsecond=0, minute=0
+        ) + datetime.timedelta(hours=self.forecast_length_hrs)
+
+
+    def scoot_readings(self):
         """Get SCOOT readings between start and end times"""
         self.logger.info(
             "Requesting SCOOT readings between %s and %s",
@@ -49,9 +58,9 @@ class TrafficForecast(DateRangeMixin, DBWriter):
                 ScootReading.measurement_start_utc >= self.start_datetime,
                 ScootReading.measurement_start_utc < self.end_datetime,
             )
-            if detector_ids:
+            if self.detector_ids:
                 scoot_road_q = scoot_road_q.filter(
-                    ScootReading.detector_id.in_(detector_ids)
+                    ScootReading.detector_id.in_(self.detector_ids)
                 )
             scoot_road_q = scoot_road_q.order_by(
                 ScootReading.detector_id, ScootReading.measurement_start_utc
@@ -64,23 +73,22 @@ class TrafficForecast(DateRangeMixin, DBWriter):
         )
         return readings
 
-    def forecasts(self, forecast_length_hrs, detector_ids=None):
-        """Forecast all features at each detector for the next `forecast_length_hrs`"""
-        # Get SCOOT readings from database
-        readings = self.scoot_readings(detector_ids=detector_ids)
+    def forecasts(self):
+        """Forecast all features at each detector up until `self.forecast_end_time`"""
+        # Get all SCOOT readings within the relevant time period from database and group them by detector ID
+        readings = self.scoot_readings()
         training_data = dict(tuple(readings.groupby(["detector_id"])))
 
-        if readings.shape[0] == 0:
+        if not readings.shape[0]:
             self.logger.error("Could not load any readings!")
             return pd.DataFrame([])
 
-        # The end of the forecast will happen `forecast_length_hrs` into the future
-        forecast_end_time = datetime.datetime.now().replace(
-            second=0, microsecond=0, minute=0
-        ) + datetime.timedelta(hours=forecast_length_hrs)
         self.logger.info(
-            "Forecasting SCOOT traffic data up until %s...", forecast_end_time
+            "Forecasting SCOOT traffic data up until %s...", self.forecast_end_time
         )
+
+        # Setup a pool to allow us to process all features in parallel
+        processing_pool = Pool(len(self.features))
 
         # Iterate over each detector ID and obtain the forecast for it
         for idx, (detector_id, fit_data) in enumerate(training_data.items(), start=1):
@@ -94,36 +102,44 @@ class TrafficForecast(DateRangeMixin, DBWriter):
             time_range = pd.DataFrame(
                 {
                     "ds": pd.date_range(
-                        start=last_reading_time, end=forecast_end_time, freq="H"
+                        start=last_reading_time, end=self.forecast_end_time, freq="H"
                     )[1:]
                 }
             )
 
-            # Iterate over each feature and obtain the forecast for it
-            for feature in self.features:
+            # Obtain the forecast for a single feature
+            def forecast_one_feature(
+                feature,
+                _logger,
+                _fit_data,
+                _time_range,
+                _last_reading_time,
+                _detector_id,
+            ):
                 # Set the maximum value for the fit - this requires the use of logistic
                 # regression in the fit itself
                 capacity = (
-                    100 if "percentage" in feature else 10 * max(fit_data[feature])
+                    100 if "percentage" in feature else 10 * max(_fit_data[feature])
                 )
-                # Construct the prophet model using the fit data
-                prophet_data = fit_data.rename(
+                # Construct the prophet model using a copy of the fit data
+                prophet_data = _fit_data.rename(
                     columns={"measurement_start_utc": "ds", feature: "y"}
                 )
                 prophet_data["cap"] = capacity
                 try:
+                    # Fit model while suppressing Stan output lines
                     with SuppressStdoutStderr():
                         model = Prophet(growth="logistic").fit(prophet_data)
                     # Predict past and future readings for this feature
-                    time_range["cap"] = capacity
-                    forecast = model.predict(time_range)[["ds", "yhat"]]
+                    _time_range["cap"] = capacity
+                    forecast = model.predict(_time_range)[["ds", "yhat"]]
                 except (ValueError, RuntimeError):
-                    self.logger.error(
+                    _logger.error(
                         "Prophet prediction failed at '%s' for %s. Using zero instead.",
-                        detector_id,
+                        _detector_id,
                         feature,
                     )
-                    forecast = time_range.copy()
+                    forecast = _time_range.copy()
                     forecast["yhat"] = 0
 
                 # Rename the columns
@@ -133,10 +149,22 @@ class TrafficForecast(DateRangeMixin, DBWriter):
                 )
                 # Keep only future predictions and force all predictions to be positive
                 forecast = forecast[
-                    forecast["measurement_start_utc"] > last_reading_time
+                    forecast["measurement_start_utc"] > _last_reading_time
                 ]
                 forecast[feature].clip(lower=0, inplace=True)
-                feature_predictions.append(forecast)
+                # feature_predictions.append(forecast)
+                return forecast
+
+            bound_forecast = partial(
+                forecast_one_feature,
+                _logger=self.logger,
+                _fit_data=fit_data,
+                _time_range=time_range,
+                _last_reading_time=last_reading_time,
+                _detector_id=detector_id,
+            )
+            feature_predictions = processing_pool.map(bound_forecast, self.features)
+
             # Combine predicted features and add detector ID
             combined_predictions = reduce(
                 lambda df1, df2: df1.merge(df2, on="measurement_start_utc"),
@@ -153,21 +181,22 @@ class TrafficForecast(DateRangeMixin, DBWriter):
                 len(training_data),
                 green(duration(start_time, time.time())),
             )
-            yield combined_predictions
+            yield (detector_id, combined_predictions)
 
-    def update_remote_tables(self, detector_ids=None):
+    def update_remote_tables(self):
         """Update the database with new Scoot traffic forecasts."""
         self.logger.info("Starting %s forecasts update...", green("SCOOT"))
         start_time = time.time()
         n_records = 0
 
         # Get the forecasts and convert to a list of dictionaries
-        for forecast_df in self.forecasts(forecast_length_hrs=72, detector_ids=detector_ids):
+        for detector_id, forecast_df in self.forecasts():
             forecast_records = forecast_df.to_dict("records")
             if len(forecast_records) > 0:
                 self.logger.info(
-                    "Preparing to insert %s forecasts into database",
+                    "Preparing to insert %s hourly forecasts for %s into database",
                     green(len(forecast_records)),
+                    detector_id,
                 )
 
                 # Add forecasts to the database
