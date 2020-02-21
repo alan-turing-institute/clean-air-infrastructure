bb19d7cb05d65b68a2eb7ab38e02fb1b22ca4cf2
Ollie <ohamelijnck@gmail.com>
2020-02-21 14:55:31 +0000

    small linting update

--- containers/cleanair/models/mr_dgp_model.py
+++ containers/cleanair/models/mr_dgp_model.py
@@ -10,6 +10,8 @@ import gpflow
 from gpflow import settings
 from gpflow.training import AdamOptimizer
 
+from scipy.cluster.vq import kmeans2
+
 from .mr_dgp import MR_DGP
 from .mr_dgp import MR_Mixture
 from .mr_dgp import MR_SE, MR_Linear, MR_KERNEL_PRODUCT
@@ -23,8 +25,6 @@ from .mr_dgp.mr_mixing_weights import (
 )
 
 from .mr_dgp.utils import set_objective
-
-from scipy.cluster.vq import kmeans2
 
 from ..loggers import get_logger
 from .model import Model
@@ -82,19 +82,24 @@ class MRDGP(Model):
         return {"restore": False, "train": True, "model_state_fp": ""}
 
     def get_kernel_product(
-        self, K, active_dims=[0], lengthscales=[1.0], variances=[1.0], name=""
+        self, kernels, active_dims=None, lengthscales=None, variances=None, name=""
     ):
         """
             Returns a product kernel across all input dimensions
         """
-        if not isinstance(K, list):
-            K = [K for i in range(len(active_dims))]
+        #set default arguments for None arguments
+        active_dims = active_dims if not None else [0]
+        lengthscales = lengthscales if not None else [1.0]
+        variances = variances if not None else [1.0]
 
-        kernels = []
-        for i, k in enumerate(K):
-            if (lengthscales is None) or (k is MR_Linear):
-                kernels.append(
-                    K[i](
+        if not isinstance(kernels, list):
+            kernels = [kernels for i in range(len(active_dims))]
+
+        kernels_objs = []
+        for i, kernel in enumerate(kernels):
+            if (lengthscales is None) or (kernel is MR_Linear):
+                kernels_objs.append(
+                    kernels[i](
                         input_dim=1,
                         variance=variances[i],
                         active_dims=[active_dims[i]],
@@ -102,8 +107,8 @@ class MRDGP(Model):
                     )
                 )
             else:
-                kernels.append(
-                    K[i](
+                kernels_objs.append(
+                    kernels[i](
                         input_dim=1,
                         lengthscales=lengthscales[i],
                         variance=variances[i],
@@ -112,7 +117,7 @@ class MRDGP(Model):
                     )
                 )
 
-        return gpflow.kernels.Product(kernels, name=name + "_product")
+        return gpflow.kernels.Product(kernels_objs, name=name + "_product")
 
     def get_inducing_points(self, X, num_z=None):
         """
@@ -122,10 +127,10 @@ class MRDGP(Model):
             X = X.reshape([X.shape[0] * X.shape[1], X.shape[2]])
 
         if num_z is None or num_z > X.shape[0]:
-            Z = X
+            z_inducing_locations = X
         else:
-            Z = kmeans2(X, num_z, minit="points")[0]
-        return Z
+            z_inducing_locations = kmeans2(X, num_z, minit="points")[0]
+        return z_inducing_locations
 
     def make_mixture(self, dataset, parent_mixtures=None, name_prefix=""):
         """
@@ -134,7 +139,8 @@ class MRDGP(Model):
         base_kernel_ad = range(dataset[0][0].shape[-1] - 1)
         base_kernel_ls = [0.1 for i in base_kernel_ad]
         base_kernel_v = [1.0 for i in base_kernel_ad]
-        K_base_1 = self.get_kernel_product(
+
+        k_base_1 = self.get_kernel_product(
             MR_SE,
             active_dims=base_kernel_ad,
             lengthscales=base_kernel_ls,
@@ -146,7 +152,7 @@ class MRDGP(Model):
         sat_kernel_ls = [1.0, 0.1, 0.1, 0.1, 0.1]
         sat_kernel_v = [1.0, 1.0, 1.0, 1.0, 1.0]
 
-        K_base_2 = self.get_kernel_product(
+        k_base_2 = self.get_kernel_product(
             MR_SE,
             active_dims=sat_kernel_ad,
             lengthscales=sat_kernel_ls,
@@ -158,17 +164,17 @@ class MRDGP(Model):
         dgp_kernel_ls = [1.0, 0.1, 0.1, 0.1, 0.1]
         dgp_kernel_v = [1.0, 1.0, 1.0, 1.0, 0.1]
 
-        K_dgp_1 = self.get_kernel_product(
+        k_dgp_1 = self.get_kernel_product(
             [MR_Linear, MR_SE, MR_SE],
             active_dims=dgp_kernel_ad,
             lengthscales=dgp_kernel_ls,
             variances=dgp_kernel_v,
             name=name_prefix + "MR_SE_DGP_1",
         )
-        K_parent_1 = None
+        k_parent_1 = None
 
         num_z = 300
-        base_Z = [
+        base_z_inducing_locations = [
             self.get_inducing_points(dataset[0][0], num_z),
             self.get_inducing_points(dataset[1][0], num_z),
         ]
@@ -176,31 +182,29 @@ class MRDGP(Model):
         sliced_dataset = np.concatenate(
             [np.expand_dims(dataset[0][0][:, 0, i], -1) for i in [1, 2]], axis=1
         )
-        dgp_Z = self.get_inducing_points(
+        dgp_z_inducing_locations = self.get_inducing_points(
             np.concatenate([dataset[0][1], sliced_dataset], axis=1), num_z
         )
 
-        def insert(D, col, i):
+        def insert(data, col, i):
             col = np.expand_dims(col, -1)
-            d_1 = D[:, :i]
-            d_2 = D[:, i:]
-            print(d_1.shape)
-            print(d_2.shape)
+            d_1 = data[:, :i]
+            d_2 = data[:, i:]
             return np.concatenate([d_1, col, d_2], axis=1)
 
-        dgp_Z = insert(dgp_Z, np.ones([dgp_Z.shape[0]]), 1)
+        dgp_z_inducing_locations = insert(dgp_z_inducing_locations, np.ones([dgp_z_inducing_locations.shape[0]]), 1)
 
-        dgp_Z = [dgp_Z]
-        parent_Z = dgp_Z
+        dgp_z_inducing_locations = [dgp_z_inducing_locations]
+        parent_z_inducing_locations = dgp_z_inducing_locations
 
-        inducing_points = [base_Z, dgp_Z, parent_Z]
+        inducing_points = [base_z_inducing_locations, dgp_z_inducing_locations, parent_z_inducing_locations]
         noise_sigmas = [[1.0, 1.0], [1.0], [1.0]]
         minibatch_sizes = [100, 100]
 
-        m = MR_Mixture(
+        model = MR_Mixture(
             datasets=dataset,
             inducing_locations=inducing_points,
-            kernels=[[K_base_1, K_base_2], [K_dgp_1], [K_parent_1]],
+            kernels=[[k_base_1, k_base_2], [k_dgp_1], [k_parent_1]],
             noise_sigmas=noise_sigmas,
             minibatch_sizes=minibatch_sizes,
             mixing_weight=MR_DGP_Only(),
@@ -211,45 +215,44 @@ class MRDGP(Model):
             name=name_prefix + "MRDGP",
         )
 
-        return m
+        return model
 
-    def fit(self, x_train, y_train, refresh=10, save_model_state=True):
+    def fit(self, x_train, y_train, save_model_state=True):
+        """
+            Fit MR_DGP to the multi resolution x_train and y_train
+        """
 
-        X_laqn = x_train["laqn"].copy()
-        Y_laqn = y_train["laqn"]["NO2"].copy()
+        x_laqn = x_train["laqn"].copy()
+        y_laqn = y_train["laqn"]["NO2"].copy()
         # ===========================Setup SAT Data===========================
-        X_sat = x_train["satellite"].copy()
-        Y_sat = y_train["satellite"]["NO2"].copy()
+        x_sat = x_train["satellite"].copy()
+        y_sat = y_train["satellite"]["NO2"].copy()
 
         # ===========================Only Lat/Lon/Epochs===========================
-        X_laqn = X_laqn[:, :3]
-        X_sat = X_sat[:, :, :3]
+        x_laqn = x_laqn[:, :3]
+        x_sat = x_sat[:, :, :3]
 
         # ===========================Setup Data===========================
         # X = [X_laqn[:, None, :], X_sat]
         # Y = [Y_laqn, Y_sat]
 
-        X_sat, Y_sat = Model.clean_data(X_sat, Y_sat)
-        X_laqn, Y_laqn = Model.clean_data(X_laqn, Y_laqn)
+        x_sat, y_sat = Model.clean_data(x_sat, y_sat)
+        x_laqn, y_laqn = Model.clean_data(x_laqn, y_laqn)
 
-        X = [X_sat, X_laqn[:, None, :]]
-        Y = [Y_sat, Y_laqn]
+        X = [x_sat, x_laqn[:, None, :]]
+        Y = [y_sat, y_laqn]
 
         # ===========================Setup Model===========================
         dataset = [[X[1], Y[1]], [X[0], Y[0]]]
-        m1 = self.make_mixture(dataset, name_prefix="m1_")
+        model_dgp = self.make_mixture(dataset, name_prefix="m1_")
         tf.local_variables_initializer()
         tf.global_variables_initializer()
-        m1.compile()
-        m = m1
-        self.model = m
+        model_dgp.compile()
+        self.model = model_dgp
 
         tf_session = self.model.enquire_session()
 
         # ===========================Train===========================
-
-        elbos = []
-
         if self.model_params["restore"]:
             saver = tf.train.Saver()
             saver.restore(
@@ -263,22 +266,23 @@ class MRDGP(Model):
         try:
             if self.model_params["train"]:
                 opt = AdamOptimizer(0.1)
+                simple_optimizing_scheme = False
 
-                if False:
+                if not simple_optimizing_scheme:
                     set_objective(AdamOptimizer, "base_elbo")
-                    opt.minimize(m, step_callback=self.elbo_logger, maxiter=1000)
+                    opt.minimize(self.model, step_callback=self.elbo_logger, maxiter=1000)
 
                     # m.disable_base_elbo()
                     # set_objective(AdamOptimizer, 'elbo')
                     # opt.minimize(m, step_callback=logger, maxiter=10)
                 else:
-                    opt.minimize(m, step_callback=self.elbo_logger, maxiter=1)
+                    opt.minimize(self.model, step_callback=self.elbo_logger, maxiter=1)
         except KeyboardInterrupt:
             print("Ending early")
 
         if save_model_state:
             saver = tf.train.Saver()
-            save_path = saver.save(
+            saver.save(
                 tf_session,
                 "{folder}/restore/{name}.ckpt".format(
                     folder=self.experiment_config["model_state_fp"],
@@ -287,17 +291,23 @@ class MRDGP(Model):
             )
 
     def _predict(self, x_test):
-        ys, ys_var = self.model.predict_y_experts(x_test, 1)
-        ys, ys_var = get_sample_mean_var(ys, ys_var)
-        return ys, ys_var
+        ys_mean, ys_var = self.model.predict_y_experts(x_test, 1)
+        ys_mean, ys_var = get_sample_mean_var(ys_mean, ys_var)
+        return ys_mean, ys_var
 
-    def predict(self, x_test, species=["NO2"], ignore=[]):
+    def predict(self, x_test, species=None, ignore=None):
+        species = species if species is not None else ['NO2']
+        ignore = ignore if ignore is not None else None
+
         return self.predict_srcs(x_test, self._predict, ignore=ignore)
 
 
-def get_sample_mean_var(ys, vs):
-    ys = ys[:, :, 0, :]
-    vs = vs[:, :, 0, :]
-    mu = np.mean(ys, axis=0)
-    sig = np.mean(vs + ys ** 2, axis=0) - np.mean(ys, axis=0) ** 2
-    return mu, sig
+def get_sample_mean_var(ys_mean, ys_var):
+    """
+        The DGP samples the predictive distribution. Return mean and variance of these.
+    """
+    ys_mean_samples = ys_mean[:, :, 0, :]
+    ys_var_samples = ys_var[:, :, 0, :]
+    ys_mean = np.mean(ys_mean_samples, axis=0)
+    ys_sig = np.mean(ys_var_samples + ys_mean_samples ** 2, axis=0) - np.mean(ys_mean_samples, axis=0) ** 2
+    return ys_mean, ys_sig
