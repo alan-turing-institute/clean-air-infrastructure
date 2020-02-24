46f8c2cfa3de46c2b12425d259d1feced4f6449b
James Robinson <james.em.robinson@gmail.com>
2020-02-24 11:16:31 +0000

    :recycle: Moved forecasting into processors

--- containers/cleanair/models/__init__.py
+++ containers/cleanair/models/__init__.py
@@ -1,12 +1,10 @@
 """Model fitting classes"""
 from .model import Model
 from .model_data import ModelData
-from .model_traffic import TrafficForecast
 from .svgp import SVGP
 
 __all__ = [
     "Model",
     "ModelData",
     "SVGP",
-    "TrafficForecast",
 ]
--- containers/cleanair/processors/__init__.py
+++ containers/cleanair/processors/__init__.py
@@ -2,9 +2,13 @@
 Module for feature extraction
 """
 from .scoot_forecast_mapping import ScootForecastMapper
+from .scoot_forecaster import ScootForecaster
+from .scoot_reading_mapping import ScootReadingMapper
 from .scoot_road_mapping import ScootRoadMapper
 
 __all__ = [
+    "ScootForecaster",
     "ScootForecastMapper",
+    "ScootReadingMapper",
     "ScootRoadMapper",
 ]
