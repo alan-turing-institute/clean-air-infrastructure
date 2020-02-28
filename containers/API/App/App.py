d0d26f95b1785b3a55287b0cd6c7ce536101a58d
Oscar Giles <ogiles@turing.ac.uk>
2020-02-28 17:52:44 +0000

    Remove secret path

--- containers/API/App/App.py
+++ containers/API/App/App.py
@@ -18,9 +18,7 @@ ma = Marshmallow(app)
 api = Api(app)
 
 # Configure session
-DB_CONNECTION_INFO = DBConnectionMixin(
-    "/Users/ogiles/Documents/project_repos/clean-air-infrastructure/terraform/.secrets/db_secrets.json"
-)
+DB_CONNECTION_INFO = DBConnectionMixin("db_secrets.json")
 engine = create_engine(DB_CONNECTION_INFO.connection_string, convert_unicode=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
