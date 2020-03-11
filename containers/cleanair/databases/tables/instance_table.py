544d6e4a65763051af2518b0dce7c050b0982e99
PatrickOHara <pohara@turing.ac.uk>
2020-03-11 14:47:09 +0000

    Added foreign key to instance table.

--- containers/cleanair/databases/tables/instance_table.py
+++ containers/cleanair/databases/tables/instance_table.py
@@ -2,7 +2,7 @@
 Table that summerises an instance (model + data + result).
 """
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from ..base import Base
 
@@ -13,10 +13,25 @@ class InstanceTable(Base):
     __table_args__ = {"schema": "model_results"}
 
     instance_id = Column(String(64), primary_key=True, nullable=False)
-    model_name = Column(String(64), nullable=False, index=False)    # ToDo: foreign key
+    model_name = Column(
+        String(64),
+        ForeignKey("model_results.model.model_name"),
+        nullable=False,
+        index=False,
+    )
     tag = Column(String(64), nullable=False, index=False)
-    param_id = Column(String(64), nullable=False, index=False)  # ToDo: foreign key
-    data_id = Column(String(64), nullable=False, index=False)   # ToDo: foreign key
+    param_id = Column(
+        String(64),
+        ForeignKey("model_results.model.param_id"),
+        nullable=False,
+        index=False,
+    )
+    data_id = Column(
+        String(64),
+        ForeignKey("model_results.data_config.data_id"),
+        nullable=False,
+        index=False,
+    )
     git_hash = Column(String(40), nullable=False, index=False)
     fit_start_time = Column(TIMESTAMP, primary_key=False, nullable=False)
     cluster_id = Column(String(64), nullable=False, index=False)
