f07741cf09c4df0b83eea7d21db00968b4525db8
Oscar Giles <ogiles@turing.ac.uk>
2020-04-01 15:35:24 +0100

    Remove schema names from base.py

--- containers/cleanair/cleanair/databases/base.py
+++ containers/cleanair/cleanair/databases/base.py
@@ -2,13 +2,3 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()  # pylint: disable=invalid-name
-SCHEMA_NAMES = [
-    "dynamic_data",
-    "dynamic_features",
-    "interest_points",
-    "model_features",
-    "model_results",
-    "processed_data",
-    "static_data",
-    "static_features",
-]
