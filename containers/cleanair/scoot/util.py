3c895fbf2e43d848abf470f42f69e2739e462504
PatrickOHara <pohara@turing.ac.uk>
2020-04-01 15:57:33 +0100

    Saving and loading files has changed.

--- containers/cleanair/scoot/util.py
+++ containers/cleanair/scoot/util.py
@@ -4,6 +4,72 @@ import pickle
 import gpflow
 import json
 import numpy as np
+from pathlib import Path
+
+
+def generate_fp(name, xp_root="experiments", folder="data", prefix="normal", postfix="scoot", extension="csv"):
+    return os.path.join(xp_root, name, folder, prefix + "_" + postfix + "." + extension)
+
+def save_model_to_file(model, name, detector_id, xp_root="experiments", prefix="normal"):
+    """
+    Save model using pickle.
+    """
+    # Create model copy
+    model_copy = gpflow.utilities.deepcopy_components(model)
+    # Save model to file
+    detector_id = detector_id.replace('/', '_')
+    filepath = os.path.join(xp_root, name, "models")
+    Path(filepath).mkdir(exist_ok=True)
+    filepath = generate_fp(name, xp_root, "models", prefix, detector_id, ".h5")
+    pickle.dump(model_copy, open(filepath), "wb")
+
+def save_results_to_file(y_pred, name, detector_id, xp_root="experiments", prefix="normal"):
+    """
+    Save results to npy with pickle.
+    """
+    filepath = generate_fp(
+        name, xp_root, "results", prefix, detector_id.replace('/', '_'), "npy"
+    )
+    np.save(filepath, y_pred)
+
+def save_processed_data_to_file(
+        X, Y, name, detector_id, xp_root="experiments", prefix="normal"
+    ):
+    """
+    Save processed data for a single detector to file.
+    """
+    x_filepath = generate_fp(
+        name, xp_root, "data", prefix, detector_id.replace("/", "_") + "_X", "npy"
+    )
+    y_filepath = generate_fp(
+        name, xp_root, "data", prefix, detector_id.replace("/", "_") + "_Y", "npy"
+    )
+    np.save(X, x_filepath)
+    np.save(Y, y_filepath)
+
+def load_model_from_file(name, detector_id, xp_root="experiments", prefix="normal"):
+    """Load model from pickle."""
+    filepath = generate_fp(name, xp_root, "models", prefix, detector_id, ".h5")
+    return pickle.load(open(filepath, "rb"))
+
+def load_results_from_file(name, detector_id, xp_root="experiments", prefix="normal"):
+    """Load results of predictions from model."""
+    filepath = generate_fp(
+        name, xp_root, "results", prefix, detector_id.replace('/', '_'), "npy"
+    )
+    return np.load(filepath)
+
+def load_processed_data_from_file(name, detector_id, xp_root="experiments", prefix="normal"):
+    """
+    Load X and Y from file.
+    """
+    x_filepath = generate_fp(
+        name, xp_root, "data", prefix, detector_id.replace("/", "_") + "_X", "npy"
+    )
+    y_filepath = generate_fp(
+        name, xp_root, "data", prefix, detector_id.replace("/", "_") + "_Y", "npy"
+    )
+    return np.load(x_filepath), np.load(y_filepath)
 
 ## Save model and X,Y arrays to file
 def save_model_and_metadata(
