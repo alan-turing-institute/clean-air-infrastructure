c1cfafb15a1b118156569e6ba43dd5c31c19fe9c
PatrickOHara <pohara@turing.ac.uk>
2020-04-11 16:14:35 +0100

    Rename setup to train.

--- containers/entrypoints/lockdown_train.py
+++ containers/entrypoints/lockdown_train.py
@@ -1,110 +1,184 @@
+"""
+Load data and setup for scoot lockdown.
+
+```bash
+python lockdown_setup.py \
+    -s terraform/.secrets/db_secrets.json \
+    -x daily \
+    --nhours 24 \
+    -u terraform/.secrets/user_settings.json
+    --root ../experiments
+```
+"""
 import os
 import json
-from datetime import datetime, timedelta
+import logging
+from datetime import datetime, timedelta, date
+from pathlib import Path
+import numpy as np
 import tensorflow as tf
-from cleanair.parsers import ScootParser
-from cleanair.scoot import (
-    load_processed_data_from_file,
-    save_model_to_file,
-    train_sensor_model,
-    parse_kernel
-)
+
+from uatraffic.util import TrafficModelParser
+from uatraffic.databases import TrafficQuery
+from uatraffic.databases import TrafficInstance
+from uatraffic.preprocess import clean_and_normalise_df
+from uatraffic.model import parse_kernel
+from uatraffic.model import train_sensor_model
+from uatraffic.model import KERNELS
+from uatraffic.util import save_scoot_df
+from uatraffic.util import save_processed_data_to_file
+from uatraffic.util import save_model_to_file
+
+
+def create_directories(root, experiment):
+    data_dir = os.path.join(root, experiment, "data")
+    results_dir = os.path.join(root, experiment, "results")
+    models_dir = os.path.join(root, experiment, "models")
+    settings_dir = os.path.join(root, experiment, "settings")
+
+    # make directories
+    Path(os.path.join(root, experiment)).mkdir(exist_ok=True, parents=True)
+    Path(data_dir).mkdir(exist_ok=True)         # input data and processed training data
+    Path(results_dir).mkdir(exist_ok=True)      # predictions from model
+    Path(models_dir).mkdir(exist_ok=True)       # saving model status
+    Path(settings_dir).mkdir(exist_ok=True)     # for storing parameters
 
 def main():
-    parser = ScootParser()
+
+    parser = TrafficModelParser()
     args = parser.parse_args()
 
-    kernel_settings = {
-        "rbf_ls=0.1_v=0.1": {
-            "name": "rbf",
-            "hyperparameters": {"lengthscale": 0.1, "variance": 0.1}
-        },
-        "matern32_ls=0.1_v=0.1": {
-            "name": "matern32",
-            "hyperparameters": {"lengthscale": 0.1, "variance": 0.1}
-        },
-        "matern52_ls=0.1_v=0.1": {
-            "name":"matern52",
-            "hyperparameters":{
-                "lengthscale":0.1,
-                "variance": 0.1
-            }
-        },
-        "periodic_0.5": {
-            "name": "periodic",
-            "hyperparameters": {
-                "period": 0.5,
-                "lengthscale": 0.7,
-                "variance": 4.5
-            }
-        },
-    }
+    if args.cluster_id == "local":
+        # setup experiment directories
+        create_directories(args.root, args.experiment)
 
-    # save the kernel settings
-    kernel_settings_fp = os.path.join(
-        args.root,
-        args.experiment,
-        "settings",
-        "kernel_settings.json"
-    )
-    try:
-        with open(kernel_settings_fp, "r+") as kernel_file:
-            current_settings = json.load(kernel_file)
-            current_settings.update(kernel_settings)
-            kernel_file.seek(0)
-            json.dump(current_settings, kernel_file)
-    except FileNotFoundError:
-        with open(kernel_settings_fp, "w") as kernel_file:
-            json.dump(kernel_settings, kernel_file)
+    # process datetimes
+    start = datetime.strptime(args.baseline_start, "%Y-%m-%d")
+    end = start + timedelta(hours=args.nhours)
 
-    # load the data settings
-    normal_end = datetime.strptime(args.normal_start, "%Y-%m-%dT%H:%M:%S") + timedelta(hours=args.nhours * args.rolls)
-    data_settings_fp = os.path.join(
-        args.root,
-        args.experiment,
-        "settings",
-        "data_settings.json"
-    )
-    with open(data_settings_fp, "r") as json_file:
-        data_settings = json.load(json_file)
+    # create an object for querying from DB
+    traffic_query = TrafficQuery(secretfile=args.secretfile)
+
+    # columns to train model on
+    x_cols=['epoch', 'lon_norm', 'lat_norm']
+    y_cols=["n_vehicles_in_interval"]
 
     # setup parameters
     optimizer = tf.keras.optimizers.Adam(0.001)
-    epochs = 2000
     logging_epoch_freq = 100
-    M = 24      # number of inducing points
+    kernel_dict = next(k for k in KERNELS if k["name"] == args.kernel)
 
-    # loop through list of sensor. train model for each sensor
-    for data in data_settings:
-        if (
-            data["normal_start"] >= args.normal_start and
-            data["normal_start"] < normal_end.strftime("%Y-%m-%dT%H:%M:%S")
-        ):
-            for detector_id in data["detectors"]:
-                for kernel_id in kernel_settings:
-                    x_normal, y_normal = load_processed_data_from_file(
-                        root=args.root,
-                        experiment=args.experiment,
-                        timestamp=data["normal_start"],
-                        detector_id=detector_id
-                    )
-                    # get a kernel from settings
-                    kernel = parse_kernel(kernel_settings[kernel_id])
+    while start < datetime.strptime(args.baseline_end, "%Y-%m-%d"):
+        # read the data from DB
+        logging.info(
+            "Getting scoot readings from %s to %s.",
+            start.strftime("%Y-%m-%d %H:%M:%S"),
+            end.strftime("%Y-%m-%d %H:%M:%S")
+        )
+        df = traffic_query.get_scoot_with_location(
+            start_time=start.strftime("%Y-%m-%d %H:%M:%S"),
+            end_time=end.strftime("%Y-%m-%d %H:%M:%S"),
+            output_type="df",
+        )
+        # data cleaning and processing
+        df = clean_and_normalise_df(df)
 
-                    # train model
-                    model = train_sensor_model(
-                        x_normal, y_normal, kernel, optimizer, epochs, logging_epoch_freq, M=M
-                    )
+        # save the data to csv
+        if args.cluster_id == "local":
+            save_scoot_df(
+                df,
+                root=args.root,
+                experiment=args.experiment,
+                timestamp=start.strftime("%Y-%m-%dT%H:%M:%S"),
+                filename="scoot"
+            )
 
-                    # save model to file
-                    save_model_to_file(
-                        model,
-                        root=args.root,
-                        experiment=args.experiment,
-                        timestamp=data["normal_start"],
-                        detector_id=detector_id,
-                        kernel_id=kernel_id
-                    )
+        # get array of numpy for X and Y
+        gb = df.groupby("detector_id")
+
+        # loop over all detectors and write each numpy to a file
+        for detector_id in args.detectors:
+            # create a data id from dictionary of data settings
+            data_config = dict(
+                detectors=[detector_id],
+                weekdays=[date.fromisoformat(start.strftime("%Y-%m-%d")).weekday()],
+                start=start.strftime("%Y-%m-%dT%H:%M:%S"),
+                end=end.strftime("%Y-%m-%dT%H:%M:%S"),
+            )
+            # create dict of model settings
+            model_params = dict(
+                n_inducing_points=args.n_inducing_points,
+                epochs=args.epochs,
+                kernel=kernel_dict,
+            )
+            # create an instance object then write instance to DB
+            instance = TrafficInstance(
+                model_name=args.model_name,
+                data_id=TrafficInstance.hash_dict(data_config),
+                param_id=TrafficInstance.hash_dict(model_params),
+                cluster_id=args.cluster_id,
+                tag=args.tag,
+                fit_start_time=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                secretfile=args.secretfile,
+            )
+
+            # get training data
+            x_train = np.array(gb.get_group(detector_id)[x_cols])
+            y_train = np.array(gb.get_group(detector_id)[y_cols])
+
+            if args.cluster_id == "local":
+                save_processed_data_to_file(
+                    x_train,
+                    y_train,
+                    root=args.root,
+                    experiment=args.experiment,
+                    timestamp=start.strftime("%Y-%m-%dT%H:%M:%S"),
+                    detector_id=detector_id
+                )
+
+            # get a kernel from settings
+            kernel = parse_kernel(kernel_dict)
+
+            # train model
+            model = train_sensor_model(
+                x_train, y_train, kernel, optimizer, args.epochs, logging_epoch_freq, M=args.n_inducing_points
+            )
+
+            # TODO: write models to blob storage
+            # save_model_to_file(model)
+            instance.update_data_table(data_config)
+            instance.update_model_table(model_params)
+            instance.update_remote_tables()
+
+        # add data settings to list
+        # data_settings.append(dict(
+        #     detectors=args.detectors,
+        #     normal_start=normal_start.strftime("%Y-%m-%dT%H:%M:%S"),
+        #     normal_end=normal_end.strftime("%Y-%m-%dT%H:%M:%S"),
+        #     lockdown_start=lockdown_start.strftime("%Y-%m-%dT%H:%M:%S"),
+        #     lockdown_end=lockdown_end.strftime("%Y-%m-%dT%H:%M:%S"),
+        # ))
+        
+        # add on n hours to start and end datetimes
+        start = start + timedelta(hours=args.nhours)
+        end = end + timedelta(hours=args.nhours)
+
+    # print(data_settings)
+    # data_settings_fp = os.path.join(
+    #     args.root,
+    #     args.experiment,
+    #     "settings",
+    #     "data_settings.json"
+    # )
+    # try:
+    #     with open(data_settings_fp, "r") as json_file:
+    #         original_settings = json.load(json_file)
+    #         original_settings.extend(data_settings)
+    # except FileNotFoundError:
+    #     original_settings = data_settings.copy()
+    # finally:
+    #     with open(data_settings_fp, "w") as json_file:
+    #         json.dump(original_settings, json_file)
 
 if __name__ == "__main__":
     main()
