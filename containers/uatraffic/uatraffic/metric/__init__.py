be7f4a05c7dabdf10bf79f2b08b5e50f3a8d134d
Oscar Giles <ogiles@turing.ac.uk>
2020-04-15 12:11:58 +0100

    Remove coverage from metrics file in uatraffic

--- containers/uatraffic/uatraffic/metric/__init__.py
+++ containers/uatraffic/uatraffic/metric/__init__.py
@@ -1,10 +1,8 @@
 """
 Metrics for evaluating traffic models and comparing against baselines.
 """
-from .coverage import percent_coverage
 from .percent import percent_of_baseline
 
 __all__ = [
-    "percent_coverage",
     "percent_of_baseline",
 ]
