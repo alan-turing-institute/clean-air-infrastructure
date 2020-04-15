81e5a98417525de64a82a79fea9c209cf15cf7f1
Oscar Giles <ogiles@turing.ac.uk>
2020-04-15 12:10:41 +0100

    Remove from database init in uatraffic module

--- containers/uatraffic/uatraffic/databases/__init__.py
+++ containers/uatraffic/uatraffic/databases/__init__.py
@@ -2,17 +2,7 @@
 Database classes and functions for traffic.
 """
 from .query import TrafficQuery
-from .instance import TrafficInstance
-from .tables import (
-    TrafficDataTable,
-    TrafficInstanceTable,
-    TrafficModelTable,
-)
 
 __all__ = [
     "TrafficQuery",
-    "TrafficInstance",
-    "TrafficDataTable",
-    "TrafficInstanceTable",
-    "TrafficModelTable",
 ]
