01312cc6093f6bd98c136ca115eede892886a146
Oscar Giles <ogiles@turing.ac.uk>
2020-04-15 12:07:10 +0100

    Remove model and plot from uatraffic init file

--- containers/uatraffic/uatraffic/__init__.py
+++ containers/uatraffic/uatraffic/__init__.py
@@ -3,16 +3,12 @@ Analysing traffic and building traffic models in London.
 """
 from . import databases
 from . import metric
-from . import model
-from . import plot
 from . import preprocess
 from . import util
 
 __all__ = [
     "databases",
     "metric",
-    "model",
-    "plot",
     "preprocess",
     "util"
 ]
