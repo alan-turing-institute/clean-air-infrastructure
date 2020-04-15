42ef3ebe417eb58aaaa3a3523a6d59d04d12ebf6
Oscar Giles <ogiles@turing.ac.uk>
2020-04-15 12:40:26 +0100

    pass k in uatraffic

--- containers/uatraffic/uatraffic/preprocess/anomaly.py
+++ containers/uatraffic/uatraffic/preprocess/anomaly.py
@@ -3,7 +3,7 @@ import logging
 
 def remove_outliers(df, k=3, col="n_vehicles_in_interval"):
     """Remove outliers $x$ where $|x - \mu| > k \sigma$ for each detector."""
-    to_remove = get_index_of_outliers(df, k=3, col=col)
+    to_remove = get_index_of_outliers(df, k=k, col=col)
     logging.info("Removed %s anomalous readings.", len(to_remove))
     logging.info("Total %s readings.", len(df))
     return df.loc[~df.index.isin(set(to_remove))]
