25d2481fca7403caa74703099bf61559f8735d24
Oscar Giles <ogiles@turing.ac.uk>
2020-04-20 15:57:07 +0100

    Update flagger requiremenet

--- containers/uatraffic/setup.py
+++ containers/uatraffic/setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["cleanair",],
+    install_requires=["cleanair", "flasgger"],
     python_requires=">=3.7",
 )
