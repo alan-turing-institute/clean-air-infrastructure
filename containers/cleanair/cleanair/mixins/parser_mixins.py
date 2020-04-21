072719876d9deb20503400c8467a76be511841fa
Oscar Giles <ogiles@turing.ac.uk>
2020-04-21 13:09:29 +0100

    Fix cleanair daterange mixin error. Was not assuming string was iso format

--- containers/cleanair/cleanair/mixins/parser_mixins.py
+++ containers/cleanair/cleanair/mixins/parser_mixins.py
@@ -31,7 +31,7 @@ class DurationParserMixin:
             "--end",
             type=str,
             default=end,
-            help="The last time point to get data for (default: {}).".format(end),
+            help="The last time point to get data for in iso format (default: {}).".format(end),
         )
         self.add_argument(
             "-n",
