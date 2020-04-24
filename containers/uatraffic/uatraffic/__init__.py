3b8902b3272f4f790b721059972d665f745029b3
PatrickOHara <pohara@turing.ac.uk>
2020-04-24 13:38:02 +0100

    Unnecessary imports removed.

--- containers/uatraffic/uatraffic/__init__.py
+++ containers/uatraffic/uatraffic/__init__.py
@@ -1,18 +1,3 @@
 """
 Analysing traffic and building traffic models in London.
 """
-from . import databases
-from . import metric
-from . import model
-from . import plot
-from . import preprocess
-from . import util
-
-__all__ = [
-    "databases",
-    "metric",
-    "model",
-    "plot",
-    "preprocess",
-    "util"
-]
