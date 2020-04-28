3e3414cbe34df9de6bf5f2bc33cdcd54e8023bf2
Oscar Giles <ogiles@turing.ac.uk>
2020-04-28 16:13:36 +0100

    :card_file_box: Declare UKMAP rather than reflecting

--- containers/cleanair/cleanair/databases/tables/ukmap_tables.py
+++ containers/cleanair/cleanair/databases/tables/ukmap_tables.py
@@ -2,14 +2,28 @@
 Table for UKMap static data
 """
 from sqlalchemy.ext.declarative import DeferredReflection
+from sqlalchemy import Column, ForeignKey, String, Integer, Enum
+from geoalchemy2 import Geometry
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, TIMESTAMP, BOOLEAN, DATE
 from ..base import Base
 
 
-class UKMap(DeferredReflection, Base):
+class UKMap(Base):
     """Table of static UKMap data"""
 
     __tablename__ = "ukmap"
     __table_args__ = {"schema": "static_data"}
+
+    geographic_type_number = Column(Integer, primary_key=True, nullable=False,)
+    date_of_feature_edit = Column(DATE)
+    feature_type = Column(String(200), index=True)
+    landuse = Column(String(200), index=True)
+    postcode = Column(String(200))
+    height_of_base_of_building = Column(DOUBLE_PRECISION)
+    calculated_height_of_building = Column(DOUBLE_PRECISION)
+    geom_length = Column(DOUBLE_PRECISION)
+    geom_area = Column(DOUBLE_PRECISION)
+    geom = (Geometry(srid=4326, spatial_index=True),)
 
     def __repr__(self):
         vals = [
