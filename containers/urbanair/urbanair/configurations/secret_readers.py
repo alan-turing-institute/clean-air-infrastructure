276822a04b71a4dafbcf650fd91986e9ef171516
Oscar Giles <ogiles@turing.ac.uk>
2020-05-13 19:51:13 +0100

    :rocket: Try secrets again

--- containers/urbanair/urbanair/configurations/secret_readers.py
+++ containers/urbanair/urbanair/configurations/secret_readers.py
@@ -1,6 +1,6 @@
 """Helper functions for reading secret files"""
 
-def read_basic_auth_secret(location = 'secrets/BASIC_AUTH_PASSWORD'):
+def read_basic_auth_secret(location = 'secrets/basic_auth_password.txt'):
     """Read a basic auth secret"""
     with open(location) as f_secret:
         return f_secret.read()
