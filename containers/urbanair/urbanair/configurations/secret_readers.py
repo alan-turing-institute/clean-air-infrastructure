d127b2d39763dfad24699138545dc261a582ca0a
Oscar Giles <ogiles@turing.ac.uk>
2020-05-14 15:14:01 +0100

    :pencil2: Make sure secrets loaded from correct place
    Ensure secrets are loaded correctly on the production config of the urbanair API

--- containers/urbanair/urbanair/configurations/secret_readers.py
+++ containers/urbanair/urbanair/configurations/secret_readers.py
@@ -1,6 +1,9 @@
 """Helper functions for reading secret files"""
+import os
 
-def read_basic_auth_secret(location = 'secrets/basic_auth_password.txt'):
+def read_basic_auth_secret(secret_directory = "/secrets", secret_file = 'basic_auth_password.txt'):
     """Read a basic auth secret"""
-    with open(location) as f_secret:
+
+    full_secret_path = os.path.join(secret_directory, secret_file)
+    with open(full_secret_path) as f_secret:
         return f_secret.read()
