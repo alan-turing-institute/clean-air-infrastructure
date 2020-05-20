e20bfa07b99dc3d197b6c5f3b41fa9d564b95b48
PatrickOHara <pohara@turing.ac.uk>
2020-05-20 14:57:22 +0100

    Tidy up experiment. need to implement

--- containers/uatraffic/uatraffic/experiment/experiment.py
+++ containers/uatraffic/uatraffic/experiment/experiment.py
@@ -4,37 +4,42 @@ Experiments summerise multiple instances.
 
 import pandas as pd
 from cleanair.databases import DBWriter
+from .instance import TrafficInstance
 
 class Experiment(DBWriter):
     """
     An experiment contains multiple instances.
     """
 
-    def __init__(self, secretfile: str = None, **kwargs):
+    def __init__(self, frame: pd.DataFrame = None, secretfile: str = None, **kwargs):
         super().__init__(secretfile=secretfile, **kwargs)
-        self._dataframe = pd.DataFrame(
-            columns=[
-                "instance_id",
-                "model_name",
-                "param_id",
-                "data_id",
-                "cluster_id",
-                "tag",
-                "git_hash",
-                "fit_start_time",
-                "data_config",
-                "model_param",
-                "preprocessing",
-            ]
-        )
+        if frame:
+            self._frame = pd.DataFrame(
+                columns=[
+                    "instance_id",
+                    "model_name",
+                    "param_id",
+                    "data_id",
+                    "cluster_id",
+                    "tag",
+                    "git_hash",
+                    "fit_start_time",
+                    "data_config",
+                    "model_param",
+                    "preprocessing",
+                ]
+            )
+        else:
+            self._frame = frame
 
     @property
-    def dataframe(self):
+    def frame(self):
         """Information about the instances"""
-        return self._dataframe
+        return self._frame
     
     def add_instance(self, instance: TrafficInstance):
         """ToDo."""
 
     def update_remote_tables(self):
         """Update the instance, data and model tables."""
+        raise NotImplementedError("Coming soon")
