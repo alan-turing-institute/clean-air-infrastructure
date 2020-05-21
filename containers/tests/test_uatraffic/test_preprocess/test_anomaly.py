90114dd648463d9c69774f115648897d2468a26c
PatrickOHara <pohara@turing.ac.uk>
2020-05-21 14:45:13 +0100

    added comments

--- containers/tests/test_uatraffic/test_preprocess/test_anomaly.py
+++ containers/tests/test_uatraffic/test_preprocess/test_anomaly.py
@@ -11,7 +11,7 @@ def test_no_outliers_removed(scoot_df):
 def test_outliers_removed(anomalous_scoot_df):
     """Test that anomalous readings are removed."""
     clean_df = preprocess.remove_outliers(anomalous_scoot_df)
-    assert 6 not in clean_df.index
+    assert 6 not in clean_df.index      # negative should be removed
     assert 7 in clean_df.index          # zero is currently accepted
-    assert 54 not in clean_df.index
-    assert 65 not in clean_df.index
+    assert 54 not in clean_df.index     # low anomaly
+    assert 65 not in clean_df.index     # high anomaly
