439944b3d6ae590b786ec48633b5c56b5cdbdc8f
PatrickOHara <pohara@turing.ac.uk>
2020-05-21 16:49:43 +0100

    Move anomolous scoot df fixture.

--- containers/tests/test_uatraffic/test_preprocess/test_anomaly.py
+++ containers/tests/test_uatraffic/test_preprocess/test_anomaly.py
@@ -1,14 +1,37 @@
 """Testing outliers and anomalies are removed as expected."""
 
+import pytest
+import pandas as pd
 from uatraffic import preprocess
 
-def test_no_outliers_removed(scoot_df):
+@pytest.fixture(scope="function")
+def anomalous_scoot_df(scoot_df):
+    """Add some anomalies to the fake scoot data."""
+    # get mean and standard deviation
+    mean = scoot_df["n_vehicles_in_interval"].mean()
+    std = scoot_df["n_vehicles_in_interval"].std()
+
+    # add negative anomaly at index 6
+    scoot_df.at[6, "n_vehicles_in_interval"] = -2
+
+    # add zero anomaly at index 7
+    scoot_df.at[7, "n_vehicles_in_interval"] = 0
+
+    # add low anomaly at index 8 (this may also be negative)
+    scoot_df.at[8, "n_vehicles_in_interval"] = mean - 3 * std - 1
+
+    # add big anomaly at index 65
+    scoot_df.at[65, "n_vehicles_in_interval"] = mean + 3 * std + 1
+
+    return scoot_df
+
+def test_no_outliers_removed(scoot_df: pd.DataFrame):
     """Test that no outliers are removed if they shouldn't."""
     num_rows = len(scoot_df)
     clean_df = preprocess.remove_outliers(scoot_df)
     assert len(clean_df) == num_rows
 
-def test_outliers_removed(anomalous_scoot_df):
+def test_outliers_removed(anomalous_scoot_df: pd.DataFrame):  # pylint: disable=redefined-outer-name
     """Test that anomalous readings are removed."""
     clean_df = preprocess.remove_outliers(anomalous_scoot_df)
     assert 6 not in clean_df.index      # negative should be removed
