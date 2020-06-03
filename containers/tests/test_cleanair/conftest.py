8339fb5eddf29e79259bbfc0cbbf7ab615c2eb92
PatrickOHara <pohara@turing.ac.uk>
2020-06-03 21:19:16 +0100

    My linting has broken on vscode. Very annoying.

---
+++ containers/tests/test_cleanair/conftest.py
@@ -0,0 +1,31 @@
+this is an obvious error
+
+import pytest
+
+@pytest.fixture(scope="module")
+def base_data_config() -> dict:
+    """An air quality data config dictionary with basic settings."""
+
+    return {
+        "train_start_date": "2020-01-01",
+        "train_end_date": "2020-01-02",
+        "pred_start_date": "2020-01-02",
+        "pred_end_date": "2020-01-03",
+        "include_satellite": False,
+        "include_prediction_y": False,
+        "train_sources": ["laqn"],
+        "pred_sources": ["laqn"],
+        "train_interest_points": "all",
+        "train_satellite_interest_points": "all",
+        "pred_interest_points": "all",
+        "species": ["NO2"],
+        "features": [
+            "value_1000_total_a_road_length",
+            "value_500_total_a_road_length",
+            "value_500_total_a_road_primary_length",
+            "value_500_total_b_road_length",
+        ],
+        "norm_by": "laqn",
+        "model_type": "svgp",
+        "tag": "test",
+    }
