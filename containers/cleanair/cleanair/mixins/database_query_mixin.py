f50932818459b430ce90af5a55d55c26a60cc093
PatrickOHara <pohara@turing.ac.uk>
2020-06-08 16:02:47 +0100

    Refactor and test of scoot query mixin.

--- containers/cleanair/cleanair/mixins/database_query_mixin.py
+++ containers/cleanair/cleanair/mixins/database_query_mixin.py
@@ -2,7 +2,7 @@
 Mixin for useful database queries
 """
 from datetime import datetime
-from typing import List, Tuple
+from typing import Iterable, Tuple, List
 import pandas as pd
 from sqlalchemy import and_, or_, func, literal, null
 from ..decorators import db_query
@@ -277,21 +277,19 @@ class ScootQueryMixin:
     """Queries for the scoot dataset."""
 
     @db_query
-    def get_scoot_with_location(self, start_time, end_time=None, detectors=None):
+    def get_scoot_with_location(
+        self,
+        start_time: str,
+        end_time: str = None,
+        detectors: List = None
+    ):
         """
         Get scoot data with lat and long positions.
 
-        Parameters
-        ___
-
-        start_time : str
-            Start datetime.
-
-        end_time : str, optional
-            End datetime (exclusive).
-
-        detectors : list, optional
-            Subset of detectors to get readings for.
+        Args:
+            start_time: Start datetime.
+            end_time: End datetime (exclusive).
+            detectors: Subset of detectors to get readings for.
         """
 
         with self.dbcnxn.open_session() as session:
@@ -325,7 +323,7 @@ class ScootQueryMixin:
             return scoot_readings
     
     @staticmethod
-    def create_day_of_week_daterange(day_of_week: int, start_date: str, end_date: str) -> List[Tuple[str, str]]:
+    def create_day_of_week_daterange(day_of_week: int, start_date: str, end_date: str) -> Iterable:
         """Create a list of tuples (start date, end date) where each start_date is the same day of the week.
 
         Args:
@@ -338,7 +336,7 @@ class ScootQueryMixin:
         """
         # TODO add testing to this function
         # get list of start times that match the weekday within the timerange
-        starts = pd.date_range(start_date, end_date).to_series()
+        starts = pd.date_range(start_date, end_date, closed="left").to_series()
         starts = (
             starts[(starts.dt.dayofweek == day_of_week) & (starts < end_date)]
             .dt.strftime("%Y-%m-%d")
@@ -346,9 +344,9 @@ class ScootQueryMixin:
         )
 
         # get list of end times that match the weekday within the timerange
-        ends = pd.date_range(start_date, end_date).to_series()
+        ends = pd.date_range(start_date, end_date, closed="left").to_series()
         ends = (
-            ends[(ends.dt.dayofweek == (day_of_week + 1) % 7) & (ends > start_date)]
+            ends[(ends.dt.dayofweek == (day_of_week + 1) % 7) & (start_date < ends)]
             .dt.strftime("%Y-%m-%d")
             .to_list()
         )
@@ -356,18 +354,18 @@ class ScootQueryMixin:
         return zip(starts, ends)
 
     @db_query
-    def get_scoot_by_dow(self, day_of_week: int, start_time: str, end_time: str = None, detectors: List = None):
+    def get_scoot_by_dow(self, day_of_week: int, start_date: str, end_date: str = None, detectors: List = None):
         """
-        Get scoot readings for days between start_time and end_time filtered by day_of_week.
+        Get scoot readings for days between start_date and end_date filtered by day_of_week.
 
         Args:
             day_of_week: Day of the week. 0=Mon, 1=Tue, etc.
-            start_time: Start datetime.
-            end_time: End datetime (exclusive).
+            start_date: Start datetime.
+            end_date: End datetime (exclusive).
             detectors: Subset of detectors to get readings for.
         """
-        if not end_time:
-            end_time = datetime.now().strftime("%Y-%m-%d")
+        if not end_date:
+            end_date = datetime.now().strftime("%Y-%m-%d")
 
 
         with self.dbcnxn.open_session() as session:
@@ -387,7 +385,7 @@ class ScootQueryMixin:
             )
             # get a list of or statements
             or_statements = []
-            for start, end in self.create_day_of_week_daterange(day_of_week, start_time, end_time):
+            for start, end in self.create_day_of_week_daterange(day_of_week, start_date, end_date):
                 # append AND statement
                 or_statements.append(
                     and_(
