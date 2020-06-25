bb3c6bcf6a4914640fe176aaeae2fc1c6c734dd5
PatrickOHara <pohara@turing.ac.uk>
2020-06-25 17:15:41 +0100

    Delete old test

--- containers/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Tests for the cleanair module.
-"""
-from .model_data_test import check_test_set
-from .model_data_test import check_training_set
--- containers/tests/model_fit_test.py
+++
@@ -1,9 +0,0 @@
-"""
-Check a model has fit the data correctly.
-"""
-
-def check_y_pred(model_data, y_pred):
-    """
-    Check that `y_pred` is in the correct shape.
-    """
-    raise NotImplementedError()
