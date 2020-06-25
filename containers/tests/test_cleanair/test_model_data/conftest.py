91c64b1bc6bb66754fb21e36bea2d2990bec125a
PatrickOHara <pohara@turing.ac.uk>
2020-06-25 19:09:42 +0100

    🚨 Black formatting

--- containers/tests/test_cleanair/test_model_data/conftest.py
+++ containers/tests/test_cleanair/test_model_data/conftest.py
@@ -9,27 +9,21 @@ from sqlalchemy.engine import Connection
 from cleanair.models import ModelData
 from cleanair.types import DataConfig, FeaturesDict, TargetDict
 
+
 @pytest.fixture(scope="function")
 def satellite_x_train() -> FeaturesDict:
     """Training dictionary."""
-    return dict(
-        laqn=np.random.rand(24, 3),
-        satellite=np.random.rand(4, 5, 3),
-    )
+    return dict(laqn=np.random.rand(24, 3), satellite=np.random.rand(4, 5, 3),)
+
 
 @pytest.fixture(scope="function")
 def satellite_y_train() -> TargetDict:
     """Training target dict."""
     return dict(
-        laqn=dict(
-            NO2=np.random.rand(24, 1),
-            PM10=np.random.rand(24, 1),
-        ),
-        satellite=dict(
-            NO2=np.random.rand(4, 1),
-            PM10=np.random.rand(4, 1),
-        )
+        laqn=dict(NO2=np.random.rand(24, 1), PM10=np.random.rand(24, 1),),
+        satellite=dict(NO2=np.random.rand(4, 1), PM10=np.random.rand(4, 1),),
     )
+
 
 @pytest.fixture(scope="function")
 def training_df() -> pd.DataFrame:
@@ -39,10 +33,9 @@ def training_df() -> pd.DataFrame:
     lat = np.random.rand()
     lon = np.random.rand()
     assert len(timerange) == 24
-    data_df = pd.DataFrame(dict(
-        measurement_start_utc=timerange,
-        NO2=np.random.rand(24),
-    ))
+    data_df = pd.DataFrame(
+        dict(measurement_start_utc=timerange, NO2=np.random.rand(24),)
+    )
     data_df["epoch"] = data_df["measurement_start_utc"].apply(lambda x: x.timestamp())
     data_df["point_id"] = point_id
     data_df["source"] = "laqn"
@@ -50,8 +43,8 @@ def training_df() -> pd.DataFrame:
     data_df["lon"] = lon
     return data_df
 
-class MockModelData:
 
+class MockModelData:
     def __init__(self, training_df):
         self.training_df = training_df
 
@@ -61,12 +54,13 @@ class MockModelData:
     def mock_generate_full_config(self, config) -> DataConfig:
         config["x_names"] = ["epoch", "lat", "lon"] + config["features"]
         return config
-    
+
     def mock_get_training_data_inputs(self) -> pd.DataFrame:
         return self.training_df
 
     def mock_get_pred_data_inputs(self) -> pd.DataFrame:
         return self.training_df
+
 
 @pytest.fixture(scope="function")
 def model_data(
@@ -81,10 +75,20 @@ def model_data(
     mock = MockModelData(training_df)
 
     # for private methods you must specify _ModelData first
-    monkeypatch.setattr(ModelData, "_ModelData__validate_config", mock.mock_validate_config)
-    monkeypatch.setattr(ModelData, "_ModelData__generate_full_config", mock.mock_generate_full_config)
-    monkeypatch.setattr(ModelData, "get_training_data_inputs", mock.mock_get_training_data_inputs)
-    monkeypatch.setattr(ModelData, "get_pred_data_inputs", mock.mock_get_pred_data_inputs)
-    dataset = ModelData(no_features_data_config, secretfile=secretfile, connection=connection)
+    monkeypatch.setattr(
+        ModelData, "_ModelData__validate_config", mock.mock_validate_config
+    )
+    monkeypatch.setattr(
+        ModelData, "_ModelData__generate_full_config", mock.mock_generate_full_config
+    )
+    monkeypatch.setattr(
+        ModelData, "get_training_data_inputs", mock.mock_get_training_data_inputs
+    )
+    monkeypatch.setattr(
+        ModelData, "get_pred_data_inputs", mock.mock_get_pred_data_inputs
+    )
+    dataset = ModelData(
+        no_features_data_config, secretfile=secretfile, connection=connection
+    )
     print(dataset.normalised_training_data_df.head())
     return dataset
--- containers/tests/test_cleanair/test_model_data/test_model_data.py
+++ containers/tests/test_cleanair/test_model_data/test_model_data.py
@@ -2,19 +2,24 @@
 Given a model data object, check that the data matches the config.
 """
 
+
 def test_training_dicts(model_data):
     """Check the shape of all the numpy arrays are correct."""
     # load the dicts from modeldata
     training_data_dict = model_data.get_training_data_arrays(dropna=False)
-    x_train = training_data_dict['X']
-    y_train = training_data_dict['Y']
-    
+    x_train = training_data_dict["X"]
+    y_train = training_data_dict["Y"]
+
     # checks for satellite
-    assert not model_data.config['include_satellite'] or 'satellite' in x_train
-    assert not model_data.config['include_satellite'] or 'satellite' in y_train
+    assert not model_data.config["include_satellite"] or "satellite" in x_train
+    assert not model_data.config["include_satellite"] or "satellite" in y_train
 
     # checks that each pollutant has a key in y_train for each source
-    all_train_sources = model_data.config['train_sources'] if not model_data.config['include_satellite'] else model_data.config['train_sources'] + ['satellite']
+    all_train_sources = (
+        model_data.config["train_sources"]
+        if not model_data.config["include_satellite"]
+        else model_data.config["train_sources"] + ["satellite"]
+    )
     for source in all_train_sources:
         # check all training sources exist in the dicts
         assert source in x_train
@@ -27,24 +32,24 @@ def test_training_dicts(model_data):
             assert len(x_train[source].shape) == 2
 
         # check y_train
-        for pollutant in model_data.config['species']:
+        for pollutant in model_data.config["species"]:
             assert pollutant in y_train[source]
 
             # check the shapes of y_train
-            assert y_train[source][pollutant].shape[1] == 1     # shape should be (N, 1)
+            assert y_train[source][pollutant].shape[1] == 1  # shape should be (N, 1)
             assert y_train[source][pollutant].shape[0] == x_train[source].shape[0]
+
 
 def test_pred_dict(model_data):
     """
     Check the test set is in the right format.
     """
     predict_data_dict = model_data.get_pred_data_arrays(dropna=False, return_y=True)
-    x_test = predict_data_dict['X']
+    x_test = predict_data_dict["X"]
     y_test = predict_data_dict["Y"]
-    assert 'satellite' not in x_test
+    assert "satellite" not in x_test
 
-
-    for source in model_data.config['pred_sources']:
+    for source in model_data.config["pred_sources"]:
         assert source in x_test
         assert len(x_test[source].shape) == 2
         assert x_test[source].shape[1] == len(model_data.config["x_names"])
