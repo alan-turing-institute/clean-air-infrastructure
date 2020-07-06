0ceab59026c988d6fc882dbce966da70335a6e3c
PatrickOHara <pohara@turing.ac.uk>
2020-07-06 20:53:31 +0100

    Fix tuple error.

--- containers/cleanair/cleanair/types/dataset_types.py
+++ containers/cleanair/cleanair/types/dataset_types.py
@@ -7,6 +7,6 @@ from nptyping import NDArray, Float64
 
 DataConfig = Dict[str, Union[str, bool, List[str]]]
 FeaturesDict = Dict[str, NDArray[Float64]]
-NDArrayTuple = Tuple(NDArray[Float64], NDArray[Float64])
+NDArrayTuple = Tuple[NDArray[Float64], NDArray[Float64]]
 TargetDict = Dict[str, Dict[str, NDArray[Float64]]]
 DatasetDict = Dict[str, Union[FeaturesDict, TargetDict]]
