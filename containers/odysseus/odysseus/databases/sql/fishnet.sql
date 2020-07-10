55c406479019cf346e6a2b844c962a5e68c062dd
PatrickOHara <pohara@turing.ac.uk>
2020-07-10 10:59:05 +0100

    🐛 Fix function

--- containers/odysseus/odysseus/databases/sql/fishnet.sql
+++ containers/odysseus/odysseus/databases/sql/fishnet.sql
@@ -1,20 +1,16 @@
-CREATE TYPE grid_cell as (              -- a record in the returned query below
-	row INTEGER,
-	col INTEGER,
-	geom GEOGRAPHY
-);
-
--- create a grid that contains a polygon
--- you can rotate the grid, pass different coordinate systems
--- and change the size of the grid cells
+DROP FUNCTION st_fishnet(geometry,integer,integer,double precision,integer) CASCADE;
 CREATE OR REPLACE FUNCTION ST_Fishnet(
     bound_polygon geometry,             -- polygon to draw fishnet over
     grid_resolution integer,            -- number of squares
     grid_step integer,                  -- size of grid cell in meters
-    rotation DOUBLE PRECISION = 0,      -- the rotation of the grid in degrees
-    srid integer = 4326                 -- spatial reference id
+    rotation DOUBLE PRECISION DEFAULT 0,      -- the rotation of the grid in degrees
+    srid integer DEFAULT 4326                 -- spatial reference id
 )
-RETURNS SETOF grid_cell                 -- return a set of grid cells
+RETURNS TABLE (
+	"row" INTEGER,
+	col INTEGER,
+	geom GEOGRAPHY
+)
 AS $$
 DECLARE
     xmin DOUBLE PRECISION := ST_XMin(ST_Transform(bound_polygon, srid));
