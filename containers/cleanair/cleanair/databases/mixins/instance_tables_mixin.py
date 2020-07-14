94f982711f2d7ebee46bb81b8cbc98e235cf4ea2
Tim Spain <t.spain@ucl.ac.uk>
2020-07-14 15:06:04 +0100

    cleanair/databases/mixins: Remove 'type: ignore' and add type hints.

--- containers/cleanair/cleanair/databases/mixins/instance_tables_mixin.py
+++ containers/cleanair/cleanair/databases/mixins/instance_tables_mixin.py
@@ -2,23 +2,29 @@
 Table that summerises an instance (model + data + result).
 """
 
-from sqlalchemy import Column, String # type: ignore
-from sqlalchemy.dialects.postgresql import TIMESTAMP, JSONB, UUID # type: ignore
-
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import TIMESTAMP, JSONB, UUID
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from sqlalchemy.sql.schema import Table
 
 class InstanceTableMixin:
     """Table of Instances."""
 
-    instance_id = Column(String(64), primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
-    tag = Column(String(64), nullable=False, index=False) # type: ignore # sqlalchemy.Column
-    git_hash = Column(String(40), nullable=False, index=False) # type: ignore # sqlalchemy.Column
-    fit_start_time = Column(TIMESTAMP, primary_key=False, nullable=False) # type: ignore # sqlalchemy.Column
-    cluster_id = Column(String(64), nullable=False, index=False) # type: ignore # sqlalchemy.Column
-    model_name = Column(String(64), nullable=False, index=False) # type: ignore # sqlalchemy.Column
-    param_id = Column(String(64), nullable=False, index=False) # type: ignore # sqlalchemy.Column
-    data_id = Column(String(64), nullable=False, index=False) # type: ignore # sqlalchemy.Column
+    if TYPE_CHECKING:
+        __table__: Table
+        __tablename__: str
+    
+    instance_id = Column(String(64), primary_key=True, nullable=False)
+    tag = Column(String(64), nullable=False, index=False)
+    git_hash = Column(String(40), nullable=False, index=False)
+    fit_start_time = Column(TIMESTAMP, primary_key=False, nullable=False)
+    cluster_id = Column(String(64), nullable=False, index=False)
+    model_name = Column(String(64), nullable=False, index=False)
+    param_id = Column(String(64), nullable=False, index=False)
+    data_id = Column(String(64), nullable=False, index=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cols = [c.name for c in self.__table__.columns]  # pylint: disable=no-member
         vals = ["{}='{}'".format(column, getattr(self, column)) for column in cols]
         return "<" + self.__tablename__ + "(" + ", ".join(vals)
@@ -27,11 +33,15 @@ class InstanceTableMixin:
 class ModelTableMixin:
     """Table of model parameters."""
 
+    if TYPE_CHECKING:
+        __table__: Table
+        __tablename__: str
+
     model_name = Column(String(64), primary_key=True, nullable=False)
     param_id = Column(String(64), primary_key=True, nullable=False)
     model_param = Column(JSONB, nullable=False, index=True)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cols = [c.name for c in self.__table__.columns]
         vals = ["{}='{}'".format(column, getattr(self, column)) for column in cols]
         return "<" + self.__tablename__ + "(" + ", ".join(vals)
@@ -40,12 +50,16 @@ class ModelTableMixin:
 class DataTableMixin:
     """Table of model parameters."""
 
+    if TYPE_CHECKING:
+        __table__: Table
+        __tablename__: str
+
     data_id = Column(String(64), primary_key=True, nullable=False)
     # we might be able to build an index on certain keys, but not the whole column :(
     data_config = Column(JSONB, nullable=False, index=False)
     preprocessing = Column(JSONB, nullable=False, index=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cols = [c.name for c in self.__table__.columns]
         vals = ["{}='{}'".format(column, getattr(self, column)) for column in cols]
         return "<" + self.__tablename__ + "(" + ", ".join(vals)
@@ -54,12 +68,15 @@ class DataTableMixin:
 class MetricsTableMixin:
     """Table for model metrics."""
 
+    if TYPE_CHECKING:
+        __table__: Table
+
     __tablename__ = "metrics"
 
-    instance_id = Column(String(64), primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
-    data_id = Column(String(64), primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
+    instance_id = Column(String(64), primary_key=True, nullable=False)
+    data_id = Column(String(64), primary_key=True, nullable=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cols = [c.name for c in self.__table__.columns]  # pylint: disable=no-member
         vals = ["{}='{}'".format(column, getattr(self, column)) for column in cols]
         return "<" + self.__tablename__ + "(" + ", ".join(vals)
@@ -68,15 +85,18 @@ class MetricsTableMixin:
 class ResultTableMixin:
     """Table mixin for model results."""
 
+    if TYPE_CHECKING:
+        __table__: Table
+
     __tablename__ = "result"
 
-    instance_id = Column(String(64), primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
-    data_id = Column(String(64), primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
+    instance_id = Column(String(64), primary_key=True, nullable=False)
+    data_id = Column(String(64), primary_key=True, nullable=False)
 
-    point_id = Column(UUID, primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
-    measurement_start_utc = Column(TIMESTAMP, primary_key=True, nullable=False) # type: ignore # sqlalchemy.Column
+    point_id = Column(UUID, primary_key=True, nullable=False)
+    measurement_start_utc = Column(TIMESTAMP, primary_key=True, nullable=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cols = [c.name for c in self.__table__.columns]  # pylint: disable=no-member
         vals = ["{}='{}'".format(column, getattr(self, column)) for column in cols]
         return "<" + self.__tablename__ + "(" + ", ".join(vals)
