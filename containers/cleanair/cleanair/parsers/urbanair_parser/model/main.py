73bb4fb777f29518cf464fc06cc63a6cea42edbf
Oscar Giles <ogiles@turing.ac.uk>
2020-07-21 14:51:06 +0100

    :construction: Add deep-gp entrypoint

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/main.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/main.py
@@ -1,11 +1,12 @@
 """Parser for model fitting."""
 
 import typer
-from . import model_data_cli, svgp_cli
+from . import model_data_cli, svgp_cli, deep_gp_cli
 
 app = typer.Typer()
 app.add_typer(model_data_cli.app, name="data")
 app.add_typer(svgp_cli.app, name="svgp")
+app.add_typer(deep_gp_cli.app, name="deep-gp")
 
 if __name__ == "__main__":
     app()
