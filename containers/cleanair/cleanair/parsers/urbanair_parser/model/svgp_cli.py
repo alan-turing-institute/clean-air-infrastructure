91e91055338a14f7b2d9aa412737655384f71761
PatrickOHara <pohara@turing.ac.uk>
2020-07-22 15:49:02 +0100

    Loading from files.

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/svgp_cli.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/svgp_cli.py
@@ -30,10 +30,8 @@ from ..shared_args import (
     UpTo_callback,
 )
 from ..shared_args.dataset_options import HexGrid
-from ..shared_args.instance_options import ClusterId, Tag
 from ..shared_args.model_options import MaxIter
-from ....instance import AirQualityInstance, AirQualityModelParams
-from ....models import ModelData, SVGP, ModelConfig
+
 from ....types import (
     Species,
     Source,
@@ -51,64 +49,24 @@ app = typer.Typer(help="SVGP model fitting")
 @app.command()
 def fit(
     input_dir: Path = typer.Argument(None),
-    cluster_id: str = "laptop",
+    kernel: str = "matern32",
     maxiter: int = MaxIter,
-    tag: str = Tag,
 ) -> None:
     """Train a model loading data from INPUT-DIR
 
     If INPUT-DIR not provided will try to load data from the urbanair CLI cache
 
     INPUT-DIR should be created by running 'urbanair model data save-cache'"""
-
     # 1. Load data and configuration file
-    # 2, Create model params
-    # 3. Fit model
-    # 4. Upload model params to blob storage/database (where in blob storage parameters are)
-    # 5. Run prediction.
-    # 6. Upload to database.
-
-    # Load training data
     X_train, Y_train, _ = get_training_arrays(input_dir)
     full_config = load_model_config(input_dir, full=True)
 
-    # Create the model
-    secretfile: str = state["secretfile"]
-    model = SVGP(batch_size=1000)  # big batch size for the grid
+    # 2. Create model params
+    model = SVGP(batch_size=1000, tasks=full_config.species)
     model.model_params["maxiter"] = maxiter
-    model.model_params["kernel"]["name"] = "matern32"
+    model.model_params["kernel"]["name"] = kernel
 
-    #  load the dataset
-    aq_model_params = AirQualityModelParams(secretfile, "svgp", model.model_params)
-    # dataset = ModelData(secretfile=secretfile)
-
-    # instance for training and forecasting air quality
-
-    fit_start_time = datetime.utcnow().isoformat()
-
-    svgp_instance = AirQualityInstance(
-        model_name="svgp",
-        param_id=aq_model_params.param_id,
-        data_id=full_config.data_id(),
-        cluster_id=cluster_id,
-        tag=tag,
-        fit_start_time=fit_start_time,
-        secretfile=secretfile,
-    )
-
-    # train and forecast the model
-    # svgp_instance.train(model, dataset)
-
+    # 3. Fit model
     model.fit(X_train, Y_train)
 
-    # result = svgp_instance.forecast(model, dataset, secretfile=secretfile)
-
-    #
-    # dataset.update_remote_tables()  # write the data id & settings to DB
-
-    # object for inserting model parameters into the database
-    # aq_model_params = AirQualityModelParams(secretfile, "svgp", model.model_params)
-    # aq_model_params.update_remote_tables()  # write model name, id & params to DB
-
-    # svgp_instance.update_remote_tables()  # write the instance to the DB
-    # result.update_remote_tables()  # write results to DB
+    # 4. Predict
---
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/update_cli.py
@@ -0,0 +1,79 @@
+"""Command line interface for updating the database with results."""
+
+
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
+import pandas as pd
+import typer
+from .model_data_cli import load_model_config
+from ..state import state
+from ..state.configuration import (
+    FORECAST_RESULTS_CSV,
+    MODEL_PARAMS,
+    TRAINING_RESULTS_CSV,
+)
+from ..shared_args.instance_options import ClusterId, Tag
+from ....instance import AirQualityInstance, AirQualityResult
+from ....types import ModelParams
+
+app = typer.Typer(help="Update database with model fit.")
+
+@app.command()
+def results(
+    input_dir: Path = typer.Argument(None),
+    cluster_id: str = ClusterId,
+    tag: str = Tag,
+):
+    """Update the results to the database."""
+    # TODO check input directory exists
+
+    # load files
+    model_params = load_model_params(input_dir)
+    result_df = load_forecast_result_df(input_dir)
+    full_config = load_model_config(input_dir, full=True)
+
+    secretfile: str = state["secretfile"]
+    instance = AirQualityInstance(
+        model_name=model_params["model_name"],
+        param_id=model_params.param_id, # TODO this will break
+        data_id=full_config.data_id(),
+        cluster_id=cluster_id,
+        tag=tag,
+        fit_start_time=datetime.utcnow().isoformat(),
+        secretfile=secretfile,
+    )
+    # TODO create method for writing model parameters to DB
+    # TODO create method for writing data config to DB
+    result = AirQualityResult(instance.instance_id, instance.data_id, result_df, secretfile)
+    instance.update_remote_tables()  # write the instance to the DB
+    result.update_remote_tables()  # write results to DB
+
+def __load_result_df(
+    input_dir: Optional[Path] = None,
+    result_csv_path: Optional[Path] = None
+) -> pd.DataFrame:
+    """Load a results dataframe."""
+    if not input_dir:
+        result_fp = result_csv_path
+    else:
+        result_fp = input_dir.joinpath(*result_csv_path.parts[-2:])
+    return pd.read_csv(result_fp)
+
+def load_training_result_df(input_dir: Optional[Path] = None) -> pd.DataFrame:
+    """Load the predictions on the training set from a csv."""
+    return __load_result_df(input_dir, TRAINING_RESULTS_CSV)
+
+def load_forecast_result_df(input_dir: Optional[Path] = None) -> pd.DataFrame:
+    """Load the predictions on the forecast set from a csv."""
+    return __load_result_df(input_dir, FORECAST_RESULTS_CSV)
+
+def load_model_params(input_dir: Optional[Path] = None) -> ModelParams:
+    """Load the model params from a json file."""
+    if not input_dir:
+        params_fp = MODEL_PARAMS
+    else:
+        params_fp = input_dir.joinpath(*MODEL_PARAMS.parts[-1:])
+    with open(params_fp, "r") as params_file:
+        return json.load(params_file)
