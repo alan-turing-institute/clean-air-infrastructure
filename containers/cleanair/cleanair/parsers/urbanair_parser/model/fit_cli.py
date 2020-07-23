d995ec5a59247849e4a178791b2f08a0022e54f0
PatrickOHara <pohara@turing.ac.uk>
2020-07-23 14:12:22 +0100

    Add options for fitting.

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/fit_cli.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/fit_cli.py
@@ -1,31 +1,42 @@
 """Commands for a Sparse Variational GP to model air quality."""
+from typing import Optional
 import typer
 from pathlib import Path
 from ..shared_args.model_options import MaxIter
 from .model_data_cli import load_model_config, get_training_arrays
+from .update_cli import load_model_params
 from ....models import SVGP, ModelMixin
+from ....types import ModelParams
 
 app = typer.Typer(help="SVGP model fitting")
 
 @app.command()
-def svgp()
-
-    run_model_fitting(model, input_dir)
-
-def (
-    model: ModelMixin,
-    input_dir: Path,
+def fit(
+    model_name: str,
+    input_dir: Path = typer.Argument(None),
+    batch_size: int = typer.Option(default=100, help="Size of batches for prediction."),
+    refresh: int = typer.Option(default=10, help="Frequency of printing ELBO."),
+    restore: bool = typer.Option(default=False, help="Restore the model state from cache."),
 ) -> None:
     """Train a model loading data from INPUT-DIR
 
     If INPUT-DIR not provided will try to load data from the urbanair CLI cache
 
     INPUT-DIR should be created by running 'urbanair model data save-cache'"""
+
     # Load data and configuration file
     X_train, Y_train, _ = get_training_arrays(input_dir)
     full_config = load_model_config(input_dir, full=True)
 
+    # Load model params from file and create model
+    model_params = load_model_params(input_dir)
+    if model_name == "svgp":
+        model = SVGP(model_params=model_params, tasks=full_config.species)
+    elif model_name == "deepgp":
+        raise NotImplementedError("Need to implement Deep GP.")
+
     # Fit model
     model.fit(X_train, Y_train)
 
-    # 4. Predict
+    # TODO Prediction
+    
--- containers/cleanair/cleanair/parsers/urbanair_parser/model/setup_cli.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/setup_cli.py
@@ -1,10 +1,18 @@
+"""Save model parameters to a json file."""
+
+import json
+from typing import Optional
 import typer
 from pathlib import Path
 from ..shared_args.model_options import MaxIter
 from .model_data_cli import load_model_config, get_training_arrays
+from ..state.configuration import MODEL_PARAMS
 from ....models import SVGP, ModelMixin
+from ....types import ModelParams
+
 
 app = typer.Typer(help="SVGP model fitting")
+
 
 @app.command()
 def svgp(
@@ -17,3 +25,12 @@ def svgp(
     model = SVGP(batch_size=1000, tasks=full_config.species)
     model.model_params["maxiter"] = maxiter
     model.model_params["kernel"]["name"] = kernel
+
+def save_model_params(model_params: ModelParams, input_dir: Optional[Path] = None) -> None:
+    """Load the model params from a json file."""
+    if not input_dir:
+        params_fp = MODEL_PARAMS
+    else:
+        params_fp = input_dir.joinpath(*MODEL_PARAMS.parts[-1:])
+    with open(params_fp, "w") as params_file:
+        json.dump(model_params, params_file)
