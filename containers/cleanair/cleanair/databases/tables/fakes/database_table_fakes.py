36491a0800b251c33e35492644edf0617a322d26
ChristyNou <christynak12@gmail.com>
2020-07-28 17:13:51 +0100

    fix self variable errors

--- containers/cleanair/cleanair/databases/tables/fakes/database_table_fakes.py
+++ containers/cleanair/cleanair/databases/tables/fakes/database_table_fakes.py
@@ -1,13 +1,15 @@
 """Fake data generators which can be inserted into database"""
 
 import random
+
 from typing import Optional
 import string
 import uuid
 from datetime import datetime, date, timedelta
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Json
 from scipy.stats import uniform, norm
 import numpy as np
+from ....utils.hashing import hash_fn
 from ....types import Source, FeatureNames
 
 def get_random_string(length: int) -> str:
@@ -25,10 +27,10 @@ def gen_point_id(v: Optional[uuid.UUID]) -> uuid.UUID:
 
 
 def gen_site_code(v) -> str:
-   "Random site code"
+    "Random site code"
     if v:
         return v
-    return _get_random_string(4)
+    return get_random_string(4)
 
 
 def gen_hash_id(v) -> str:
@@ -52,7 +54,7 @@ class MetaPointSchema(BaseModel):
     location: Optional[str]
 
     _gen_point_id = validator("id", allow_reuse=True, always=True)(gen_point_id)
-
+    #pylint: disable=E0213,R0201
     @validator("location", always=True)
     def gen_location(cls, v):
         "Random location"
@@ -89,7 +91,7 @@ class LAQNReadingSchema(BaseModel):
     value: Optional[float]
 
     _gen_value = validator("value", always=True, allow_reuse=True)(gen_norm_value)
-
+    #pylint: disable=E0213,R0201
     @validator("measurement_end_utc", always=True)
     def gen_measurement_end_time(cls, v, values):
         "Generate end time one hour after start time"
@@ -144,7 +146,7 @@ class AirQualityModelSchema(BaseModel):
     param_id: Optional[str]
 
     _gen_hash_id = validator("param_id", allow_reuse=True, always=True)(gen_hash_id)
-
+    #pylint: disable=E0213,R0201
     @validator("model_param", always=True)
     def gen_model_param(cls, v):
         "Generate empty parammeters"
@@ -160,7 +162,7 @@ class AirQualityDataSchema(BaseModel):
     preprocessing: Optional[Json]
 
     _gen_hash_id = validator("data_id", allow_reuse=True, always=True)(gen_hash_id)
-   
+    #pylint: disable=E0213,R0201
     @validator("data_config", always=True)
     def gen_data_config(cls, v):
         "Generate an empty data config"
