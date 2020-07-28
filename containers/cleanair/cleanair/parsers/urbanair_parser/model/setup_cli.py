9ff0155b14d0f2965d1d1cacbc10d27a5712329d
PatrickOHara <pohara@turing.ac.uk>
2020-07-28 15:11:02 +0100

    🐛 Added params to the setup CLI for deep gp

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/setup_cli.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/setup_cli.py
@@ -16,7 +16,7 @@ from ..shared_args.model_options import (
     NumInducingPoints,
 )
 from ..state.configuration import MODEL_PARAMS
-from ....types.model_types import KernelParams, SVGPParams
+from ....types.model_types import KernelParams, SVGPParams, MRDGPParams
 
 app = typer.Typer(help="Setup model parameters.")
 
@@ -50,6 +50,68 @@ def svgp(
 
     # Save model parameters
     save_model_params(model_params, input_dir=input_dir)
+
+@app.command()
+def mrdgp(
+    input_dir: Path = typer.Argument(None),
+    maxiter: int = MaxIter,
+) -> None:
+    """Create model params for Deep GP."""
+    params_dict = {
+            "base_laqn": {
+                "kernel": {
+                    "name": "MR_SE_LAQN_BASE",
+                    "type": "se",
+                    "active_dims": [0, 1, 2],  # epoch, lat, lon,
+                    "lengthscales": [0.1, 0.1, 0, 1],
+                    "variance": [1.0, 1.0, 1.0],
+                },
+                "num_inducing_points": 300,
+                "minibatch_size": 100,
+                "likelihood_variance": 0.1,
+                "maxiter": maxiter,
+            },
+            "base_sat": {
+                "kernel": {
+                    "name": "MR_SE_SAT_BASE",
+                    "type": "se",
+                    "active_dims": [0, 1, 2],  # epoch, lat, lon,
+                    "lengthscales": [0.1, 0.1, 0, 1],
+                    "variance": [1.0, 1.0, 1.0],
+                },
+                "num_inducing_points": 300,
+                "minibatch_size": 100,
+                "likelihood_variance": 0.1,
+                "maxiter": maxiter,
+            },
+            "dgp_sat": {
+                "kernel": [
+                    {
+                        "name": "MR_LINEAR_SAT_DGP",
+                        "type": "linear",
+                        "active_dims": [0],  # previous GP, lat, lon,
+                        "variance": [1.0],
+                    },
+                    {
+                        "name": "MR_SE_SAT_DGP",
+                        "type": "se",
+                        "active_dims": [2, 3],  # previous GP, lat, lon,
+                        "lengthscales": [0.1, 0, 1],
+                        "variance": [1.0, 1.0],
+                    },
+                ],
+                "num_inducing_points": 300,
+                "minibatch_size": 100,
+                "likelihood_variance": 0.1,
+                "maxiter": maxiter,
+            },
+            "mixing_weight": {"name": "dgp_only", "param": None},
+            "num_samples_between_layers": 1,
+            "num_prediction_samples": 1,
+        }
+    model_params = MRDGPParams(**params_dict)
+    save_model_params(model_params, input_dir=input_dir)
+
 
 def save_model_params(model_params: BaseModel, input_dir: Optional[Path] = None) -> None:
     """Load the model params from a json file."""
