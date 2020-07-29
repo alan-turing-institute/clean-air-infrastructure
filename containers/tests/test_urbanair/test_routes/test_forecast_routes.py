bf45640135bbe8db750669d1c9f200b882440fb9
ChristyNou <christynak12@gmail.com>
2020-07-29 18:16:14 +0100

    remove extra test_model_results

--- containers/tests/test_urbanair/test_routes/test_forecast_routes.py
+++ containers/tests/test_urbanair/test_routes/test_forecast_routes.py
@@ -59,18 +59,6 @@ class TestAdvanced:
             "/api/v1/forecasts/forecast_model_results/", params={"instance_id": instance_id},
         )
         assert response.status_code == 200
-        data = response.json()
-        print (data)
-
-    def test_model_result(self, client_class, forecast_stat_records):
-
-        "Test forecast result info API"
-        instance_id = forecast_stat_records[4][0].instance_id
-        
-        response = client_class.get(
-            "/api/v1/forecasts/forecast_model_results/", params={"instance_id": instance_id},
-        )
-        assert response.status_code == 200
         
 
     def test_result(self, client_class, forecast_stat_records):
