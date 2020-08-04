3c00d332fe213fa7f249bb745be95566b6a50a70
Oscar Giles <ogiles@turing.ac.uk>
2020-08-04 15:14:20 +0100

    :wheel_of_dharma: Setting up API odysseus routes with path stripping

--- containers/urbanair/urbanair/odysseus.py
+++ containers/urbanair/urbanair/odysseus.py
@@ -10,5 +10,13 @@ app = FastAPI(
     title="Odysseus API", description="Project Odysseus API", version="0.0.1",
 )
 
+app.mount(
+    "/static",
+    StaticFiles(
+        directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+    ),
+    name="static",
+)
+
 app.include_router(static.router)
 app.include_router(jamcam.router, prefix="/api/v1/jamcams", tags=["jamcam"])
