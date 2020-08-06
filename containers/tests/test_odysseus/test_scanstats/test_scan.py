27de539d26d948fafcc830fef626843ff3ad242d
Chance Haycock <chancehaycock@me.com>
2020-08-06 15:12:51 +0100

    Fix tests with forecast argument re-factor

--- containers/tests/test_odysseus/test_scanstats/test_scan.py
+++ containers/tests/test_odysseus/test_scanstats/test_scan.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 import datetime
+from datetime import timedelta
 
-import numpy as np
 import pandas as pd
 
 from odysseus.scanstat.preprocess import preprocessor
@@ -19,9 +19,7 @@ if TYPE_CHECKING:
 def test_scan(scan_scoot: ScanScoot) -> None:
     """Test whole pipeline of scan functions with settings below."""
 
-    days_in_past = int(scan_scoot.train_hours / 24)
-    days_in_future = int(scan_scoot.forecast_hours / 24)
-    ts_method = scan_scoot.model_name
+    # Set up quick alias
     readings = scan_scoot.readings
     print(readings)
 
@@ -38,14 +36,15 @@ def test_scan(scan_scoot: ScanScoot) -> None:
     # Update the number of detectors for the rest of the test - some are thrown
     # away in the pre-process stage.
     init_num_detectors = len(proc_df["detector_id"].unique())
-    t_max = proc_df["measurement_end_utc"].max()
 
     # 2) Produce forecast
     forecast_df = forecast(
         proc_df,
-        days_in_past=days_in_past,
-        days_in_future=days_in_future,
-        method=ts_method,
+        scan_scoot.train_start,
+        scan_scoot.train_upto,
+        scan_scoot.forecast_start,
+        scan_scoot.forecast_upto,
+        method=scan_scoot.model_name,
     )
     print(forecast_df)
 
@@ -61,7 +60,7 @@ def test_scan(scan_scoot: ScanScoot) -> None:
     agg_df = aggregate_readings_to_grid(forecast_df)
     print(agg_df)
 
-    aggregate_checks(agg_df, days_in_future, scan_scoot.grid_resolution)
+    aggregate_checks(agg_df, scan_scoot.forecast_hours, scan_scoot.grid_resolution)
 
     # 4) Scan
     all_scores = scan(
@@ -71,7 +70,7 @@ def test_scan(scan_scoot: ScanScoot) -> None:
         scan_scoot.forecast_upto,
     )
 
-    scan_checks(all_scores, t_max, days_in_future, scan_scoot.grid_resolution)
+    scan_checks(all_scores, scan_scoot.forecast_upto, scan_scoot.forecast_days, scan_scoot.grid_resolution)
     print(all_scores)
 
     # 5) Aggregate scores to gridcell level using the average
@@ -82,7 +81,7 @@ def test_scan(scan_scoot: ScanScoot) -> None:
         scan_scoot.forecast_upto,
     )
 
-    average_score_checks(grid_level_scores, days_in_future, scan_scoot.grid_resolution)
+    average_score_checks(grid_level_scores, scan_scoot.forecast_hours, scan_scoot.grid_resolution)
     print(grid_level_scores)
 
     return
@@ -150,6 +149,8 @@ def forecast_checks(
         "measurement_end_utc",
         "count",
         "baseline",
+        "baseline_lower",
+        "baseline_upper",
     ]
     assert set(cols) == set(forecast_df.columns)
 
@@ -166,7 +167,7 @@ def forecast_checks(
 
 
 def aggregate_checks(
-    agg_df: pd.DataFrame, days_in_future: int, grid_resolution: int
+    agg_df: pd.DataFrame, forecast_hours: int, grid_resolution: int
 ) -> None:
     """Test that individual detector data is aggregated correctly to each grid
     cell using `aggregate_to_grid()`"""
@@ -192,7 +193,7 @@ def aggregate_checks(
     min_readings = agg_df.groupby(["row", "col"])["measurement_start_utc"].count().min()
     max_readings = agg_df.groupby(["row", "col"])["measurement_start_utc"].count().max()
     assert min_readings == max_readings
-    assert min_readings == 24 * days_in_future
+    assert min_readings == forecast_hours
 
     # Check that row and col numbers fall within range
     assert agg_df["row"].min() >= 1
@@ -203,8 +204,8 @@ def aggregate_checks(
 
 def scan_checks(
     all_scores: pd.DataFrame,
-    t_max: datetime,
-    days_in_future: int,
+    forecast_upto: datetime,
+    forecast_days: int,
     grid_resolution: int,
 ) -> None:
     """ Test that output from the main `scan()` function is sensible."""
@@ -240,23 +241,21 @@ def scan_checks(
     ) - 1
 
     assert len(all_scores["measurement_end_utc"].unique()) == 1
-    assert all_scores.at[0, "measurement_end_utc"] == t_max
-    assert all_scores["measurement_start_utc"].min() == t_max - np.timedelta64(
-        days_in_future, "D"
-    )
+    assert all_scores.at[0, "measurement_end_utc"] == forecast_upto
+    assert all_scores["measurement_start_utc"].min() == forecast_upto - timedelta(days=forecast_days)
 
     assert all_scores["l_score_ebp"].min() >= 1
 
 
 def average_score_checks(
-    grid_level_scores: pd.DataFrame, days_in_future: int, grid_resolution: int,
+    grid_level_scores: pd.DataFrame, forecast_hours: int, grid_resolution: int,
 ) -> None:
     """ Test that output from the main `average_gridcell_scores()` function is sensible."""
 
     # All outputted values should not be NaN
     assert not grid_level_scores.isnull().values.any()
 
-    assert len(grid_level_scores) == 24 * days_in_future * grid_resolution ** 2
+    assert len(grid_level_scores) == forecast_hours * grid_resolution ** 2
 
     grid_level_cols = [
         "measurement_start_utc",
