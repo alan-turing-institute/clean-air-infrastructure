a3ec7130b9e712155d91809e9ce8d318af2a0579
Chance Haycock <chancehaycock@me.com>
2020-08-07 10:54:16 +0100

    Add new metrics

--- containers/odysseus/odysseus/scanstat/metrics.py
+++ containers/odysseus/odysseus/scanstat/metrics.py
@@ -7,7 +7,7 @@ of the event"""
 import numpy as np
 
 
-def likelihood_ratio_ebp(baseline_count: float, actual_count: float) -> float:
+def ebp(baseline_count: float, actual_count: float) -> float:
     """Simple Expression for the likelihood ratio
     Args:
         baseline_count: Sum of baseline counts in space-time region S
@@ -15,10 +15,70 @@ def likelihood_ratio_ebp(baseline_count: float, actual_count: float) -> float:
     Returns:
         likelihood ratio score
     """
+
+    if actual_count < 0 or baseline_count < 0:
+        raise ValueError("Negative count value passed in scan")
+
+    # F(S) defaults to 1
     if baseline_count == 0:
         return 1.0
     if actual_count > baseline_count:
         return np.power((actual_count / baseline_count), actual_count) * np.exp(
             baseline_count - actual_count
+        )
+    return 1.0
+
+
+def ebp_asym(baseline_count: float, actual_count: float) -> float:
+
+    """Extension to the original EBP likelihood ratio
+    Args:
+        baseline_count: Sum of baseline counts in space-time region S
+        actual_count: Sum of actual counts in space-time region S
+    Returns:
+        Asymmetric likelihood ratio score
+    """
+
+    if actual_count < 0 or baseline_count < 0:
+        raise ValueError("Negative count value passed in scan")
+
+    if baseline_count == 0:
+        return 0.0
+    if actual_count >= baseline_count:
+        return (
+            np.power((actual_count / baseline_count), actual_count)
+            * np.exp(baseline_count - actual_count)
+            - 1
+        )
+    return 1 - np.power((actual_count / baseline_count), actual_count) * np.exp(
+        baseline_count - actual_count
+    )
+
+
+def kulldorf(
+    baseline_count_in: float,
+    baseline_count_total: float,
+    actual_count_in: float,
+    actual_count_total: float,
+) -> float:
+
+    if (
+        actual_count_in < 0
+        or baseline_count_in < 0
+        or baseline_count_total < 0
+        or actual_count_total < 0
+    ):
+        raise ValueError("Negative count value passed in scan")
+
+    actual_count_out = actual_count_total - actual_count_in
+    baseline_count_out = baseline_count_total - baseline_count_in
+
+    if baseline_count_in == 0 or baseline_count_out == 0:
+        return 1.0
+    if (actual_count_in / baseline_count_in) > (actual_count_out / baseline_count_out):
+        return (
+            np.power(actual_count_in / baseline_count_in, actual_count_in)
+            * np.power(actual_count_out / baseline_count_out, actual_count_out)
+            * np.power(actual_count_total / baseline_count_total, -actual_count_total)
         )
     return 1.0
