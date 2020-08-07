09441e6549b9437469d9dec934696d61ae7b3377
Chance Haycock <chancehaycock@me.com>
2020-08-07 15:26:32 +0100

    Document metrics

--- containers/odysseus/odysseus/scanstat/metrics.py
+++ containers/odysseus/odysseus/scanstat/metrics.py
@@ -31,7 +31,11 @@ def ebp(baseline_count: float, actual_count: float) -> float:
 
 def ebp_asym(baseline_count: float, actual_count: float) -> float:
 
-    """Extension to the original EBP likelihood ratio
+    """Extension to the original EBP likelihood ratio which aso assigns
+    meaningful scores to space-time regions whose actual activity is
+    QUIETER than what is expected. If this is the case, this represented
+    with a negative score. Note that this metric is centred about 0, whereas
+    the original EBP (`ebp`) has a minimum value of 1.
     Args:
         baseline_count: Sum of baseline counts in space-time region S
         actual_count: Sum of actual counts in space-time region S
@@ -61,6 +65,23 @@ def kulldorf(
     actual_count_in: float,
     actual_count_total: float,
 ) -> float:
+
+    """Implementation of the original Kulldorf Spatial Scan metric.
+    This differs to the `ebp` metric as it takes into account a region's score
+    inside AND outside the region of interest. i.e. this metric aims to find
+    space-time regions which have the largest score inside it and the smallest
+    score outside of it. Depending on the application, this metric is sometimes
+    better at finding smaller regions of interest.
+    Args:
+        baseline_count_in: Sum of baseline predictions INSIDE the search region
+        baseline_count_total: Sum of baseline predictions over the whole space-time domain
+                              For a given scan, this is clearly constant
+        actual_count_in: Sum of actual counts INSIDE the search region
+        actual_count_total: Sum of actual countsover the whole space-time domain
+                              For a given scan, this is clearly constant
+    Returns:
+        Kulldorf Metric for space-time region.
+    """
 
     if (
         actual_count_in < 0
