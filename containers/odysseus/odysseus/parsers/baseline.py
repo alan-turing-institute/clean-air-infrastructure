2f5fd1ef9ae31709f0a36e4e5d2e8a78c9a0915f
PatrickOHara <pohara@turing.ac.uk>
2020-08-13 19:45:57 +0100

    🔥 Removed old parsers and entrypoints. Fixing bugs.

--- containers/odysseus/odysseus/parsers/baseline.py
+++ containers/odysseus/odysseus/parsers/baseline.py
@@ -1,65 +1,64 @@
-"""
-Calculate the percent of baseline metric for a recent day.
-"""
+
 from datetime import datetime
 from dateutil.parser import isoparse
+import typer
 from odysseus.scoot_processing import TrafficPercentageChange
 from odysseus.dates import (
     LOCKDOWN_BASELINE_END,
     NORMAL_BASELINE_END,
 )
-from odysseus.parsers import BaselineParser
+from cleanair.parsers.urbanair_parser.shared_args import (
+    NDays,
+    UpTo,
+)
+from cleanair.parsers.urbanair_parser.state import state
 
+app = typer.Typer(help="Percent of baseline.")
 
-def main():
-    """
-    Calculate the percent of baseline metric for a recent day.
-    """
+@app.command()
+def scoot(
+    backfill: bool = typer.Option(False),
+    ndays: int = NDays,
+    upto: str = UpTo,
+) -> None:
+    """Percent of baseline for scoot."""
+    secretfile: str = state["secretfile"]
 
-    # get args from parser d
-    parser = BaselineParser()
-    args = parser.parse_args()
-
-    if args.backfill:
+    if backfill:
         # Calculate how many days are backfillable
         ndays_lockdown = (datetime.today() - isoparse(LOCKDOWN_BASELINE_END)).days
         ndays_normal = (datetime.today() - isoparse(NORMAL_BASELINE_END)).days
 
         # get query object
         traffic_query_normal = TrafficPercentageChange(
-            secretfile=args.secretfile,
-            end=args.comparison_end_date,
+            secretfile=secretfile,
+            end=upto,
             nhours=(ndays_normal * 24) - 24,
             baseline_tag="normal",
         )
 
         traffic_query_lockdown = TrafficPercentageChange(
-            secretfile=args.secretfile,
-            end=args.comparison_end_date,
+            secretfile=secretfile,
+            end=upto,
             nhours=(ndays_lockdown * 24) - 24,
             baseline_tag="lockdown",
         )
 
     else:
-
         # get query object
         traffic_query_normal = TrafficPercentageChange(
-            secretfile=args.secretfile,
-            end=args.comparison_end_date,
-            nhours=(args.ndays * 24) - 24,
+            secretfile=secretfile,
+            end=upto,
+            nhours=ndays - 24,
             baseline_tag="normal",
         )
 
         traffic_query_lockdown = TrafficPercentageChange(
-            secretfile=args.secretfile,
-            end=args.comparison_end_date,
-            nhours=(args.ndays * 24) - 24,
+            secretfile=secretfile,
+            end=upto,
+            nhours=(ndays) - 24,
             baseline_tag="lockdown",
         )
 
     traffic_query_lockdown.update_remote_tables()
     traffic_query_normal.update_remote_tables()
-
-
-if __name__ == "__main__":
-    main()
