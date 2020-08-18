72d14696a9234ece9b807f2d15a73619ccfe8a3b
PatrickOHara <pohara@turing.ac.uk>
2020-08-18 11:42:14 +0100

    Write tests for scoot datasets.

---
+++ containers/tests/test_odysseus/test_dataset/conftest.py
@@ -0,0 +1,15 @@
+"""Fixtures for datasets."""
+
+from typing import List
+import pytest
+from odysseus.dataset import ScootConfig, ScootPreprocessing
+
+@pytest.fixture(scope="function")
+def scoot_config(detectors: List[str], train_start: str, train_upto: str) -> ScootConfig:
+    """Create a scoot data config pydantic model."""
+    return ScootConfig(detectors=detectors, start=train_start, upto=train_upto)
+
+@pytest.fixture(scope="function")
+def scoot_preprocessing() -> ScootPreprocessing:
+    """Preprocessing settings for scoot datasets."""
+    return ScootPreprocessing(features=["time"], target=["n_vehicles_in_interval"])
---
+++ containers/tests/test_odysseus/test_dataset/test_scoot_dataset.py
@@ -0,0 +1,23 @@
+"""Tests for the scoot dataset."""
+
+import pytest
+from cleanair.databases import Connector
+from cleanair.timestamps import as_datetime
+from odysseus.dataset import ScootConfig, ScootDataset, ScootPreprocessing
+
+def validate_scoot_dataset(dataset: ScootDataset) -> None:
+    """Run checks on a scoot dataset."""
+    nhours = (as_datetime(dataset.data_config.upto) - as_datetime(dataset.data_config.start)).days * 24
+    ndetectors = len(dataset.data_config.detectors)
+    assert nhours * ndetectors == len(dataset.dataframe)
+
+def test_scoot_dataset_init(secretfile: str, connection: Connector, scoot_config: ScootConfig, scoot_preprocessing: ScootPreprocessing) -> None:
+    """Test the init function of a scoot dataset."""
+
+    with pytest.raises(ValueError):
+        # if a dataframe or secretfile not passed then value error should be raised
+        ScootDataset(scoot_config, scoot_preprocessing)
+
+    # load dataset from database
+    dataset = ScootDataset(scoot_config, scoot_preprocessing, secretfile=secretfile, connection=connection)
+    validate_scoot_dataset(dataset)
