28a389c79c3e73564ffb1e6e20331dbbc9de4d97
PatrickOHara <pohara@turing.ac.uk>
2020-08-20 20:31:56 +0100

    ✨ Load experiment from configs and update tables.

--- containers/odysseus/odysseus/experiment/experiment.py
+++ containers/odysseus/odysseus/experiment/experiment.py
@@ -5,7 +5,7 @@ Experiments summerise multiple instances.
 from abc import abstractmethod
 from typing import Optional
 import pandas as pd
-from cleanair.databases.mixins import InstanceTableMixin, ModelTableMixin
+from cleanair.databases.mixins import DataTableMixin, InstanceTableMixin, ModelTableMixin
 from .traffic_instance import TrafficInstance
 
 
@@ -55,11 +55,28 @@ class ExperimentMixin:
     def model_table(self) -> ModelTableMixin:
         """The modelling table."""
 
+    @property
+    @abstractmethod
+    def data_config_table(self) -> DataTableMixin:
+        """The data config table."""
+
     def add_instance(self, instance: TrafficInstance):
         """ToDo."""
 
     def update_remote_tables(self):
         """Update the instance, data and model tables."""
+        # update the model params table
+        model_records = self.frame[["model_name", "model_params", "param_id"]].to_dict("records")
+        self.commit_records(
+            model_records, on_conflict="overwrite", table=self.model_table,
+        )
+
+        # update the data config table
+        data_records = self.frame[["data_id", "data_config", "preprocessing"]].to_dict("records")
+        self.commit_records(
+            data_records, on_conflict="overwrite", table=self.data_config_table,
+        )
+        # update the instance table
         site_records = self.frame.to_dict("records")
         self.commit_records(
             site_records, on_conflict="overwrite", table=self.instance_table,
