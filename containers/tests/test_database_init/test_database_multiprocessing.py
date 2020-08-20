8d7fd36be5a93324f84af3fdf657cbaee5b21015
Oscar Giles <ogiles@turing.ac.uk>
2020-08-20 14:00:26 +0100

    :white_check_mark: Fix tests

--- containers/tests/test_database_init/test_database_multiprocessing.py
+++ containers/tests/test_database_init/test_database_multiprocessing.py
@@ -23,16 +23,11 @@ def example_db_function(secretfile, connection):
     return f
 
 
-def test_threadpool(example_db_function):
+@pytest.mark.parametrize("i", [1, 2])
+def test_threadpool(example_db_function, i):
     "Check we get the same session within a thread"
     with ThreadPoolExecutor(max_workers=4) as executor:
         a = executor.submit(example_db_function)
-        b = executor.submit(example_db_function)
-
         output_a = set(a.result())
-        output_b = set(b.result())
 
         assert len(output_a) == 1
-        assert len(output_b) == 1
-
-        print(list(output_a)[0] != list(output_b)[0])
