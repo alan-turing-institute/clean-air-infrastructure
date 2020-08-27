4e9eaad24f0f36940f4247703d314b6a62502026
PatrickOHara <pohara@turing.ac.uk>
2020-08-27 09:25:47 +0100

    Deleted old parsers

--- containers/odysseus/odysseus/parsers/metrics_cli.py
+++ containers/odysseus/odysseus/parsers/metrics_cli.py
@@ -1,10 +1,17 @@
+"""Evaluate metrics on odysseus models."""
 
 import typer
 
 app = typer.Typer()
-scoot_app = typer.Typer()
-app.add_typer(scoot_app)
 
-@scoot_app.command()
-def gpr() -> None:
-    """Evaluate a Gaussian Process Regression model on scoot."""
+@app.command()
+def scoot() -> None:
+    """Evaluate a Gaussian Process Regression model on scoot.""" 
+
+    # TODO 1. get experiment class from the tag and fit_start_time
+    # TODO 2. using the instance ids get all the forecasts for each of those instance ids
+    # TODO 3. look up the data id in the data_config table from the forecast table using instance id
+    # TODO 4. from the data id get the data config and load the datasets
+    # TODO 5. calculate the metrics between the forecast and observations
+    # TODO 6. write the metrics to the database using the instance id and data id
+    raise NotImplementedError("See TODO items in comments.")
