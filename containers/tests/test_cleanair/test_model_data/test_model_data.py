785bc75e0452e3034356c88eae988a4e42275fb1
Oscar Giles <ogiles@turing.ac.uk>
2020-09-08 11:24:39 +0100

    :rewind: Commenting out tests not used as require refactor later
    Refactor these tests when bringing in ModelData config

---
+++ containers/tests/test_cleanair/test_model_data/test_model_data.py
@@ -0,0 +1,71 @@
+"""
+Given a model data object, check that the data matches the config.
+"""
+
+# ToDo: Refactor when ModelData class if refactored
+
+# from cleanair.types import Species, TargetDict
+
+
+# def test_training_dicts(model_data):
+#     """Check the shape of all the numpy arrays are correct."""
+#     # load the dicts from modeldata
+#     training_data_dict = model_data.get_training_data_arrays(dropna=False)
+#     x_train = training_data_dict["X"]
+#     y_train = training_data_dict["Y"]
+
+#     # checks for satellite
+#     assert not model_data.config["include_satellite"] or "satellite" in x_train
+#     assert not model_data.config["include_satellite"] or "satellite" in y_train
+
+#     # checks that each pollutant has a key in y_train for each source
+#     all_train_sources = (
+#         model_data.config["train_sources"]
+#         if not model_data.config["include_satellite"]
+#         else model_data.config["train_sources"] + ["satellite"]
+#     )
+#     for source in all_train_sources:
+#         # check all training sources exist in the dicts
+#         assert source in x_train
+#         assert source in y_train
+
+#         # check the shape of x_train
+#         if source == "satellite":
+#             assert len(x_train[source].shape) == 3
+#         else:
+#             assert len(x_train[source].shape) == 2
+
+#         # check y_train
+#         for pollutant in model_data.config["species"]:
+#             assert y_train[source][pollutant].shape[0] == x_train[source].shape[0]
+#         validate_target(y_train[source])
+
+
+# def test_pred_dict(model_data):
+#     """
+#     Check the test set is in the right format.
+#     """
+#     predict_data_dict = model_data.get_pred_data_arrays(dropna=False, return_y=True)
+#     x_test = predict_data_dict["X"]
+#     y_test = predict_data_dict["Y"]
+#     assert "satellite" not in x_test
+
+#     for source in model_data.config["pred_sources"]:
+#         # check x test
+#         assert source in x_test
+#         assert len(x_test[source].shape) == 2
+#         assert x_test[source].shape[1] == len(model_data.config["x_names"])
+
+#         # check y test
+#         assert source in y_test
+#         assert len(model_data.config["species"]) == len(y_test[source])
+#         validate_target(y_test[source])
+
+
+# def validate_target(Y: TargetDict) -> bool:
+#     """Check the shapes and structure of Y."""
+#     for pollutant, array in Y.items():
+#         print(pollutant)
+#         assert Species.has_key(pollutant)
+#         assert len(array.shape) == 2
+#         assert array.shape[1] == 1
