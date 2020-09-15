5c026ee68a3d40915b3513f01d6d7751f7adc08a
PatrickOHara <pohara@turing.ac.uk>
2020-09-15 14:28:02 +0100

    ✅ Tests for save load models & params

--- containers/tests/test_cleanair/test_utils/conftest.py
+++ containers/tests/test_cleanair/test_utils/conftest.py
@@ -14,6 +14,31 @@ if TYPE_CHECKING:
     from cleanair.types import FeaturesDict, TargetDict
 
 
+@pytest.fixture(scope="function")
+def model_name() -> str:
+    """Name of model for testing utils."""
+    return "test_model"
+
+
+@pytest.fixture(scope="function")
+def tf_session():
+    """A tensorflow session that lasts for only the scope of a function.
+
+    Yields:
+        Tensorflow session.
+    """
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        yield sess
+
+
+@pytest.fixture(autouse=True)
+def init_graph():
+    """Initialise a tensorflow graph."""
+    with tf.Graph().as_default():
+        yield
+
+
 @pytest.fixture(scope="session")
 def input_dir(tmp_path_factory) -> Path:
     """Temporary input directory."""
