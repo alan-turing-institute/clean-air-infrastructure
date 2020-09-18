419c28cd03109c17ef8e378225b1da2c1ccf11d3
Tim Spain <t.spain@ucl.ac.uk>
2020-09-18 15:31:23 +0100

    Load blob with the example values from blob_storage.py:main.

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/load_blob.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/load_blob.py
@@ -2,6 +2,7 @@
 import typer
 from ....loggers import red, green
 from ..state import state, DATA_CACHE
+from ....utils.azure import blob_storage
 
 app = typer.Typer(help="Load a model from blob storage")
 
@@ -12,5 +13,25 @@ def blob(
     ),
 ) -> None:
     """Load a model from blob storage"""
-
     state["logger"].info("Load a model from blob storage")
+
+    # Tokens and the like
+    SAS_TOKEN = blob_storage.generate_sas_token(
+        resource_group="Datasets",
+        storage_container_name="londonaqdatasets",
+        suffix=None,
+    )
+
+    state["logger"].info("Have token")
+
+    # Do the download
+    blob_storage.download_blob(
+        resource_group="Datasets",
+        storage_container_name="glahexgrid",
+        blob_name="Hex350_grid_GLA.zip",
+        account_url="https://londonaqdatasets.blob.core.windows.net",
+        target_file="test.txt",
+        sas_token=SAS_TOKEN,
+        )
+
+    state["logger"].info("Download complete")
