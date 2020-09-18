f4c036399bcb83e51d72a30b693d15f0a4a09adb
Tim Spain <t.spain@ucl.ac.uk>
2020-09-18 15:28:48 +0100

    Load blob using the load function.

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/main.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/main.py
@@ -1,13 +1,13 @@
 """Parser for model fitting."""
 
 import typer
-from . import model_data_cli, setup_cli, save_blob
+from . import model_data_cli, setup_cli, save_blob, load_blob
 
 app = typer.Typer(help="Model fitting and data preperation")
 app.add_typer(model_data_cli.app, name="data")
 app.add_typer(setup_cli.app, name="setup")
 app.add_typer(save_blob.app, name="save")
-app.add_typer(save_blob.app, name="load")
+app.add_typer(load_blob.app, name="load")
 
 if __name__ == "__main__":
     app()
