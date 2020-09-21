0d5b4bb35f8c429d2605b6c393e1389bc2998a28
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2020-09-21 17:25:51 +0100

    🐛 Fix that AzureApp was not seeing

--- containers/urbanair/urbanair/config/__init__.py
+++ containers/urbanair/urbanair/config/__init__.py
@@ -1,4 +1,4 @@
 """Configurations"""
-from .config import get_settings, Settings
+from .config import get_settings, Settings, AzureApp
 
-__all__ = ["get_settings", "Settings"]
+__all__ = ["get_settings", "Settings", "AzureApp"]
