8a5827e21c8a8cf5a702982ecc192621319a234d
Oscar Giles <ogiles@turing.ac.uk>
2020-09-22 10:19:58 +0100

    :white_check_mark: Ensure we create database correctly in tests so we can configure db role first

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# exit when any command fails
+set -e
+
 python containers/entrypoints/setup/configure_db_roles.py -s .secrets/db_secrets_offline.json -c configuration/database_role_config/local_database_config.yaml
 python containers/entrypoints/setup/insert_static_datasets.py insert -t $SAS_TOKEN -s .secrets/db_secrets_offline.json -d street_canyon rectgrid_100 hexgrid london_boundary oshighway_roadlink scoot_detector urban_village
