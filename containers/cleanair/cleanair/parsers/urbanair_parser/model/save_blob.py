6ac6ee9c7c066e9c7907667f649482a4745ef22f
Tim Spain <t.spain@ucl.ac.uk>
2020-09-24 15:50:13 +0100

    Upload model data to blob storage.

--- containers/cleanair/cleanair/parsers/urbanair_parser/model/save_blob.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/model/save_blob.py
@@ -2,15 +2,56 @@
 import typer
 from ....loggers import red, green
 from ..state import state, DATA_CACHE
+from ....utils.azure import blob_storage
+from pathlib import Path
 
 app = typer.Typer(help="Save a model to blob storage")
 
 @app.command()
 def blob(
     model_id: str = typer.Option(
-        "CACHE", help="ID of the model to save",
+        "", help="ID of the model to save",
+    ),
+    token_group: str = typer.Option(
+        "", help="Name of the resource group to obtain the SAS token for.",
+    ),
+    token_container: str = typer.Option(
+        "", help="Name of the storage container to obtain the SAS token for.",
+    ),
+    storage_container: str = typer.Option(
+        "", help="Name of the storage container holding the blob."
+    ),
+    account_url: str = typer.Option(
+        "", help="URL of the Azure account holding the data."
+        ),
+    source_dir: str = typer.Option(
+        DATA_CACHE, help="Local directory to stor the blob in."
     ),
 ) -> None:
     """Save a model to blob storage"""
 
     state["logger"].info("Save a model to blob storage")
+
+    # Tokens and the like
+    SAS_TOKEN = blob_storage.generate_sas_token(
+        resource_group="TIMTEST",
+        storage_container_name="cleanairtimtest",
+        suffix=None,
+    )
+
+    for zipname in ("data.zip",):# "model.zip", "results.zip"):
+        source_file = Path(source_dir).joinpath(zipname)
+
+        state["logger"].info(f"dir = {source_dir}, file name = {zipname}, source file = {source_file}")
+
+        # Download the three compessed files to the data cache
+        blob_storage.upload_blob(
+            resource_group="",
+            storage_container_name=storage_container,
+            blob_name=model_id+ "/"+"model.zip",
+            account_url=account_url,
+            source_file=source_file,
+            sas_token=SAS_TOKEN,
+        )
+
+        state["logger"].info("Upload complete")
--- containers/cleanair/cleanair/utils/azure/blob_storage.py
+++ containers/cleanair/cleanair/utils/azure/blob_storage.py
@@ -86,7 +86,48 @@ def download_blob(
     with open(target_file, "wb") as my_blob:
         blob_data.readinto(my_blob)
 
+def upload_blob(
+    resource_group: str,
+    storage_container_name: str,
+    blob_name: str,
+    account_url: str,
+    source_file: str,
+    sas_token: str = None,
+) -> None:
+    """Upload a file as a blob to a storge container
+    
+    Args:
+        resource_group: The Azure Resource group
+        storage_container_name: The name of the storage container
+        blob_name: Full path to the blob
+        account_url: URL of Azure storage account
+        source_file: File to write data to
+        sas_token: SAS token for Azure blob storage
 
+    Examples:
+        >>> upload_blob(
+                resource_group="Datasets",
+                storage_container_name="glahexgrid",
+                blob_name="Hex350_grid_GLA.zip",
+                account_url="https://londonaqdatasets.blob.core.windows.net",
+                source_file="example.zip",
+                sas_token="<sas-token>"
+            )
+    """
+
+    blob_service_client = BlobServiceClient(
+        account_url=account_url, credential=sas_token
+    )
+
+    blob_container_client = blob_service_client.get_container_client(
+        storage_container_name
+    )
+
+    with open(source_file, "rb") as data:
+        blob_client = blob_container_client.upload_blob(name=blob_name, data=data)
+
+
+        
 if __name__ == "__main__":
 
     SAS_TOKEN = generate_sas_token(
