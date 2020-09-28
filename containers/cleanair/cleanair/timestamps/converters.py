6d4a175d74b39b2b660d8745392d689a6ef0800d
James Robinson <james.em.robinson@gmail.com>
2020-09-28 12:26:39 +0100

    :art: Added type hints to time conversion functions

--- containers/cleanair/cleanair/timestamps/converters.py
+++ containers/cleanair/cleanair/timestamps/converters.py
@@ -3,14 +3,15 @@ Timestamp conversion functions
 """
 from contextlib import suppress
 from datetime import date, datetime, timedelta
+from typing import Any, Union
 from dateutil import parser
 import pytz
 
 
-def as_datetime(maybe_dt):
+def as_datetime(maybe_dt: Any) -> datetime:
     """Convert an input that might be a datetime into a datetime"""
     with suppress(ValueError):
-        return day_to_iso(maybe_dt)
+        return day_to_datetime(maybe_dt)
     if isinstance(maybe_dt, datetime):
         return maybe_dt
     if isinstance(maybe_dt, date):
@@ -18,7 +19,7 @@ def as_datetime(maybe_dt):
     return parser.isoparse(maybe_dt)
 
 
-def safe_strptime(naive_string, format_str):
+def safe_strptime(naive_string: str, format_str: str) -> datetime:
     """Wrapper around strptime to allow for broken time strings"""
     try:
         return datetime.strptime(naive_string, format_str)
@@ -31,7 +32,9 @@ def safe_strptime(naive_string, format_str):
     )
 
 
-def datetime_from_str(naive_string, timezone, rounded=False):
+def datetime_from_str(
+    naive_string: str, timezone: str, rounded: bool = False
+) -> datetime:
     """Convert naive string to localised datetime"""
     local_tz = pytz.timezone(timezone)
     datetime_naive = safe_strptime(naive_string, r"%Y-%m-%d %H:%M:%S")
@@ -41,12 +44,12 @@ def datetime_from_str(naive_string, timezone, rounded=False):
     return datetime_aware
 
 
-def datetime_from_unix(timestamp):
+def datetime_from_unix(timestamp: Union[str, int]) -> datetime:
     """Convert unix timestamp to datetime"""
     return datetime.fromtimestamp(timestamp, pytz.utc)
 
 
-def utcstr_from_unix(timestamp, rounded=False):
+def utcstr_from_unix(timestamp: Union[str, int], rounded: bool = False) -> str:
     """Convert unix timestamp to UTC string"""
     datetime_aware = datetime_from_unix(timestamp)
     if rounded:
@@ -54,7 +57,7 @@ def utcstr_from_unix(timestamp, rounded=False):
     return datetime_aware.strftime(r"%Y-%m-%d %H:%M:%S")
 
 
-def utcstr_from_datetime(input_datetime, rounded=False):
+def utcstr_from_datetime(input_datetime: datetime, rounded: bool = False) -> str:
     """Convert datetime to UTC string"""
     datetime_aware = input_datetime.astimezone(pytz.utc)
     if rounded:
@@ -62,51 +65,45 @@ def utcstr_from_datetime(input_datetime, rounded=False):
     return datetime_aware.strftime(r"%Y-%m-%d %H:%M:%S")
 
 
-def unix_from_str(naive_string, timezone, rounded=False):
+def unix_from_str(naive_string: str, timezone: str, rounded: bool = False) -> datetime:
     """Convert naive string to unix timestamp"""
     return datetime_from_str(naive_string, timezone, rounded).timestamp()
 
 
-def to_nearest_hour(input_datetime):
+def to_nearest_hour(input_datetime: datetime) -> datetime:
     """Rounds to nearest hour by adding a timedelta of one hour if the minute is 30 or later then truncating on hour"""
     if input_datetime.minute >= 30:
         input_datetime += timedelta(hours=1)
     return input_datetime.replace(minute=0, second=0, microsecond=0)
 
 
-def day_to_iso(day: str) -> str:
-    """Convert one of 'now', 'lasthour', 'today', 'tomorrow' or 'yesterday' to a iso string"""
-
+def day_to_datetime(day: str) -> datetime:
+    """Convert one of 'now', 'lasthour', 'today', 'tomorrow' or 'yesterday' to a datetime"""
     # Convert end argument into a datetime
     if day == "now":
-        return datetime.now().replace(microsecond=0, second=0, minute=0).isoformat()
+        return datetime.now().replace(microsecond=0, second=0, minute=0)
     if day == "lasthour":
-        return (
-            (datetime.now() - timedelta(hours=1))
-            .replace(microsecond=0, second=0, minute=0)
-            .isoformat()
+        return (datetime.now() - timedelta(hours=1)).replace(
+            microsecond=0, second=0, minute=0
         )
     if day == "today":
-        return datetime.combine(date.today(), datetime.min.time()).isoformat()
+        return datetime.combine(date.today(), datetime.min.time())
 
     if day == "tomorrow":
-        return datetime.combine(
-            date.today() + timedelta(days=1), datetime.min.time()
-        ).isoformat()
+        return datetime.combine(date.today() + timedelta(days=1), datetime.min.time())
 
     if day == "overmorrow":
-        return datetime.combine(
-            date.today() + timedelta(days=2), datetime.min.time()
-        ).isoformat()
+        return datetime.combine(date.today() + timedelta(days=2), datetime.min.time())
 
     if day == "thirdmorrow":
-        return datetime.combine(
-            date.today() + timedelta(days=3), datetime.min.time()
-        ).isoformat()
+        return datetime.combine(date.today() + timedelta(days=3), datetime.min.time())
 
     if day == "yesterday":
-        return datetime.combine(
-            date.today() - timedelta(days=1), datetime.min.time()
-        ).isoformat()
+        return datetime.combine(date.today() - timedelta(days=1), datetime.min.time())
 
     raise ValueError(f"{day} is not a valid day")
+
+
+def day_to_iso(day: str) -> str:
+    """Convert one of 'now', 'lasthour', 'today', 'tomorrow' or 'yesterday' to a iso string"""
+    return day_to_datetime(day).isoformat()
