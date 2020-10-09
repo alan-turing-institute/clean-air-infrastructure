e7ee03df37a15a617440d899ea030db481da1379
James <james.brandreth@gmail.com>
2020-10-09 13:58:11 +0100

    :art: Removed unused import

--- containers/cleanair/cleanair/databases/tables/jamcam_tables.py
+++ containers/cleanair/cleanair/databases/tables/jamcam_tables.py
@@ -1,5 +1,5 @@
 """Tables for jamcam results"""
-from sqlalchemy import Column, String, BigInteger, Text, Integer
+from sqlalchemy import Column, String, BigInteger, Text
 from sqlalchemy.dialects.postgresql import (
     TIMESTAMP,
     DATE,
