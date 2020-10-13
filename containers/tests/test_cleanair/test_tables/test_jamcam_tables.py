e6e25b6a94fb707dd9473597d5788cc1594f2de9
James <james.brandreth@gmail.com>
2020-10-13 16:19:33 +0100

    :rotating_light: Appeased black

--- containers/tests/test_cleanair/test_tables/test_jamcam_tables.py
+++ containers/tests/test_cleanair/test_tables/test_jamcam_tables.py
@@ -1,21 +1,18 @@
-from cleanair.databases import (
-	DBWriter,
-)
+from cleanair.databases import DBWriter
 from cleanair.databases.tables import JamCamMetaData
 
 
 def test_jamcam_metadata_query(secretfile, connection):
 
-	db_instance = DBWriter(
-		secretfile=secretfile, connection=connection, initialise_tables=False
-	)
+    db_instance = DBWriter(
+        secretfile=secretfile, connection=connection, initialise_tables=False
+    )
 
-	with db_instance.dbcnxn.open_session() as session:
-		query = session.query(JamCamMetaData)
-		results = query.all()
-		first = query.first()
+    with db_instance.dbcnxn.open_session() as session:
+        query = session.query(JamCamMetaData)
+        results = query.all()
+        first = query.first()
 
-		print(results)
+        print(results)
 
-		assert False
-
+        assert False
