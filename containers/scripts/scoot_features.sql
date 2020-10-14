0dbb218465e077b11e373def51f90c73ae38ac53
Oscar Giles <ogiles@turing.ac.uk>
2020-10-14 09:29:38 +0100

    :card_file_box: Update sql

--- containers/scripts/scoot_features.sql
+++ containers/scripts/scoot_features.sql
@@ -3,4 +3,4 @@ FROM model_features.dynamic_feature
 JOIN interest_points.meta_point on interest_points.meta_point.id = point_id
 WHERE model_features.dynamic_feature.measurement_start_utc >= '2020-10-13T00:00:00' 
 AND model_features.dynamic_feature.measurement_start_utc < '2020-10-14T00:00:00'
-ORDER BY point_id ASC, feature_name ASC, measurement_start_utc ASC  LIMIT 100
+ORDER BY point_id ASC, feature_name ASC, measurement_start_utc ASC
