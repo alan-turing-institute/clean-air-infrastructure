d3ee9c0ff6aec02c36dd306a1713d3f3420bec7d
PatrickOHara <pohara@turing.ac.uk>
2020-11-09 11:58:25 +0000

    Need to write down structure of experiment

--- containers/cleanair/cleanair/experiment/experiment.py
+++ containers/cleanair/cleanair/experiment/experiment.py
@@ -2,7 +2,7 @@
 
 from abc import abstractmethod
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 import pandas as pd
 from sqlalchemy import inspect
 from ..databases.mixins import (
@@ -42,12 +42,6 @@ class ExperimentMixin:
         # TODO add instance to dictionary of instances
         # TODO create a new file manager for the instance
 
-    @abstractmethod
-    def load_dataset_from_database(self, data_id: str) -> Any:
-        """With the data id, use the data config to load the dataset from the database"""
-        # if multiple instances have the same data id
-        # then copy the dataset to each of the instance folders
-
     def get_file_manager(self, instance_id: str) -> FileManager:
         """Get the file manager for the given instance"""
         return self._file_managers[instance_id]
@@ -55,6 +49,47 @@ class ExperimentMixin:
     @abstractmethod
     def run(self) -> None:
         """Run the experiment for each instance"""
+
+class SetupExperimentMixin(ExperimentMixin):
+    """Setup an experiment by loading datasets or creating model parameters"""
+
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self._dataset: Dict[str, Any] = dict()
+        self_data_config_lookup: Dict[str, Any] = dict()
+
+    @abstractmethod
+    def load_training_dataset(self, data_id: str) -> Tuple(Any, Any):
+        """Use the data id to load the dataset"""
+
+    @abstractmethod
+    def load_test_dataset(self, data_id: str) -> Any:
+        """Use the data id to load a test dataset"""
+
+    def add_instance(self, instance: InstanceMixin) -> None:
+        super().add_instance(instance)
+        self._data_config_lookup[instance.data_id] = instance.data_config
+
+    def add_dataset(self, data_id: str, dataset: Tuple[Any, Any]) -> None:
+        """Add a dataset"""
+        valid_data_id = False
+        for _, instance in self._instances.values():
+            if instance.data_id == data_id:
+                valid_data_id = True
+        if not valid_data_id:
+            raise ValueError("There are no instances in the experiment with data id:" + data_id)
+        self._dataset[data_id] = dataset
+
+    def load_training_datasets(self) -> Dict[str, Tuple[Any, Any]]:
+        """Get a dictionary of training datasets indexed by the data id"""
+        data_id_list: List[str] = [instance.data_id for _, instance in self._instances.values()]
+        for data_id in data_id_list:
+            training_dataset = self.load_training_dataset(data_id)
+            self.add_dataset(data_id, training_dataset)
+        return 
+
+    def load_test_datasets(self) -> Dict[str, Any]:
+        """Get a dictionary of test datasets indexed by the data id"""
 
 class UpdateExperimentMixin(ExperimentMixin):
     """An experiment that can write to databases"""
@@ -99,3 +134,10 @@ class UpdateExperimentMixin(ExperimentMixin):
 
         # update the instance table
         self.update_table_from_frame(frame, self.instance_table)
+
+class ModelComparisionExperiment(SetupExperimentMixin):
+    """Compare each model on the same datasets"""
+
+class DatasetComparisonExperiment(SetupExperimentMixin):
+    """Compare different dataset configurations (e.g. features) for the same model parameters"""
+
