e75e8e8d22aaf6935da4b7139b0ec4e5a9e5fecb
James <james.brandreth@gmail.com>
2020-11-17 14:35:04 +0000

    :lock: Loads tomtom api key from environment variable

--- containers/urbanair/urbanair/queries/jamcam.py
+++ containers/urbanair/urbanair/queries/jamcam.py
@@ -1,6 +1,7 @@
 import requests
+from ..config import get_settings
 
 
 def get_tomtom_data(zoom: int, x: int, y: int):
-	key = ''
+	key = get_settings().tomtom_api_key
 	return requests.get(f'https://api.tomtom.com/traffic/map/4/tile/flow/absolute/{zoom}/{x}/{y}.png?key={key}&tileSize=512').content
