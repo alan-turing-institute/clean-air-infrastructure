37da360c1be44547542bcfa17a55c2407b572e12
PatrickOHara <pohara@turing.ac.uk>
2020-11-25 16:10:10 +0000

    🐛 Test is broken

---
+++ containers/tests/test_cleanair/test_utils/test_dimension_calculator.py
@@ -0,0 +1,9 @@
+"""Test the dimension calculator for data config"""
+
+from cleanair.utils import total_num_features
+
+def test_total_num_features(laqn_valid_config, valid_full_config_dataset):
+    """Test the total number of features is calculated"""
+    assert total_num_features(laqn_valid_config) == 4
+    assert total_num_features(valid_full_config_dataset) == 20
+    # TODO re-write test for dynamic features
