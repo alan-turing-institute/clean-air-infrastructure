5959da10d283534a110e59eeb159e09f74b79043
Oscar Giles <ogiles@turing.ac.uk>
2020-12-02 14:55:29 +0000

    ⏪ Dont make updates to azure pipelines
    This reverts commit 007de7eb469422c22004ffe18f7215b7aa292b73.

--- terraform/data_collection/kubernetes/main.tf
+++ terraform/data_collection/kubernetes/main.tf
@@ -73,6 +73,17 @@ resource "azurerm_kubernetes_cluster_node_pool" "jamcam_pool" {
   node_taints           = ["group=gpu:NoSchedule"]
 }
 
+resource "azurerm_kubernetes_cluster_node_pool" "jamcambackfill_pool" {
+  name                  = "jamcambackfill"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
+  vm_size               = "Standard_NC24"
+  enable_auto_scaling   = true
+  max_count             = 12
+  min_count             = 0
+  node_count            = 12
+  os_disk_size_gb       = 100
+  node_taints           = ["group=gpu:NoSchedule"]
+}
 
 resource "azurerm_kubernetes_cluster_node_pool" "cleanair_pool" {
   name                  = "cleanair"
@@ -91,23 +102,11 @@ resource "azurerm_kubernetes_cluster_node_pool" "cleanair_pool_gpu" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
   vm_size               = "Standard_NC6"
   enable_auto_scaling   = true
-  max_count             = 1
+  max_count             = 5
   min_count             = 0
   node_count            = 1
   os_disk_size_gb       = 100
   node_taints           = ["group=cleangpu2:NoSchedule"]
-}
-
-resource "azurerm_kubernetes_cluster_node_pool" "jamcambackfill_pool" {
-  name                  = "jamcambf"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
-  vm_size               = "Standard_NC24"
-  enable_auto_scaling   = true
-  max_count             = 5
-  min_count             = 0
-  node_count            = 5
-  os_disk_size_gb       = 100
-  node_taints           = ["group=gpuBackFill:NoSchedule"]
 }
 
 # Set permissions for the pre-existing service principal
