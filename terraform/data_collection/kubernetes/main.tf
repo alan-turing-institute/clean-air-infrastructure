2e5036bc9f0fa828cc59eb78c56c647fcc1086f1
Oscar Giles <ogiles@turing.ac.uk>
2020-12-03 15:41:46 +0000

    ☸️ Piggybacking changes for CleanAir GPU onto this

--- terraform/data_collection/kubernetes/main.tf
+++ terraform/data_collection/kubernetes/main.tf
@@ -91,11 +91,11 @@ resource "azurerm_kubernetes_cluster_node_pool" "cleanair_pool_gpu" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
   vm_size               = "Standard_NC6"
   enable_auto_scaling   = true
-  max_count             = 1
+  max_count             = 2
   min_count             = 0
   node_count            = 1
   os_disk_size_gb       = 100
-  node_taints           = ["group=cleangpu2:NoSchedule"]
+  node_taints           = ["group=cleangpu:NoSchedule"]
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "jamcambackfill_pool" {
