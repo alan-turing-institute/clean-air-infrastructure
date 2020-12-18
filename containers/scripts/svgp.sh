dee04cd9e26cff09afafe8ed6c7dc1aef29ab360
Oscar Giles <ogiles@turing.ac.uk>
2020-12-18 10:48:44 +0000

    🚧 Checking scoot script

--- containers/scripts/svgp.sh
+++ containers/scripts/svgp.sh
@@ -6,17 +6,24 @@ set -e
 # set the secretfile filepath
 urbanair init local --secretfile "$DB_SECRET_FILE"
 
+# Check what scoot data is available (should be 100% - if it isn't scoot data collection isnt working)
+urbanair inputs scoot check --upto today --ndays 5 --nhours 0 
+
+# To check what scoot data missing (I.e. wasn't found in TfL bucket) (should be ~80%. If it isn't might be an issue with scoot system (TfL side))
+urbanair inputs scoot check --upto today --ndays 5 --nhours 0 --missing
+
 
 # Forecast scoot data
-# urbanair --secretfile processors scoot forecast --traindays 5 --preddays 2 --trainupto yesterday 
+urbanair --secretfile processors scoot forecast --traindays 5 --preddays 2 --trainupto today
 
 # Processs scoot features
-# urbanair features scoot fill --ndays 7 --upto overmorrow \
-#     --source laqn \
-#     --source hexgrid \
-#     --insert-method missing \
-#     --nworkers 2 \
-#     --use-readings
+urbanair features scoot fill --ndays 7 --upto overmorrow \
+    --source laqn \
+    --source satellite \
+    --source hexgrid \
+    --insert-method missing \
+    --nworkers 2 \
+    --use-readings
 
 # generate the data config
 urbanair model data generate-config \
