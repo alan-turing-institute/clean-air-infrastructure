6420c80beecf4c6661e6f08c514f4173b01475ae
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-02-16 15:20:55 +0000

    🐛 Fix total num features

--- containers/cleanair/cleanair/utils/dimension_calculator.py
+++ containers/cleanair/cleanair/utils/dimension_calculator.py
@@ -1,14 +1,14 @@
 """Calculate the input dimension of data"""
 
-from ..types import FullDataConfig
+from ..types import DataConfig
 
 
-def total_num_features(data_config: FullDataConfig) -> int:
+def total_num_features(data_config: DataConfig) -> int:
     """Calculate number of features"""
     num_space_dimensions = 2
     num_time_dimensions = 1
-    num_static_features = len(data_config.feature_names)
-    num_dynamic_features = 0  # TODO once SCOOT is ready get num dynamic features
+    num_static_features = len(data_config.static_features) * len(data_config.buffer_sizes)
+    num_dynamic_features = len(data_config.dynamic_features) * len(data_config.buffer_sizes)
     return (
         num_static_features
         + num_dynamic_features
