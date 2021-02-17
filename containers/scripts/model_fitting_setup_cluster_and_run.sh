c09fc414f3de5649b977a06673966cbf49fdb148
Ollie <ohamelijnck@gmail.com>
2021-02-17 22:39:18 +0000

    fix sbatch settings

--- containers/scripts/model_fitting_setup_cluster_and_run.sh
+++ containers/scripts/model_fitting_setup_cluster_and_run.sh
@@ -1,13 +1,13 @@
 #load required variables
 source ../../.secrets/model_fitting_settings.sh
 
-echo $CACHE_DIR
-exit()
 
 #does not assume that the cluster is clean. if cleanair already exists it will delete and reset
 #setup folder structure on cluster and pull most recent docker file
 #we run singularity pull inside a cluster node because pearl sometimes fails on the login node
+
 echo 'Setting up cluster and pulling docker image'
+
 ssh -T -i $CLUSTER_KEY $CLUSTER_USER@$CLUSTER_ADDR  << HERE
     rm -rf cleanair
     mkdir -p logs
@@ -16,7 +16,7 @@ ssh -T -i $CLUSTER_KEY $CLUSTER_USER@$CLUSTER_ADDR  << HERE
     cd cleanair
     mkdir containers    
     cd containers
-    SINGULARITY_DOCKER_USERNAME=$DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD=$DOCKER_PASSWORD bash -c 'srun -n 1 --gres=gpu:0 --export=ALL singularity pull -F docker://cleanairdocker.azurecr.io/model_fitting:latest'
+    SINGULARITY_DOCKER_USERNAME=$DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD=$DOCKER_PASSWORD bash -c 'srun --export=ALL singularity pull -F docker://cleanairdocker.azurecr.io/model_fitting:latest'
 
 HERE
 
@@ -36,9 +36,11 @@ ssh -T -i $CLUSTER_KEY $CLUSTER_USER@$CLUSTER_ADDR  << HERE
 #!/bin/bash
 #SBATCH --job-name=mrdgp
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:0
+#SBATCH --mem-per-cpu=32000
 
 
 #SBATCH --output=logs/slurm_%j__mrdgp,order_id-0.log
@@ -55,7 +57,7 @@ ssh -T -i $CLUSTER_KEY $CLUSTER_USER@$CLUSTER_ADDR  << HERE
 cd ~/cleanair
 # run script with arguments
 #singularity exec containers/model_fitting_latest.sif urbanair model fit $MODEL $CACHE_DIR/
-singularity exec containers/model_fitting_latest.sif urbanair experiment batch dgp_vary_static_feature $i 1 --experiment-root $CACHE_DIR/
+singularity exec containers/model_fitting_latest.sif urbanair experiment batch dgp_vary_static_features $(($i-1)) 1 --experiment-root $CACHE_DIR/
 
 END
     
