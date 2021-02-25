d2f1f9688575e1b69e8a60ef495fbdc89e50e13b
Oliver Hamelijnck <ohamelijnck@gmail.com>
2021-02-25 16:05:46 +0000

    add helper tags

--- containers/scripts/mrdgp.sh
+++ containers/scripts/mrdgp.sh
@@ -3,30 +3,66 @@
 # exit when any command fails
 set -e
 
+#default values
+LOCAL=0
+DOWNLOAD_DATA=1
+
+#handle input flags
+while [[ $# -gt 0 ]]
+do
+key="$1"
+    case $key in
+        --local)
+            LOCAL=1
+            shift # past argument
+        ;;
+        --no-download)
+            DOWNLOAD_DATA=0
+            shift # past argument
+        ;;
+        --help)
+            HELP=1
+            shift # past argument
+        ;;
+    esac
+done
+
+if [ "$HELP" == '1' ]; then
+    echo 'Help:'
+    echo '  --local : Run locally'
+    echo '  --no-download : Do not re-download data'
+    exit
+fi
+
 # set the secretfile filepath
-#TODO: this throws line errors but doesnt seem to be needed?
-urbanair init local --secretfile "$DB_SECRET_FILE"
+if [ "$LOCAL" == '0' ]; then
+    urbanair init local --secretfile "$DB_SECRET_FILE"
+else
+    urbanair init production
+fi
 
-# generate the data config
-urbanair model data generate-config \
-    --trainupto yesterday \
-    --traindays 3 \
-    --preddays 2 \
-    --static-features total_road_length \
-    --static-features flat \
-    --feature-buffer 500 \
-    --train-source laqn \
-    --train-source satellite \
-    --pred-source laqn \
-    --pred-source hexgrid \
-    --species NO2 \
-    --overwrite
+if [ "$DOWNLOAD_DATA" == '1' ]; then
+    # generate the data config
+    urbanair model data generate-config \
+        --trainupto yesterday \
+        --traindays 3 \
+        --preddays 2 \
+        --static-features total_road_length \
+        --static-features flat \
+        --feature-buffer 500 \
+        --train-source laqn \
+        --train-source satellite \
+        --pred-source laqn \
+        --pred-source hexgrid \
+        --species NO2 \
+        --overwrite
 
-# check the data exists in the DB
-urbanair model data generate-full-config
+    # check the data exists in the DB
+    urbanair model data generate-full-config
 
-# download the data using the config
-urbanair model data download --training-data --prediction-data --output-csv
+    # download the data using the config
+    urbanair model data download --training-data --prediction-data --output-csv
+fi
 
 # create the model parameters
 urbanair model setup mrdgp --maxiter 5000 --num-inducing-points 500
