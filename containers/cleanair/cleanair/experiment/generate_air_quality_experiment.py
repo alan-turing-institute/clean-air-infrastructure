a92837a3a63af63bdd60424babed4fdcdfdd8746
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-03-09 21:48:36 +0000

    ♻️ Refactor but lots todo

--- containers/cleanair/cleanair/experiment/generate_air_quality_experiment.py
+++ containers/cleanair/cleanair/experiment/generate_air_quality_experiment.py
@@ -1,8 +1,15 @@
-"""Generate air quality experiments"""
+"""Generate air quality experiments.
+
+Experiments go through 3 stages:
+1. "Vary" means a somewhat brute force approach
+2. "Tuning" means we are beginning to understand which params are good
+3. "Production" means we think this is the best parameter setup
+"""
 
 import itertools
 
-from typing import List, Optional
+from typing import List
+from cleanair.types.dataset_types import DataConfig
 from ..utils import total_num_features
 from ..mixins import InstanceMixin
 from ..models import ModelConfig
@@ -13,21 +20,8 @@ from .default_air_quality_data_config import (
     default_sat_data_config,
 )
 
-# list of static features to iterate through
-STATIC_FEATURES_LIST = [
-    [],
-    [StaticFeatureNames.total_a_road_length],
-    [StaticFeatureNames.water],
-    [StaticFeatureNames.park],
-    [
-        StaticFeatureNames.total_a_road_length,
-        StaticFeatureNames.water,
-        StaticFeatureNames.park,
-    ],
-]
-
 # list used in varying inducing points experiments
-STANDARD_FEATURES_LIST = [
+TUNING_STATIC_FEATURES = [
     [StaticFeatureNames.total_a_road_primary_length, StaticFeatureNames.flat],
     [
         StaticFeatureNames.total_a_road_length,
@@ -36,40 +30,54 @@ STANDARD_FEATURES_LIST = [
         StaticFeatureNames.max_canyon_ratio,
     ],
 ]
-STANDARD_BUFFER_SIZE = [FeatureBufferSize.two_hundred, FeatureBufferSize.one_hundred]
 
-STANDARD_LENGTHSCALES = [0.01, 0.1, 1.0]
-STANDARD_SIG_Y = [5.0]
+# for the "vary static features" experiments, we evaluate how effective different static features are
+VARY_STATIC_FEATURES = [
+    [],
+    [StaticFeatureNames.flat],
+    [StaticFeatureNames.max_canyon_ratio],
+    [StaticFeatureNames.park],
+    [StaticFeatureNames.total_a_road_length],
+    [StaticFeatureNames.total_a_road_primary_length],
+    [StaticFeatureNames.water],
+    [StaticFeatureNames.total_a_road_primary_length, StaticFeatureNames.flat],
+    [
+        StaticFeatureNames.total_a_road_length,
+        StaticFeatureNames.total_a_road_primary_length,
+        StaticFeatureNames.flat,
+        StaticFeatureNames.max_canyon_ratio,
+    ],
+]
+
+# vary the size of the buffer around an interest point for different granularity
+VARY_BUFFER_SIZES = [
+    [FeatureBufferSize.one_hundred],
+    [FeatureBufferSize.two_hundred],
+    [FeatureBufferSize.five_hundred],
+    [FeatureBufferSize.one_thousand],
+    [FeatureBufferSize.one_hundred, FeatureBufferSize.five_hundred],
+    [FeatureBufferSize.two_hundred, FeatureBufferSize.one_thousand],
+]
 
 
-def _get_svgp_kernel_settings(data_config, feature_list):
-    """Return input_dim and active_dims for SVGP model_params."""
-    if len(feature_list) == 0:
-        active_dims = [0, 1, 2]  # work around so that no features are used
-        feature_list = [
-            StaticFeatureNames.park
-        ]  # tempory feature which wont be used by model
-        input_dim = 3
-    else:
-        active_dims = None  # use all features
-        input_dim = total_num_features(data_config)
-
-    return feature_list, input_dim, active_dims
+PRODUCTION_STATIC_FEATURES = [
+    StaticFeatureNames.total_a_road_primary_length,
+    StaticFeatureNames.flat,
+]
+PRODUCTION_BUFFER_SIZES = [
+    FeatureBufferSize.one_hundred,
+    FeatureBufferSize.five_hundred,
+]
+VARY_LENGTHSCALES = [0.01, 0.1, 1.0]
+VARY_LIKELIHOOD_VARIANCE = [5.0]
 
 
-def _get_dgp_kernel_settings(feature_list):
-    """Return input_dim and active_dims for SVGP model_params."""
-    n_features = len(feature_list)
+def __static_features_fix(data_config: DataConfig) -> DataConfig:
+    """Hack for a bug that occurs when the list of static features is empty.
 
-    if len(feature_list) == 0:
-        feature_list = [
-            StaticFeatureNames.park
-        ]  # tempory feature which wont be used by model
-
-    # the DGP  uses n_features to construct active_dims and input_dim internally
-    n_features = n_features + 3
-
-    return feature_list, n_features
+    If the list of static features is empty, add a park feature."""
+    data_config.static_features = [StaticFeatureNames.park]
+    return data_config
 
 
 def svgp_vary_standard(secretfile: str) -> List[InstanceMixin]:
@@ -82,26 +90,22 @@ def svgp_vary_standard(secretfile: str) -> List[InstanceMixin]:
 
     model_config = ModelConfig(secretfile=secretfile)
 
-    for features, buffer_size, lengthscale, sig_y in itertools.product(
-        STANDARD_FEATURES_LIST,
-        STANDARD_BUFFER_SIZE,
-        STANDARD_LENGTHSCALES,
-        STANDARD_SIG_Y,
+    for static_features, lengthscale, sig_y in itertools.product(
+        TUNING_STATIC_FEATURES, VARY_LENGTHSCALES, VARY_LIKELIHOOD_VARIANCE,
     ):
-
+        # create data config
         data_config = default_laqn_data_config()
-        features, input_dim, active_dims = _get_svgp_kernel_settings(
-            data_config, features
-        )
+        data_config.static_features = static_features
+        data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
 
-        # create a data config from static_features
-        data_config.buffer_sizes = [buffer_size]
-        data_config.static_features = features
+        input_dim = total_num_features(data_config)
+        active_dims = list(range(input_dim))  # explicitly calculate active dims
+        data_config = __static_features_fix(data_config)
 
-        # model_config.validate_config(data_config)
-
+        model_config.validate_config(data_config)
         full_data_config = model_config.generate_full_config(data_config)
 
+        # create model params and change
         model_params = default_svgp_model_params(
             num_inducing_points=num_z,
             maxiter=maxiter,
@@ -130,22 +134,15 @@ def dgp_vary_standard(secretfile: str) -> List[InstanceMixin]:
 
     model_config = ModelConfig(secretfile=secretfile)
 
-    for features, buffer_size, lengthscale, sig_y in itertools.product(
-        STANDARD_FEATURES_LIST,
-        STANDARD_BUFFER_SIZE,
-        STANDARD_LENGTHSCALES,
-        STANDARD_SIG_Y,
+    for static_features, lengthscale, sig_y in itertools.product(
+        TUNING_STATIC_FEATURES, VARY_LENGTHSCALES, VARY_LIKELIHOOD_VARIANCE,
     ):
-
-        data_config = default_sat_data_config()
-        features, n_features = _get_dgp_kernel_settings(features)
-
         # create a data config from static_features
-        data_config.buffer_sizes = [buffer_size]
-        data_config.static_features = features
-
-        # model_config.validate_config(data_config)
-
+        data_config = default_sat_data_config()
+        data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
+        data_config.static_features = static_features
+        n_features = total_num_features(data_config)
+        data_config = __static_features_fix(data_config)
         full_data_config = model_config.generate_full_config(data_config)
 
         model_params = default_mrdgp_model_params(
@@ -174,26 +171,18 @@ def cached_instance(secretfile: str) -> List[InstanceMixin]:
     data_config = default_sat_data_config()
 
     # get all features and sources
-    static_features = list(StaticFeatureNames)
-    buffer_sizes = list(FeatureBufferSize)
-    train_sources = [Source.laqn, Source.satellite]
-    pred_sources = [Source.laqn, Source.hexgrid]
-
-    # create a data config from static_features
-    data_config.buffer_sizes = buffer_sizes
-    data_config.static_features = static_features
-    data_config.train_sources = train_sources
-    data_config.pred_sources = pred_sources
+    data_config.buffer_sizes = list(FeatureBufferSize)
+    data_config.static_features = list(StaticFeatureNames)
+    data_config.train_sources = [Source.laqn, Source.satellite]
+    data_config.pred_sources = [Source.laqn, Source.hexgrid]
+    input_dim = total_num_features(data_config)
+    active_dims = list(range(input_dim))
 
     # ensure valid config and data is available
     model_config.validate_config(data_config)
-
     full_data_config = model_config.generate_full_config(data_config)
 
     # SVGP as a tempory model just to create the instance
-    static_features, input_dim, active_dims = _get_svgp_kernel_settings(
-        data_config, static_features
-    )
     model_params = default_svgp_model_params(
         active_dims=active_dims, input_dim=input_dim
     )
@@ -213,20 +202,24 @@ def svgp_vary_static_features(secretfile: str) -> List[InstanceMixin]:
     instance_list: List[InstanceMixin] = []
 
     model_config = ModelConfig(secretfile=secretfile)
-    for static_features in STATIC_FEATURES_LIST:
-        static_features, input_dim, active_dims = _get_svgp_kernel_settings(
-            static_features
-        )
-
-        model_params = default_svgp_model_params(
-            active_dims=active_dims, input_dim=input_dim
-        )
-
+    for static_features in VARY_STATIC_FEATURES:
         # create a data config from static_features
         data_config = default_laqn_data_config()
         data_config.static_features = static_features
+
+        # get num features - if no static features then add 'fake' feature
+        input_dim = total_num_features(data_config)
+        active_dims = list(range(input_dim))
+        data_config = __static_features_fix(data_config)
+
+        # ensure the data settings are valid
         model_config.validate_config(data_config)
         full_data_config = model_config.generate_full_config(data_config)
+
+        # get the model params for the given data settings
+        model_params = default_svgp_model_params(
+            active_dims=active_dims, input_dim=input_dim
+        )
 
         # create instance and add to list
         instance = InstanceMixin(
@@ -236,21 +229,33 @@ def svgp_vary_static_features(secretfile: str) -> List[InstanceMixin]:
     return instance_list
 
 
-def svgp_vary_num_inducing_points(
-    num_inducing_points_list: Optional[List[int]] = None,
-) -> List[InstanceMixin]:
+def svgp_vary_num_inducing_points(secretfile: str) -> List[InstanceMixin]:
     """Vary the number of inducing points in an SVGP"""
-    data_config = ...
-    instance_list = []
-    num_inducing_points_list = (
-        [100, 200] if not num_inducing_points_list else num_inducing_points_list
-    )
+    instance_list: List[InstanceMixin] = []
+    model_config = ModelConfig(secretfile=secretfile)
+
+    data_config = default_laqn_data_config()
+    data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
+    data_config.static_features = PRODUCTION_STATIC_FEATURES
+
+    # get num features - if no static features then add 'fake' feature
+    input_dim = total_num_features(data_config)
+    active_dims = list(range(input_dim))
+    data_config = __static_features_fix(data_config)
+
+    # ensure the data settings are valid
+    model_config.validate_config(data_config)
+    full_data_config = model_config.generate_full_config(data_config)
+
+    num_inducing_points_list = [100, 200, 500, 1000]
     for num_inducing_points in num_inducing_points_list:
         model_params = default_svgp_model_params(
-            num_inducing_points=num_inducing_points
+            num_inducing_points=num_inducing_points,
+            active_dims=active_dims,
+            input_dim=input_dim,
         )
         instance = InstanceMixin(
-            data_config, ModelName.svgp, model_params, tag=Tag.validation
+            full_data_config, ModelName.svgp, model_params, tag=Tag.validation
         )
         instance_list.append(instance)
     return instance_list
@@ -258,26 +263,23 @@ def svgp_vary_num_inducing_points(
 
 def dgp_vary_static_features(secretfile: str) -> List[InstanceMixin]:
     """Default DGP with changing static features"""
-
     instance_list: List[InstanceMixin] = []
-
     model_config = ModelConfig(secretfile=secretfile)
-    data_config = default_sat_data_config()
 
-    for static_features in STATIC_FEATURES_LIST:
-        n_features = len(static_features)
-        # default model parameters for every model
-        if len(static_features) == 0:
-            static_features = [
-                StaticFeatureNames.park
-            ]  # tempory feature which wont be used by model
-        # add 3 for epoch, lat, lon
-        n_features = 3 + n_features
-        model_params = default_mrdgp_model_params(n_features=n_features)
+    for static_features in VARY_STATIC_FEATURES:
 
         # create a data config from static_features
+        data_config = default_sat_data_config()
         data_config.static_features = static_features
+        n_features = total_num_features(data_config)
+        data_config = __static_features_fix(data_config)
+
+        # ensure the data settings are valid
+        model_config.validate_config(data_config)
         full_data_config = model_config.generate_full_config(data_config)
+
+        # create model parameters
+        model_params = default_mrdgp_model_params(n_features=n_features)
 
         # create instance and add to list
         instance = InstanceMixin(
@@ -294,26 +296,20 @@ def dgp_vary_inducing_and_maxiter(secretfile: str) -> List[InstanceMixin]:
     iters = [1000, 5000, 10000]
 
     instance_list: List[InstanceMixin] = []
-
     model_config = ModelConfig(secretfile=secretfile)
-    data_config = default_sat_data_config()
-    static_features = STANDARD_FEATURES_LIST
-
-    n_features = len(static_features)
-
-    # default model parameters for every model
-    if len(static_features) == 0:
-        static_features = [
-            StaticFeatureNames.park
-        ]  # tempory feature which wont be used by model
-
-    # add 3 for epoch, lat, lon
-    n_features = 3 + n_features
 
     # create a data config from static_features
-    data_config.static_features = static_features
+    data_config = default_sat_data_config()
+    data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
+    data_config.static_features = PRODUCTION_STATIC_FEATURES
+    n_features = total_num_features(data_config)
+    data_config = __static_features_fix(data_config)
+
+    # ensure the data settings are valid
+    model_config.validate_config(data_config)
     full_data_config = model_config.generate_full_config(data_config)
 
+    # try all permutations of inducing points and max iterations
     for num_z, maxiter in itertools.product(inducing_point_sizes, iters):
         model_params = default_mrdgp_model_params(
             n_features=n_features, num_inducing_points=num_z, maxiter=maxiter
@@ -336,37 +332,18 @@ def dgp_small_inducing_and_maxiter(secretfile: str) -> List[InstanceMixin]:
 
     instance_list: List[InstanceMixin] = []
 
+    # create a data config from static_features
     model_config = ModelConfig(secretfile=secretfile)
     data_config = default_sat_data_config()
-
-    static_features = STANDARD_FEATURES_LIST
-    buffer_sizes = STANDARD_BUFFER_SIZE
-
-    n_features = len(static_features)
-
-    # default model parameters for every model
-    if len(static_features) == 0:
-        active_dims = [0, 1, 2]  # work around so that no features are used
-        static_features = [
-            StaticFeatureNames.park
-        ]  # tempory feature which wont be used by model
-        input_dim = 3
-    else:
-        active_dims = None  # use all features
-        input_dim = len(static_features)  # tempory feature which wont be used by model
-
-    # add 3 for epoch, lat, lon
-    n_features = 3 + n_features
-
-    # create a data config from static_features
-    data_config.buffer_sizes = buffer_sizes
-    data_config.static_features = static_features
-
+    data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
+    data_config.static_features = PRODUCTION_STATIC_FEATURES
+    input_dim = total_num_features(data_config)  # calculate num features before fix
+    data_config = __static_features_fix(data_config)
     full_data_config = model_config.generate_full_config(data_config)
 
     for num_z, maxiter in itertools.product(inducing_point_sizes, iters):
         model_params = default_mrdgp_model_params(
-            n_features=n_features, num_inducing_points=num_z, maxiter=maxiter
+            n_features=input_dim, num_inducing_points=num_z, maxiter=maxiter
         )
 
         # create instance and add to list
@@ -385,18 +362,14 @@ def svgp_small_inducing_and_maxiter(secretfile: str) -> List[InstanceMixin]:
 
     instance_list: List[InstanceMixin] = []
 
+    # create a data config from static_features
     model_config = ModelConfig(secretfile=secretfile)
     data_config = default_laqn_data_config()
-
-    static_features = STANDARD_FEATURES_LIST
-    buffer_sizes = STANDARD_BUFFER_SIZE
-
-    static_features, input_dim, active_dims = _get_svgp_kernel_settings(static_features)
-
-    # create a data config from static_features
-    data_config.buffer_sizes = buffer_sizes
-    data_config.static_features = static_features
-
+    data_config.buffer_sizes = PRODUCTION_BUFFER_SIZES
+    data_config.static_features = PRODUCTION_STATIC_FEATURES
+    input_dim = total_num_features(data_config)
+    active_dims = list(range(input_dim))  # explicitly calculate active dims
+    data_config = __static_features_fix(data_config)
     full_data_config = model_config.generate_full_config(data_config)
 
     for num_z, maxiter in itertools.product(inducing_point_sizes, iters):
---
+++ containers/tests/test_cleanair/test_experiment/test_generate_experiment.py
@@ -0,0 +1,9 @@
+"""Test air quality experiments are generated"""
+
+# TODO lots of small issues to fix before we can do proper testing
+# from cleanair.experiment.generate_air_quality_experiment import svgp_vary_static_features, VARY_STATIC_FEATURES
+
+# def test_svgp_vary_static_features(secretfile, connection, fake_laqn_static_dataset):
+#     """Test parameters are correct for varying static features"""
+#     instance_list = svgp_vary_static_features(secretfile, connection=connection)
+#     assert len(instance_list) == len(VARY_STATIC_FEATURES)
