7e701057ebbae77ff9e93f8c1931a22c1ef4695c
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-03-31 14:34:15 +0100

    🚨 Add experiment dir to cli

--- containers/cleanair/cleanair/parsers/urbanair_parser/production/main.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/production/main.py
@@ -1,9 +1,11 @@
 """CLI for production models"""
 
 from enum import Enum
+from pathlib import Path
 import typer
 from ..experiment.main import run, setup, update
 from ....types import ExperimentName
+from ..shared_args import ExperimentDir
 
 app = typer.Typer(help="Production CLI")
 
@@ -16,24 +18,28 @@ class StaticDynamic(str, Enum):
 
 
 @app.command()
-def svgp(static_or_dynamic: StaticDynamic) -> None:
+def svgp(
+    static_or_dynamic: StaticDynamic, experiment_root: Path = ExperimentDir
+) -> None:
     """Run the production SVGP model"""
     if static_or_dynamic == StaticDynamic.dynamic:
         experiment_name = ExperimentName.production_svgp_dynamic
     else:
         experiment_name = ExperimentName.production_svgp_static
-    setup(experiment_name)
-    run(experiment_name)
-    update(experiment_name)
+    setup(experiment_name, experiment_root=experiment_root)
+    run(experiment_name, experiment_root=experiment_root)
+    update(experiment_name, experiment_root=experiment_root)
 
 
 @app.command()
-def mrdgp(static_or_dynamic: StaticDynamic) -> None:
+def mrdgp(
+    static_or_dynamic: StaticDynamic, experiment_root: Path = ExperimentDir
+) -> None:
     """Run the production MRDGP model"""
     if static_or_dynamic == StaticDynamic.dynamic:
         experiment_name = ExperimentName.production_mrdgp_dynamic
     else:
         experiment_name = ExperimentName.production_mrdgp_static
-    setup(experiment_name)
-    run(experiment_name)
-    update(experiment_name)
+    setup(experiment_name, experiment_root=experiment_root)
+    run(experiment_name, experiment_root=experiment_root)
+    update(experiment_name, experiment_root=experiment_root)
