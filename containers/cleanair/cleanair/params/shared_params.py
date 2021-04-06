1ec5a8b95f29cc3289b32f153c9ae6df3b6cdd97
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-04-06 16:34:08 +0100

    🚑 Reduce number of features for production model

--- containers/cleanair/cleanair/params/shared_params.py
+++ containers/cleanair/cleanair/params/shared_params.py
@@ -9,15 +9,10 @@ MINIBATCH_SIZE: int = 100
 
 PRODUCTION_DYNAMIC_FEATURES = [
     DynamicFeatureNames.avg_n_vehicles,
-    DynamicFeatureNames.max_n_vehicles,
 ]
 PRODUCTION_FORECAST_DAYS = 2
 
 PRODUCTION_STATIC_FEATURES = [
     StaticFeatureNames.total_a_road_primary_length,
-    StaticFeatureNames.flat,
 ]
-PRODUCTION_BUFFER_SIZES = [
-    FeatureBufferSize.one_hundred,
-    FeatureBufferSize.five_hundred,
-]
+PRODUCTION_BUFFER_SIZES = [FeatureBufferSize.two_hundred]
