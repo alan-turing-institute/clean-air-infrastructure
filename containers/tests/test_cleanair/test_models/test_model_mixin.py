cf6e94febdb508a5e5a923a463f2cc5e327b7492
James <james.brandreth@gmail.com>
2021-04-16 15:17:02 +0100

    :white_check_mark: Added model data cleaning test

--- containers/tests/test_cleanair/test_models/test_model_mixin.py
+++ containers/tests/test_cleanair/test_models/test_model_mixin.py
@@ -1,6 +1,7 @@
 """Test the model mixin."""
-
+import numpy as np
 from cleanair.models import SVGP
+
 
 # pylint: disable=unused-argument
 def test_elbo_logger(tf_session, svgp_model_params, x_train, y_train) -> None:
@@ -11,3 +12,14 @@ def test_elbo_logger(tf_session, svgp_model_params, x_train, y_train) -> None:
     model.fit(x_train, y_train)
     assert len(model.elbo) == svgp_model_params.maxiter
     assert len(model.elbo) == model.epoch
+
+
+def test_clean_data(svgp_model_params, x_cleaning_array, y_cleaning_array) -> None:
+    model = SVGP(svgp_model_params)
+
+    x_clean, y_clean = model.clean_data(x_cleaning_array, y_cleaning_array)
+
+    np.testing.assert_array_equal(y_clean, np.array([[1.0], [9.0]], np.float64))
+    np.testing.assert_array_equal(
+        x_clean, np.array([[1.0, 1.0], [9.0, 9.0]], np.float64)
+    )
