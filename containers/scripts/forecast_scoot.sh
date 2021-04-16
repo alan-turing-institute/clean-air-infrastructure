00b1e81d363133a4135e9f0c5d8021941944bd66
James <james.brandreth@gmail.com>
2021-04-16 15:33:30 +0100

    :rocket: Changed scoot to train over 14 days

--- containers/scripts/forecast_scoot.sh
+++ containers/scripts/forecast_scoot.sh
@@ -7,7 +7,7 @@
 urbanair init local --secretfile "$DB_SECRET_FILE"
 
 # Forecast scoot data
-urbanair processors scoot forecast --traindays 5 --preddays 2 --trainupto tomorrow
+urbanair processors scoot forecast --traindays 14 --preddays 2 --trainupto tomorrow
 
 # Processs scoot features
 urbanair features scoot fill --ndays 7 --upto thirdmorrow \
