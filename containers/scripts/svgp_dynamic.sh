60a9efc847b67b8e8fef9237fc7598d7d099ce6e
James <james.brandreth@gmail.com>
2021-04-28 15:41:46 +0100

    :rocket: Added logging to production:
    - Scoot forecasting
    - SVPG + MRDGP

--- containers/scripts/svgp_dynamic.sh
+++ containers/scripts/svgp_dynamic.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Function to upload logs to blob storage
+DATE=`date +"%Y_%m_%d_%T"`
+LOGFILE="mrdpg_static_${DATE}.log"
+
+check_exit() {
+    if [ $1 -ne 0 ];
+    then
+        urbanair logs upload $LOGFILE
+        exit 1
+    fi
+}
 
 # set the secretfile filepath (if on own machine, use 'init production' to write to the production database)
-urbanair init local --secretfile "$DB_SECRET_FILE"
+urbanair init local --secretfile "$DB_SECRET_FILE" >> $LOGFILE 2>&1
 
-urbanair production svgp dynamic
+urbanair production svgp dynamic >> $LOGFILE 2>&1
 
+urbanair logs upload $LOGFILE
--- containers/scripts/svgp_static.sh
+++ containers/scripts/svgp_static.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# Function to upload logs to blob storage
+DATE=`date +"%Y_%m_%d_%T"`
+LOGFILE="mrdpg_static_${DATE}.log"
+
+check_exit() {
+    if [ $1 -ne 0 ];
+    then
+        urbanair logs upload $LOGFILE
+        exit 1
+    fi
+}
 
 # set the secretfile filepath (if on own machine: init production)
-urbanair init local --secretfile "$DB_SECRET_FILE"
+urbanair init local --secretfile "$DB_SECRET_FILE" >> $LOGFILE 2>&1
 
-urbanair production svgp static
+urbanair production svgp static >> $LOGFILE 2>&1
+
+urbanair logs upload $LOGFILE
