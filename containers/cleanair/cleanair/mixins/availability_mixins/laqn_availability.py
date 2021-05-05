2d8059b9308b29b9650c4c220067f9551e33bae8
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-05-05 17:47:53 +0100

    Queries for laqn sensors

--- containers/cleanair/cleanair/mixins/availability_mixins/laqn_availability.py
+++ containers/cleanair/cleanair/mixins/availability_mixins/laqn_availability.py
@@ -58,7 +58,7 @@ class LAQNAvailabilityMixin:
 
     @db_query()
     def get_raw_laqn_data(
-        self, species: List[str], start_date: str, end_date: Optional[str] = None,
+        self, species: List[str], start_date: str, end_date: Optional[str] = None, point_ids: List[str] = None, site_codes: List[str] = None
     ):
 
         """Get raw LAQN sensor data between a start_date and end_date for a particular species
@@ -83,6 +83,10 @@ class LAQNAvailabilityMixin:
 
             if end_date:
                 in_data = in_data.filter(LAQNReading.measurement_start_utc < end_date)
+            if site_codes:
+                in_data = in_data.filter(LAQNSite.site_code.in_(site_codes))
+            if point_ids:
+                in_data = in_data.filter(LAQNSite.point_id.in_(point_ids))
 
             return in_data.order_by(
                 LAQNSite.point_id,
