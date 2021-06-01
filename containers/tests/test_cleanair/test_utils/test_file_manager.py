5510f6429bb961509441bc6c9bec1f184ffc0318
James Brandreth <james.brandreth@gmail.com>
2021-06-01 11:06:58 +0100

    :rotating_light: Removed unused import

--- containers/tests/test_cleanair/test_utils/test_file_manager.py
+++ containers/tests/test_cleanair/test_utils/test_file_manager.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 from datetime import datetime, timedelta
-from json import load
 from typing import List, TYPE_CHECKING
 import numpy as np
 import tensorflow as tf
