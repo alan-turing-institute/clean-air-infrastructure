549cfb233449bf530e697ec48efe0e0b3d8388e3
dead-water <toxicgas1@mac.com>
2021-08-05 11:49:06 -0700

    adding cache priming

--- containers/scripts/mrdgp_dynamic.sh
+++ containers/scripts/mrdgp_dynamic.sh
@@ -18,3 +18,28 @@ urbanair init local --secretfile "$DB_SECRET_FILE" >> $LOGFILE 2>&1
 urbanair production mrdgp dynamic >> $LOGFILE 2>&1
 
 urbanair logs upload $LOGFILE
+
+# prime the cache by polling the API, triggering the TTLCaching functions
+HTTP_DATE="$(date +'%Y-%m-%d')"
+HTTP_USER="prime-cache"
+HTTP_PASS="$(< $HTTP_SECRET_FILE)"
+
+for i in {00..23}
+  do
+  # prime each hour of json call
+  wget -O - --http-user="${HTTP_USER}" --http-password="${HTTP_PASS}" \
+  "https://urbanair.turing.ac.uk/api/v1/air_quality/forecast/hexgrid/json?time=${HTTP_DATE}T12%3A${i}&lon_min=-0.51&lon_max=0.335&lat_min=51.286&lat_max=51.692" &> /dev/null
+
+  # prime each hour of geojson call
+  wget -O - --http-user="${HTTP_USER}" --http-password="${HTTP_PASS}" \
+  "https://urbanair.turing.ac.uk/api/v1/air_quality/forecast/hexgrid/geojson?time=${HTTP_DATE}T12%3A${i}&lon_min=-0.51&lon_max=0.335&lat_min=51.286&lat_max=51.692" &> /dev/null
+done
+
+# prime 48 hour json call
+wget -O - --http-user="${HTTP_USER}" --http-password="${HTTP_PASS}" \
+ "https://urbanair.turing.ac.uk/api/v1/air_quality/forecast/hexgrid/json/48hr?date=${HTTP_DATE}&lon_min=-0.51&lon_max=0.335&lat_min=51.286&lat_max=51.692" &> /dev/null
+# prime 48 hour geojson call
+wget -O - --http-user="${HTTP_USER}" --http-password="${HTTP_PASS}" \
+ "https://urbanair.turing.ac.uk/api/v1/air_quality/forecast/hexgrid/json/48hr?date=${HTTP_DATE}&lon_min=-0.51&lon_max=0.335&lat_min=51.286&lat_max=51.692" &> /dev/null
+
+ 
