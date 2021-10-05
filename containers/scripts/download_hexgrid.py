82c2039f1583490b5b91bb39987269d4a7297d07
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-10-05 17:05:15 +0100

    Add filepath

--- containers/scripts/download_hexgrid.py
+++ containers/scripts/download_hexgrid.py
@@ -1,6 +1,7 @@
 """Download the hexgrid to a CSV file"""
 
 from pathlib import Path
+import pandas as pd
 import typer
 
 from cleanair.databases import DBReader
@@ -10,11 +11,12 @@ from cleanair.mixins.query_mixins.hexgrid_query_mixin import HexGridQueryMixin
 class HexGridQuery(HexGridQueryMixin, DBReader):
     """Hexgrid query"""
 
-def main(secretfile: Path):
+def main(secretfile: Path, hexgrid_csv: Path):
     """Download hexgrid"""
     query = HexGridQuery(secretfile=secretfile)
-    df = query.query_hexgrid(output_type="df")
-    print(df.sample(5))
+    df: pd.DataFrame = query.query_hexgrid(output_type="df")
+    print(df.sample(5)) 
+    df.to_csv(hexgrid_csv)
 
 if __name__ == "__main__":
     typer.run(main)
