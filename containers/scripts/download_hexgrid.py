c5a98dcee91d04580079df1da879d363a0198ed1
Oliver Hamelijnck <ohamelijnck@gmail.com>
2021-10-06 10:10:24 +0100

    add target file path

--- containers/scripts/download_hexgrid.py
+++ containers/scripts/download_hexgrid.py
@@ -10,11 +10,12 @@ from cleanair.mixins.query_mixins.hexgrid_query_mixin import HexGridQueryMixin
 class HexGridQuery(HexGridQueryMixin, DBReader):
     """Hexgrid query"""
 
-def main(secretfile: Path):
+def main(secretfile: Path, outputfile: Path):
     """Download hexgrid"""
     query = HexGridQuery(secretfile=secretfile)
     df = query.query_hexgrid(output_type="df")
-    print(df.sample(5))
+
+    df.to_csv(outputfile, index=False)
 
 if __name__ == "__main__":
     typer.run(main)
