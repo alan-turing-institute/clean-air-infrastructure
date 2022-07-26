6b36c4a663c657a7d377a58748c2e203ef537538
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2022-07-26 12:54:49 +0100

    🏷️ Fix type hinting

--- containers/urbanair/urbanair/databases/database.py
+++ containers/urbanair/urbanair/databases/database.py
@@ -1,5 +1,5 @@
 """UrbanAir API database interactions"""
-from typing import List, Tuple, Optional
+from typing import List, Tuple
 from fastapi import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Query, Session
