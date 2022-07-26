396b1d6fc9cfb7469a99e3a754d6060ce5bf5875
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2022-07-26 13:04:01 +0100

    Handle eccodes env vars

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -11,18 +11,19 @@ cd ..
 export ECCODES_MAJOR_VERSION=2
 export ECCODES_MINOR_VERSION=26
 export ECCODES_PATCH_VERSION=0
-export ECCODES_SRC_DIR="eccodes-${ECCODES_VERSION}-Source"
 export ECCODES_VERSION="${ECCODES_MAJOR_VERSION}.${ECCODES_MINOR_VERSION}.${ECCODES_PATCH_VERSION}"
-wget -c "https://confluence.ecmwf.int/download/attachments/45757960/${ECCODES_SRC_DIR}.tar.gz"
-tar -xzf "${ECCODES_SRC_DIR}.tar.gz"
+export ECCODES_SRC_DIR="eccodes-${ECCODES_VERSION}-Source"
+export ECCODES_TAR_NAME="eccodes-${ECCODES_VERSION}-Source.tar.gz"
+wget -c "https://confluence.ecmwf.int/download/attachments/45757960/${ECCODES_TAR_NAME}"
+tar -xzf ${ECCODES_TAR_NAME}
 mkdir ${ECCODES_SRC_DIR}/build
 cd ${ECCODES_SRC_DIR}/build
 cmake -DENABLE_AEC=OFF ..
 cmake --build .
 cmake --install .
+cd ../../clean-air-infrastructure
 
 # create secretfile for db
-cd ../..
 mkdir -p .secrets
 echo '{
     "username": "postgres",
