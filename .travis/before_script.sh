1fc143a89d2cfe753180909e243d87b3b5c89e12
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2022-07-26 19:26:25 +0100

    What's wrong with the secretfile?

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -3,5 +3,5 @@
 # exit when any command fails
 set -e
 
-python containers/entrypoints/setup/configure_db_roles.py -s $DB_SECRET_FILE -c configuration/database_role_config/local_database_config.yaml
-python containers/entrypoints/setup/insert_static_datasets.py insert -t $SAS_TOKEN -s $DB_SECRET_FILE -d street_canyon hexgrid london_boundary oshighway_roadlink scoot_detector
+python containers/entrypoints/setup/configure_db_roles.py -s .secrets/db_secrets_offline.json -c configuration/database_role_config/local_database_config.yaml
+python containers/entrypoints/setup/insert_static_datasets.py insert -t $SAS_TOKEN -s .secrets/db_secrets_offline.json -d street_canyon hexgrid london_boundary oshighway_roadlink scoot_detector
