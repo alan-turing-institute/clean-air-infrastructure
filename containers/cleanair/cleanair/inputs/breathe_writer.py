78bf830fc8442a29cc1759743567d934b91b2fd5
Sueda Ciftci <sueda.ciftci@warwick.ac.uk>
2023-01-18 19:19:08 +0000

    modify BL request readings

--- containers/cleanair/cleanair/inputs/breathe_writer.py
+++ containers/cleanair/cleanair/inputs/breathe_writer.py
@@ -1,4 +1,6 @@
-import datetime
+
+from datetime import datetime, timedelta
+import pytz
 import requests
 import json
 from ..mixins.api_request_mixin import APIRequestMixin
@@ -43,34 +45,39 @@ class BreatheWriter(DateRangeMixin, APIRequestMixin, BreatheAvailabilityMixin, D
             return None
         except (TypeError, KeyError):
             return None
-    # def request_site_readings(self, start_date, end_date, site_code):
-    #     """
-    #     Request all readings for {site_code} between {start_date} and {end_date}
-    #     Remove duplicates and add the site_code
-    #     """
-    #     try:
-    #         # This call retrieves data from Breathe London nodes as a JSON object.
-    #         endpoint = "http://api.breathelondon.org/api/api/getClarityData/:siteCode/:species/:startTime/:endTime/:averaging".format(
-    #             site_code, str(start_date), str(end_date)
-    #         )
-    #         raw_data = self.get_response(endpoint, timeout=120.0).json()[
-    #             "AirQualityData"
-    #         ]["Data"]
-    #         # Drop duplicates
-    #         processed_data = [dict(t) for t in {tuple(d.items()) for d in raw_data}]
-    #         # Add the site_code
-    #         for reading in processed_data:
-    #             reading["SiteCode"] = site_code
-    #             timestamp_start = datetime_from_str(
-    #                 reading.pop("@MeasurementDateGMT"), timezone="GMT", rounded=True
-    #             )
-    #             timestamp_end = timestamp_start + datetime.timedelta(hours=1)
-    #             reading["MeasurementStartUTC"] = utcstr_from_datetime(timestamp_start)
-    #             reading["MeasurementEndUTC"] = utcstr_from_datetime(timestamp_end)
-    #         return processed_data
-    #     except requests.exceptions.HTTPError as error:
-    #         self.logger.warning("Request to %s failed:", endpoint)
-    #         self.logger.warning(error)
-    #         return None
-    #     except (TypeError, KeyError):
-    #         return None
+    def request_site_readings(self, start_date, end_date, site_code, species, averaging):
+        """
+        Request all readings for {site_code}, {species} between {start_date} and {end_date} {averaging} eg:hourly
+        Remove duplicates and add the site_code
+        """
+        try:
+            # This call retrieves data from Breathe London nodes as a JSON object.
+            endpoint = "http://api.breathelondon.org/api/api/getClarityData/{}/{}/{}/{}/{}".format(
+                site_code, species, str(start_date), str(end_date), averaging
+            )
+            sites = self.get_response(endpoint, timeout=120.0).content
+            raw_data =sites.decode()
+            parsed_data = json.loads(raw_data)
+            # Add the site_code
+            for reading in parsed_data:
+                reading["SiteCode"] = site_code
+                # Use strptime to convert the string to a datetime object
+                start_time = reading["DateTime"]
+                date_time_obj = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+                utc_time = pytz.utc.localize(date_time_obj)
+                # convert to UTC
+                formatted_date_time = utc_time.strftime("%Y-%m-%d %H:%M:%S")
+                timestamp_start = datetime_from_str(
+                    str(formatted_date_time), timezone="GMT", rounded=True
+                )
+                timestamp_end = timestamp_start + timedelta(hours=1)
+                reading["MeasurementStartUTC"] = utcstr_from_datetime(timestamp_start)
+                reading["MeasurementEndUTC"] = utcstr_from_datetime(timestamp_end)
+            return parsed_data
+        
+        except requests.exceptions.HTTPError as error:
+            self.logger.warning("Request to %s failed:", endpoint)
+            self.logger.warning(error)
+            return None
+        except (TypeError, KeyError):
+            return None
