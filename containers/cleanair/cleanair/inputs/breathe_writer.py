76dc25f0f7c562e61cbffa8efcb847f29b4cd6af
Sueda Ciftci <sueda.ciftci@warwick.ac.uk>
2023-01-19 19:44:02 +0000

    all functions for breathe London

--- containers/cleanair/cleanair/inputs/breathe_writer.py
+++ containers/cleanair/cleanair/inputs/breathe_writer.py
@@ -7,8 +7,9 @@ from ..mixins.api_request_mixin import APIRequestMixin
 from ..mixins.date_range_mixin import DateRangeMixin
 from ..mixins.availability_mixins import LAQNAvailabilityMixin
 from ..mixins.availability_mixins.breathe_availability import BreatheAvailabilityMixin
-from ..databases import DBWriter
-from ..databases.tables import MetaPoint, LAQNSite, LAQNReading
+from ..databases import DBWriter 
+from ..databases.tables.breathe_tables import BreatheSite, BreatheReading
+from ..databases.tables import  MetaPoint
 from ..loggers import get_logger, green
 from ..timestamps import datetime_from_str, utcstr_from_datetime
 
@@ -65,14 +66,10 @@ class BreatheWriter(DateRangeMixin, APIRequestMixin, BreatheAvailabilityMixin, D
                 start_time = reading["DateTime"]
                 date_time_obj = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%fZ")
                 utc_time = pytz.utc.localize(date_time_obj)
-                # convert to UTC
-                formatted_date_time = utc_time.strftime("%Y-%m-%d %H:%M:%S")
-                timestamp_start = datetime_from_str(
-                    str(formatted_date_time), timezone="GMT", rounded=True
-                )
+                timestamp_start = utc_time
                 timestamp_end = timestamp_start + timedelta(hours=1)
-                reading["MeasurementStartUTC"] = utcstr_from_datetime(timestamp_start)
-                reading["MeasurementEndUTC"] = utcstr_from_datetime(timestamp_end)
+                reading["MeasurementStartUTC"] = timestamp_start.strftime("%Y-%m-%d %H:%M:%S")
+                reading["MeasurementEndUTC"] = timestamp_end.strftime("%Y-%m-%d %H:%M:%S")
             return parsed_data
         
         except requests.exceptions.HTTPError as error:
@@ -81,3 +78,98 @@ class BreatheWriter(DateRangeMixin, APIRequestMixin, BreatheAvailabilityMixin, D
             return None
         except (TypeError, KeyError):
             return None
+
+    def update_site_list_table(self):
+        """
+        Update the breathe_site table
+        """
+        self.logger.info("Starting Breathe site list update...")
+
+        # Open a DB session
+        with self.dbcnxn.open_session() as session:
+            # Reload site information and update the database accordingly
+            self.logger.info("Requesting site info from %s", green("API"))
+
+            # Retrieve site entries (discarding any that do not have a known position)
+            site_entries = [
+                s
+                for s in self.request_site_entries()
+                if s["Latitude"] and s["Longitude"]
+            ]
+            for entry in site_entries:
+                entry["geometry"] = MetaPoint.build_ewkt(
+                    entry["Latitude"], entry["Longitude"]
+                )
+
+            # Only consider unique sites
+            unique_sites = {
+                s["geometry"]: MetaPoint.build_entry("breathe", geometry=s["geometry"])
+                for s in site_entries
+            }
+
+            # Update the interest_points table and retrieve point IDs
+            point_id = {}
+            for geometry, interest_point in unique_sites.items():
+                merged_point = session.merge(interest_point)
+                session.flush()
+                point_id[geometry] = str(merged_point.id)
+
+            # Add point IDs to the site_entries
+            for entry in site_entries:
+                entry["point_id"] = point_id[entry["geometry"]]
+
+            # Build the site entries and commit
+            self.logger.info("Updating site info database records")
+            for site in site_entries:
+                session.merge(BreatheSite.build_entry(site))
+            self.logger.info(
+                "Committing changes to database tables %s and %s",
+                green(MetaPoint.__tablename__),
+                green(BreatheSite.__tablename__),
+            )
+            session.commit()
+
+    def update_reading_table(self, usecore=True):
+        """Update the readings table with new sensor readings."""
+        self.logger.info(
+            "Starting %s readings update using data from %s to %s...",
+            green("Breathe"),
+            self.start_datetime,
+            self.end_datetime,
+        )
+
+        # Open a DB session
+        with self.dbcnxn.open_session() as session:
+            # Load readings for all sites and update the database accordingly
+            site_info_query = session.query(BreatheSite)
+            self.logger.info(
+                "Requesting readings from %s for %s sites",
+                green("API"),
+                green(len(list(site_info_query))),
+            )
+
+        # Get all readings for each site between its start and end dates and update the database
+        site_readings = self.get_readings_by_site(
+            site_info_query, self.start_date, self.end_date
+        )
+        site_records = [
+            BreatheReading.build_entry(site_reading, return_dict=usecore)
+            for site_reading in site_readings
+        ]
+
+        # Commit the records to the database
+        self.commit_records(site_records, on_conflict="overwrite", table=BreatheReading)
+
+        # Commit changes
+        self.logger.info(
+            "Committing %s records to database table %s",
+            green(len(site_readings)),
+            green(BreatheReading.__tablename__),
+        )
+
+    def update_remote_tables(self):
+        """Update all relevant tables on the remote database"""
+        self.update_site_list_table()
+        self.update_reading_table()
+
+    
