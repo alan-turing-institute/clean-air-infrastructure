6bfe35c961e7d8df20d96fd94cc1a802b28adf0c
Sueda Ciftci <sueda.ciftci@warwick.ac.uk>
2023-06-08 15:18:25 +0100

    add editable dates

--- containers/cleanair/cleanair/parsers/urbanair_parser/dataset/main.py
+++ containers/cleanair/cleanair/parsers/urbanair_parser/dataset/main.py
@@ -1,12 +1,14 @@
-"""Exporting dataset from Cleanair database."""
-# pylint: disable=C0411
+"""
+Export LAQN datasets with specified dates
+"""
 
 import typer
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
+from typing import List, Optional
 
-from ....dataset.model_config import ModelConfig
+from ....dataset.model_config import ModelConfig, DataConfig
 from ....dataset.model_data import ModelData
 from ..state import state
 from typing import Dict
@@ -27,26 +29,37 @@ app = typer.Typer(help="Experiment CLI")
 @app.command()
 def download(
     download_root: Path,
+    train_start_date: Optional[datetime] = None,
+    train_end_date: Optional[datetime] = None,
+    pred_start_date: Optional[datetime] = None,
+    pred_end_date: Optional[datetime] = None,
+    static_features: Optional[List[StaticFeatureNames]] = None,
     verbose: bool = False,
 ) -> None:
-    """Setup load data"""
-    secretfile: str = state["secretfile"]
-    initialise_logging(verbose)  # set logging level
+    """
+    Set up data download.
+    """
+
+    # Set up logging level
+    initialise_logging(verbose)
+
+    # Create download root directory if it doesn't exist
     download_root.mkdir(parents=True, exist_ok=True)
 
-    # todo scecify dataConfig uzantilarini
+    # Define data configuration
     data_config = DataConfig(
-        train_start_date=datetime(2021, 4, 15),
-        train_end_date=datetime(2021, 4, 19),
-        pred_start_date=datetime(2021, 4, 19),
-        pred_end_date=datetime(2021, 4, 20),
+        train_start_date=train_start_date,
+        train_end_date=train_end_date,
+        pred_start_date=pred_start_date,
+        pred_end_date=pred_end_date,
         include_prediction_y=False,
         train_sources=[Source.laqn],
         pred_sources=[Source.laqn],
         pred_interest_points={Source.laqn: "all"},
         species=[Species.NO2],
         norm_by=Source.laqn,
-        static_features=[
+        static_features=static_features
+        or [
             StaticFeatureNames.park,
             StaticFeatureNames.flat,
             StaticFeatureNames.total_road_length,
@@ -57,10 +70,12 @@ def download(
         dynamic_features=[],
     )
 
+    # Set up model configuration
+    secretfile = state["secretfile"]
     model_config = ModelConfig(secretfile=secretfile)
     model_data = ModelData(secretfile=secretfile)
     full_config = model_config.generate_full_config(data_config)
-    training_data: Dict[Source, pd.DateFrame] = model_data.download_config_data(  # noqa
-        full_config, training_data=True
-    )
+
+    # Download and save training data
+    training_data = model_data.download_config_data(full_config, training_data=True)
     training_data[Source.laqn].to_csv(download_root / "training_data.csv")
