1565aa9e2a53e1c3a460eb0ebeaf9a5ef2dc22c5
Sueda Ciftci <sueda.ciftci@warwick.ac.uk>
2024-07-20 23:41:07 +0100

    update train mrdgp with file manager

--- containers/cleanair/gpjax_models/gpjax_models/parser/model/fit_cli.py
+++ containers/cleanair/gpjax_models/gpjax_models/parser/model/fit_cli.py
@@ -4,16 +4,18 @@ import typer
 import pickle
 import pandas as pd
 from typing import Optional
+from pathlib import Path
 import os
 import jax
 import optax
 import numpy as np
 import jax.numpy as jnp
 
-from jax.config import config as jax_config
+from jax import config
 
-jax_config.update("jax_enable_x64", True)
-
+# Set a configuration option if needed (example)
+config.update("jax_enable_x64", True)
+from ...utils.file_manager import FileManager
 from ...models.svgp import SVGP
 from ...models.stgp_svgp import STGP_SVGP_SAT, STGP_SVGP
 from ...models.stgp_mrdgp import STGP_MRDGP
@@ -24,12 +26,6 @@ train_file_path = "datasets/aq_data.pkl"
 
 
 app = typer.Typer()
-
-# Defining blob storage and other constants here
-RESOURCE_GROUP = "Datasets"
-STORAGE_CONTAINER_NAME = "aqdata"
-STORAGE_ACCOUNT_NAME = "londonaqdatasets"
-ACCOUNT_URL = "https://londonaqdatasets.blob.core.windows.net/"
 
 
 @app.command()
@@ -77,7 +73,7 @@ def train_svgp_sat(
     root_dir: str,
     M: int = 500,
     batch_size: int = 200,
-    num_epochs: int = 2500,
+    num_epochs: int = 50,
 ):
     """
     Train the SVGP_GPF2 model on the given training data.
@@ -135,6 +131,7 @@ def train_svgp_sat(
         },
     }
     # Train the model
+    breakpoint()
     model.fit(x_sat, y_sat, pred_laqn_data, pred_sat_data)
     typer.echo("Training complete!")
 
@@ -209,7 +206,7 @@ def train_svgp_laqn(
 # TODO make one train comand to reach out config to get the model name
 @app.command()
 def train_mrdgp(
-    root_dir: str,
+    root_dir: Path,
     M: Optional[int] = 500,
     batch_size: Optional[int] = 200,
     num_epochs: Optional[int] = 100,
@@ -226,36 +223,25 @@ def train_mrdgp(
     """
 
     model = STGP_MRDGP(M, batch_size, num_epochs, pretrain_epochs, root_dir)
+    file_manager = FileManager(root_dir)
     # Load training data
     typer.echo("Loading training data!")
-    # Iterate over the directories and subdirectories
-    for dirpath, _, filenames in os.walk(root_dir):
-        # Check if 'training_dataset.pkl' exists in the current directory
-        if "training_dataset.pkl" in filenames:
-            # If found, load the data
-            file_path = os.path.join(dirpath, "training_dataset.pkl")
-            with open(file_path, "rb") as file:
-                train_data = pickle.load(file)
+    training_data = file_manager.load_training_data()
 
     typer.echo("Loading testing data!")
-    for dirpath, _, filenames in os.walk(root_dir):
-        # Check if 'training_dataset.pkl' exists in the current directory
-        if "test_dataset.pkl" in filenames:
-            # If found, load the data
-            file_path = os.path.join(dirpath, "test_dataset.pkl")
-            with open(file_path, "rb") as file:
-                test_data_dict = pickle.load(file)
+    test_data = file_manager.load_training_data()
 
-    train_dict, test_dict = generate_data(train_data, test_data_dict)
-    x_laqn = train_dict["laqn"]["X"]
-    y_laqn = train_dict["laqn"]["Y"]
-    x_sat = train_dict["sat"]["X"]
-    y_sat = train_dict["sat"]["Y"]
+    breakpoint()
+    training_dict, test_dict = generate_data(training_data, test_data)
+    x_laqn = training_dict["laqn"]["X"]
+    y_laqn = training_dict["laqn"]["Y"]
+    x_sat = training_dict["sat"]["X"]
+    y_sat = training_dict["sat"]["Y"]
 
     pred_sat_data = {
         "sat": {
-            "X": train_dict["sat"]["X"],
-            "Y": train_dict["sat"]["Y"],
+            "X": training_dict["sat"]["X"],
+            "Y": training_dict["sat"]["Y"],
         },
     }
 
@@ -268,9 +254,9 @@ def train_mrdgp(
             "X": test_dict["laqn"]["X"],
             "Y": None,
         },
-        "train_laqn": {
-            "X": train_dict["laqn"]["X"],
-            "Y": train_dict["laqn"]["Y"],
+        "training_laqn": {
+            "X": training_dict["laqn"]["X"],
+            "Y": training_dict["laqn"]["Y"],
         },
     }
     model.fit(x_sat, y_sat, x_laqn, y_laqn, pred_laqn_data, pred_sat_data)
@@ -282,21 +268,31 @@ def train_mrdgp_trf(
     root_dir: str,
     M: Optional[int] = 500,
     batch_size: Optional[int] = 200,
-    num_epochs: Optional[int] = 100,
-    pretrain_epochs: Optional[int] = 100,
+    num_epochs: Optional[int] = 50,
+    pretrain_epochs: Optional[int] = 50,
+    random_seed: Optional[int] = 0,
 ):
     """
     Train the SVGP_GPF2 model on the given training data.
 
     Args:
-        train_file_path (str): Path to the training data pickle file.
+        root_dir (str): Root directory containing training and testing data.
         M (int): Number of inducing variables.
         batch_size (int): Batch size for training.
         num_epochs (int): Number of training epochs.
+        pretrain_epochs (int): Number of pretraining epochs.
+        random_seed (int): Random seed for reproducibility.
     """
 
+    generator = np.random.default_rng(random_seed)
     model = STGP_MRDGP(
-        M, batch_size, num_epochs, pretrain_epochs, root_dir, random_seed=42
+        M,
+        batch_size,
+        num_epochs,
+        pretrain_epochs,
+        root_dir,
+        jax_random_seed=random_seed,
+        generator=generator,
     )
     # Load training data
     typer.echo("Loading training data!")
@@ -311,7 +307,7 @@ def train_mrdgp_trf(
 
     typer.echo("Loading testing data!")
     for dirpath, _, filenames in os.walk(root_dir):
-        # Check if 'training_dataset.pkl' exists in the current directory
+        # Check if 'test_dataset.pkl' exists in the current directory
         if "test_dataset.pkl" in filenames:
             # If found, load the data
             file_path = os.path.join(dirpath, "test_dataset.pkl")
